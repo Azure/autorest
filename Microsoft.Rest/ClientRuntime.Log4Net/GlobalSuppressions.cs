f86c49ed0cab94d42271cc1b83abcd1253a9e386
yugangw-msft <yugangw@microsoft.com>
2015-02-25 15:20:13 -0800

    supress violations from ETW and trace project

--- Microsoft.Rest/ClientRuntime.Log4Net/GlobalSuppressions.cs
+++ Microsoft.Rest/ClientRuntime.Log4Net/GlobalSuppressions.cs
@@ -3,3 +3,4 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", MessageId = "log4net.ILog.DebugFormat(System.String,System.Object[])", Scope = "member", Target = "Microsoft.Rest.Tracing.Log4Net.Log4NetTracingInterceptor.#Enter(System.String,System.Object,System.String,System.Collections.Generic.IDictionary`2<System.String,System.Object>)")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1305:SpecifyIFormatProvider", MessageId = "System.String.Format(System.String,System.Object,System.Object)", Scope = "member", Target = "Microsoft.Rest.Tracing.Log4Net.Log4NetTracingInterceptor.#Exit(System.String,System.Object)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification="We do give it strong name and sign it when build in CI server and verify it")]
