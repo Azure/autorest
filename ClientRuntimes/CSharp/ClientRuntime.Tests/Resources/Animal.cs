ae92bd048fc0d19d12b24ff6585b650d107b4533
stankovski <denis.stankovski@gmail.com>
2015-05-08 11:28:21 -0700

    Added tests for polymorphic serialization

---
+++ ClientRuntimes/CSharp/ClientRuntime.Tests/Resources/Animal.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Microsoft.Rest.ClientRuntime.Tests.Resources
+{
+    [JsonObject("animal")]
+    public class Animal
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    [JsonObject("dog")]
+    public class Dog : Animal
+    {
+        [JsonProperty("likesDogfood")]
+        public bool LikesDogfood { get; set; }
+    }
+
+    [JsonObject("cat")]
+    public class Cat : Animal
+    {
+        [JsonProperty("likesMice")]
+        public bool LikesMice { get; set; }
+
+        [JsonProperty("dislikes")]
+        public Animal Dislikes { get; set; }
+    }
+
+    public class Zoo
+    {
+        public int Id { get; set; }
+        private List<Animal> _animals = new List<Animal>();
+        public List<Animal> Animals { get { return _animals; } set { _animals = value; } }
+    }
+}
