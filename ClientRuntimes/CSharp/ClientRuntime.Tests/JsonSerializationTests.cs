fcf9dcdca656506bf8bd302acb9645d42599afc9
stankovski <denis.stankovski@gmail.com>
2015-05-12 16:21:11 -0700

    Added read-only property contract resolver and updated json converters

--- ClientRuntimes/CSharp/ClientRuntime.Tests/JsonSerializationTests.cs
+++ ClientRuntimes/CSharp/ClientRuntime.Tests/JsonSerializationTests.cs
@@ -1,14 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using Microsoft.Rest.ClientRuntime.Tests.Fakes;
 using Microsoft.Rest.ClientRuntime.Tests.Resources;
-using Microsoft.Rest.TransientFaultHandling;
+using Microsoft.Rest.JsonSerialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Microsoft.Rest.ClientRuntime.Tests
@@ -36,6 +32,107 @@ namespace Microsoft.Rest.ClientRuntime.Tests
             Assert.Equal(zoo.Animals[1].GetType(), zoo2.Animals[1].GetType());
             Assert.Equal(((Cat)zoo.Animals[1]).Dislikes.GetType(), ((Cat)zoo2.Animals[1]).Dislikes.GetType());
             Assert.Contains("dType", serializedJson);
+        }
+
+        [Fact]
+        public void PolymorphismWorksWithReadOnlyProperties()
+        {
+            var deserializeSettings = new JsonSerializerSettings();
+            deserializeSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            deserializeSettings.NullValueHandling = NullValueHandling.Ignore;
+            deserializeSettings.Converters.Add(new PolymorphicDeserializeJsonConverter<Animal>("dType"));
+
+            string zooWithPrivateSet = @"{
+              ""Id"": 1,
+              ""Animals"": [
+                {
+                  ""dType"": ""dog"",
+                  ""likesDogfood"": true,
+                  ""name"": ""Fido""
+                },
+                {
+                  ""dType"": ""cat"",
+                  ""likesMice"": false,
+                  ""dislikes"": {
+                    ""dType"": ""dog"",
+                    ""likesDogfood"": true,
+                    ""name"": ""Angry""
+                  },
+                  ""name"": ""Felix""
+                },
+                {
+                  ""dType"": ""siamese"",
+                  ""color"": ""grey"",
+                  ""likesMice"": false,
+                  ""dislikes"": null,
+                  ""name"": ""Felix""
+                }
+              ]
+            }";
+
+            var zoo2 = JsonConvert.DeserializeObject<Zoo>(zooWithPrivateSet, deserializeSettings);
+
+            Assert.Equal("grey", ((Siamese)zoo2.Animals[2]).Color);
+        }
+
+        [Fact]
+        public void RawJsonIsSerialized()
+        {
+            var serializeSettings = new JsonSerializerSettings();
+            serializeSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            serializeSettings.ContractResolver = new ReadOnlyJsonContractResolver();
+
+            var firstAlienJson = JsonConvert.SerializeObject(new Alien("green") { Name = "autorest", Planet = "Mars", Body = JObject.Parse(@"{ ""custom"" : ""json"" }") }, 
+                Formatting.Indented, serializeSettings);
+
+            var firstAlien = JsonConvert.DeserializeObject<Alien>(firstAlienJson, serializeSettings);
+
+            string secondAlienJson = @"{
+                    ""color"": ""green"",
+                    ""planet"": ""Mars"",
+                    ""name"": ""autorest"",
+                    ""body"": { ""custom"" : ""json"" },
+                }";
+
+            var secondAlien = JsonConvert.DeserializeObject<Alien>(secondAlienJson, serializeSettings);
+
+            Assert.Equal("autorest", firstAlien.Name);
+            Assert.Null(firstAlien.Color);
+            Assert.Null(firstAlien.GetPlanetName());
+            Assert.Equal("json", firstAlien.Body.custom.ToString());
+
+            Assert.Equal("autorest", secondAlien.Name);
+            Assert.Equal("green", secondAlien.Color);
+            Assert.Equal("json", secondAlien.Body.custom.ToString());
+        }
+
+        [Fact]
+        public void ReadOnlyPropertiesWorkStandalone()
+        {
+            var serializeSettings = new JsonSerializerSettings();
+            serializeSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+            serializeSettings.ContractResolver = new ReadOnlyJsonContractResolver();
+
+            var firstAlienJson = JsonConvert.SerializeObject(new Alien("green") { Name = "autorest", Planet = "Mars" },
+                Formatting.Indented, serializeSettings);
+
+            var firstAlien = JsonConvert.DeserializeObject<Alien>(firstAlienJson, serializeSettings);
+
+            string secondAlienJson = @"{
+                    ""color"": ""green"",
+                    ""planet"": ""Mars"",
+                    ""name"": ""autorest""
+                }";
+
+            var secondAlien = JsonConvert.DeserializeObject<Alien>(secondAlienJson, serializeSettings);
+
+            Assert.Equal("autorest", firstAlien.Name);
+            Assert.Null(firstAlien.Color);
+            Assert.Null(firstAlien.GetPlanetName());
+
+            Assert.Equal("autorest", secondAlien.Name);
+            Assert.Equal("green", secondAlien.Color);
+            Assert.Equal("Mars", secondAlien.GetPlanetName());
         }
     }
 }
--- ClientRuntimes/CSharp/ClientRuntime/JsonSerialization/PolymorphicDeserializeJsonConverter.cs
+++ ClientRuntimes/CSharp/ClientRuntime/JsonSerialization/PolymorphicDeserializeJsonConverter.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace Microsoft.Rest
+namespace Microsoft.Rest.JsonSerialization
 {
     /// <summary>
     /// JsonConverter that handles deserialization for polymorphic objects
