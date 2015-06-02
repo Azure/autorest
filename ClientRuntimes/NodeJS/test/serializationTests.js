7657667024c001aced330619ceaa4b641b713db6
Amar Zavery <amzavery@microsoft.com>
2015-06-02 13:25:26 -0700

    fix header

--- ClientRuntimes/NodeJS/test/serializationTests.js
+++ ClientRuntimes/NodeJS/test/serializationTests.js
@@ -1,6 +1,3 @@
-//
-// Copyright (c) Microsoft and contributors.  All rights reserved.
-//
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
