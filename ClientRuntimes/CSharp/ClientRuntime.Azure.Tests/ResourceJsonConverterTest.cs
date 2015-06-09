10391be35464495f3000c7e3e77909f2638f7b00
stankovski <denis.stankovski@gmail.com>
2015-06-09 12:33:37 -0700

    4099032: Added support for "ProvisionState" field in generic resources

--- ClientRuntimes/CSharp/ClientRuntime.Azure.Tests/ResourceJsonConverterTest.cs
+++ ClientRuntimes/CSharp/ClientRuntime.Azure.Tests/ResourceJsonConverterTest.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using Microsoft.Rest.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Microsoft.Azure.Common.Test
@@ -105,6 +106,51 @@ namespace Microsoft.Azure.Common.Test
   }
 }", json);
         }
+
+        [Fact]
+        public void TestGenericResourceSerialization()
+        {
+            var sampleResource = new GenericResource()
+            {
+                Location = "EastUS",
+                Properties = JObject.Parse("{ \"size\" : \"3\" }")
+            };
+            sampleResource.Tags = new Dictionary<string, string>();
+            sampleResource.Tags["tag1"] = "value1";
+            var serializeSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver()
+            };
+            serializeSettings.Converters.Add(new ResourceJsonConverter());
+            serializeSettings.Converters.Add(new PolymorphicSerializeJsonConverter<SampleResourceChild>("dType"));
+            string json = JsonConvert.SerializeObject(sampleResource, serializeSettings);
+            Assert.Equal(@"{
+  ""properties"": {
+    ""size"": ""3""
+  },
+  ""location"": ""EastUS"",
+  ""tags"": {
+    ""tag1"": ""value1""
+  }
+}", json);
+
+            var deserializeSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver()
+            };
+            deserializeSettings.Converters.Add(new ResourceJsonConverter());
+            deserializeSettings.Converters.Add(new PolymorphicDeserializeJsonConverter<SampleResourceChild>("dType"));
+            var deserializedResource = JsonConvert.DeserializeObject<GenericResource>(json, deserializeSettings);
+            var jsonoverProcessed = JsonConvert.SerializeObject(deserializedResource, serializeSettings);
+
+            Assert.Equal(json, jsonoverProcessed);
+        }
         
         [Fact]
         public void TestProvisioningStateDeserialization()
@@ -137,6 +183,43 @@ namespace Microsoft.Azure.Common.Test
             var deserializedResource = JsonConvert.DeserializeObject<SampleResource>(expected, deserializeSettings);
 
             Assert.Equal("some string", deserializedResource.ProvisioningState);
+        }
+
+        [Fact]
+        public void TestGenericResourceDeserialization()
+        {
+            var expected = @"{
+                              ""location"": ""EastUS"",
+                              ""tags"": {
+                                ""tag1"": ""value1""
+                              },
+                              ""properties"": {
+                                ""size"": ""3"",
+                                ""provisioningState"": ""some string"",
+                                ""child"": {
+                                  ""dType"": ""SampleResourceChild1"",
+                                  ""name1"": ""name1"",
+                                  ""id"": ""child""
+                                }
+                              }
+                            }";
+
+            var deserializeSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver()
+            };
+            deserializeSettings.Converters.Add(new ResourceJsonConverter());
+            deserializeSettings.Converters.Add(new PolymorphicDeserializeJsonConverter<SampleResourceChild>("dType"));
+            var deserializedResource = JsonConvert.DeserializeObject<GenericResource>(expected, deserializeSettings);
+
+            Assert.Equal("some string", deserializedResource.ProvisioningState);
+            Assert.Equal("EastUS", deserializedResource.Location);
+            Assert.Equal("value1", deserializedResource.Tags["tag1"]);
+            Assert.Equal("3", ((JObject)deserializedResource.Properties)["size"]);
+            Assert.Equal("name1", ((JObject)deserializedResource.Properties)["child"]["name1"]);
         }
     }
 }
