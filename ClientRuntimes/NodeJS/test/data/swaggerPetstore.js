1e3ee420c05ba5bbefbadc5ec68d9bd6c1af7469
Yugang Wang <walterwyg@hotmail.com>
2015-06-17 19:21:31 -0700

    consolidate all the change in the generated code

--- ClientRuntimes/NodeJS/test/data/swaggerPetstore.js
+++ ClientRuntimes/NodeJS/test/data/swaggerPetstore.js
@@ -36,6 +36,9 @@ var SwaggerPetstore = ( /** @lends SwaggerPetstore */ function() {
    * {@link https://github.com/request/request#requestoptions-callback Options doc}
    */
   function SwaggerPetstore(baseUri, options) {
+    if (!options) {
+      options = {};
+    }
     SwaggerPetstore['super_'].call(this, options.credentials, options);
     
     this.baseUri = baseUri;
