99d1762ab2ee0fbf1485a2c8a76dc28dcf7d3348
stankovski <denis.stankovski@gmail.com>
2015-06-24 14:36:31 -0700

    Fixed path to AutoRest.Core.dll

--- AutoRest/Generators/CSharp/CSharp.Tests/RegenerateExpectedClasses.ps1
+++ AutoRest/Generators/CSharp/CSharp.Tests/RegenerateExpectedClasses.ps1
@@ -24,7 +24,7 @@ $tests =
 }
 
 # TODO: direct reference to AutoRest .Net objects and methods leaves handles to the binaries open. Invoke the command line instead.
-Import-Module "$PSScriptRoot\..\..\..\..\binaries\Net45-Debug\AutoRest.Core.dll"
+Import-Module "$PSScriptRoot\..\..\..\..\binaries\net45\AutoRest.Core.dll"
 
 foreach ($test in $tests.GetEnumerator())
 {
@@ -35,7 +35,8 @@ foreach ($test in $tests.GetEnumerator())
     $settings.OutputDirectory = "$PSScriptRoot\Expected\$($test.Key)"
     $settings.Input = "$PSScriptRoot\$($test.Value)"
     $settings.Header = "NONE"
+    #Remove-Item .\$($settings.OutputDirectory) -Recurse -Force
     Write-Output "Generating $($test.Value)"
     [Microsoft.Rest.Generator.AutoRest]::Generate($settings)
-    #&"$PSScriptRoot\..\..\..\..\binaries\Net45-Debug\AutoRest.exe" -Modeler Swagger -CodeGenerator $flavor -OutputDirectory "$PSScriptRoot\Expected" -Namespace "$namespace" -Input "$PSScriptRoot\$($test.Value)" -Header NONE
+    #&"$PSScriptRoot\..\..\..\..\binaries\net45\AutoRest.exe" -Modeler Swagger -CodeGenerator $flavor -OutputDirectory "$PSScriptRoot\Expected" -Namespace "$namespace" -Input "$PSScriptRoot\$($test.Value)" -Header NONE
 }
