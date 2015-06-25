2398676ff154b8953f799e181d2ad310038b2f48
ogail <Abdelrahman.Elogeel@microsoft.com>
2015-06-25 14:38:57 -0700

    Fix tests race condition

---
+++ AutoRest/Generators/CSharp/Azure.CSharp.Tests/Expected/SwaggerBat/AzureReport.Cs/IAutoRestReportServiceForAzure.cs
@@ -0,0 +1,31 @@
+namespace Fixtures.Azure.SwaggerBatAzureReport
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Rest;
+    using Microsoft.Azure;
+    using Models;
+
+    /// <summary>
+    /// Test Infrastructure for AutoRest
+    /// </summary>
+    public partial interface IAutoRestReportServiceForAzure : IDisposable
+    {
+        /// <summary>
+        /// The base URI of the service.
+        /// </summary>
+        Uri BaseUri { get; set; }
+
+        /// <summary>
+        /// Get test coverage report
+        /// </summary>
+        /// <param name='cancellationToken'>
+        /// Cancellation token.
+        /// </param>
+        Task<AzureOperationResponse<IDictionary<string, int?>>> GetReportWithOperationResponseAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+    }
+}
