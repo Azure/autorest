42065c0b90f3842ece1833bce7e26708c26537ea
yugangw-msft <yugangw@microsoft.com>
2015-07-09 17:54:26 -0700

    notetest

--- AutoRest/Generators/AcceptanceTests/NugetPackageTest/PackageTests.cs
+++ AutoRest/Generators/AcceptanceTests/NugetPackageTest/PackageTests.cs
@@ -25,24 +25,10 @@ namespace NugetPackageTest
         public ServiceController Fixture { get; set; }
 
         [Fact]
-        public void TestClientRuntimeWorks()
+        public void TestCSharpCodeGenWorks()
         {
-
             var client = new AutoRestNumberTestService(Fixture.Uri);
-            client.Number.PutBigFloat(3.402823e+20);
-            client.Number.PutSmallFloat(3.402823e-20);
-            client.Number.PutBigDouble(2.5976931e+101);
-            client.Number.PutSmallDouble(2.5976931e-101);
-            client.Number.PutBigDoubleNegativeDecimal(-99999999.99);
-            client.Number.PutBigDoublePositiveDecimal(99999999.99);
-            client.Number.GetNull();
             Assert.Equal(3.402823e+20, client.Number.GetBigFloat());
-            Assert.Equal(3.402823e-20, client.Number.GetSmallFloat());
-            Assert.Equal(2.5976931e+101, client.Number.GetBigDouble());
-            Assert.Equal(2.5976931e-101, client.Number.GetSmallDouble());
-            Assert.Equal(-99999999.99, client.Number.GetBigDoubleNegativeDecimal());
-            Assert.Equal(99999999.99, client.Number.GetBigDoublePositiveDecimal());
-            Assert.Throws<JsonReaderException>(() => client.Number.GetInvalidDouble());
             Assert.Throws<JsonReaderException>(() => client.Number.GetInvalidFloat());
         }
     }
