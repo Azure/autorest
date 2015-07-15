7d9a72cc4478534a4379c3b8d5f564fc86c9c77b
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2015-07-15 11:11:33 -0700

    Added PageCollection to ClientRuntime

---
+++ ClientRuntimes/CSharp/ClientRuntime.Azure.Tests/PageJsonTest.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Rest.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Microsoft.Azure;
+
+namespace Microsoft.Rest.ClientRuntime.Azure.Test
+{
+    public class Product
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        public Product()
+        {
+        }
+    }
+
+    public class PageJsonTest
+    {
+        [Fact]
+        public void TestNextLinkDeSerialization()
+        {
+            var responseBody = @"{
+  ""value"": [
+    {
+      ""id"": ""Product_1"",
+      ""name"": ""ProductOne""
+    },
+    {
+      ""id"": ""Product_2"",
+      ""name"": ""ProductThree""
+    }
+  ],
+  ""nextLink"": ""https://sdktestvault7826.vault.azure.net:443/keys?api-version=2015-06-01""
+}";
+
+            var deserializeSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver()
+            };
+            deserializeSettings.Converters.Add(new ResourceJsonConverter());
+            deserializeSettings.Converters.Add(new PolymorphicDeserializeJsonConverter<Product>("dType"));
+            var deserializedProduct = JsonConvert.DeserializeObject<PageCollection<Product>>(responseBody, deserializeSettings);
+            
+            Assert.Equal(2, deserializedProduct.Items.Count);
+            Assert.Equal("https://sdktestvault7826.vault.azure.net:443/keys?api-version=2015-06-01", deserializedProduct.NextPage);
+        }
+
+        [Fact]
+        public void TestOdataNextLinkDeSerialization()
+        {
+            var responseBody = @"{
+  ""value"": [
+    {
+      ""id"": ""Product_1"",
+      ""name"": ""ProductOne""
+    }
+  ],
+  ""@odata.nextLink"": ""https://ODataLink.vault.azure.net:443/keys?api-version=2015-06-01""
+}";
+
+            var deserializeSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                ContractResolver = new ReadOnlyJsonContractResolver()
+            };
+            deserializeSettings.Converters.Add(new ResourceJsonConverter());
+            deserializeSettings.Converters.Add(new PolymorphicDeserializeJsonConverter<Product>("dType"));
+            var deserializedProduct = JsonConvert.DeserializeObject<PageCollection<Product>>(responseBody, deserializeSettings);
+
+            Assert.Equal(1, deserializedProduct.Items.Count);
+            Assert.Equal("https://ODataLink.vault.azure.net:443/keys?api-version=2015-06-01", deserializedProduct.NextPage);
+        }
+    }
+}
---
+++ ClientRuntimes/CSharp/ClientRuntime.Azure/PageCollection.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure
+{
+    /// <summary>
+    /// Defines a page in Azure responses.
+    /// </summary>
+    /// <typeparam name="T">Type of the page content items</typeparam>
+    [JsonObject]
+    public class PageCollection<T> : IEnumerable<T>
+    {
+        [JsonProperty("nextLink")]
+        private string NextLink { get; set; }
+
+        [JsonProperty("@odata.nextLink")]
+        private string NextOdataLink { get; set; }
+
+        /// <summary>
+        /// Gets the page items collection.
+        /// </summary>
+        [JsonProperty("value")]
+        public IList<T> Items { get; private set; }
+
+        /// <summary>
+        /// Gets the link to the next page.
+        /// </summary>
+        public string NextPage
+        {
+            get
+            {
+                if(NextLink != null)
+                {
+                    return NextLink;
+                }
+
+                return NextOdataLink;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>A an enumerator that can be used to iterate through the collection.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>A an enumerator that can be used to iterate through the collection.</returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
