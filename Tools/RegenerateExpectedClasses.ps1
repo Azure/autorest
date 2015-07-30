7b408ea093280deb87704ba6279687867b41597e
Amar Zavery <amzavery@microsoft.com>
2015-07-30 11:04:35 -0700

    Removed RegenerateExpectedClasses.ps1 files as gulp will be used to generate code

--- Tools/RegenerateExpectedClasses.ps1
+++
@@ -1,8 +0,0 @@
-Write-Output ">>>>>>>>> Generating CSharp clients"
-powershell -File ..\AutoRest\Generators\CSharp\CSharp.Tests\RegenerateExpectedClasses.ps1
-Write-Output ">>>>>>>>> Generating Azure CSharp clients"
-powershell -File ..\AutoRest\Generators\CSharp\Azure.CSharp.Tests\RegenerateExpectedClasses.ps1
-Write-Output ">>>>>>>>> Generating Node.js clients"
-powershell -File ..\AutoRest\Generators\NodeJS\NodeJS.Tests\RegenerateExpectedClasses.ps1
-Write-Output ">>>>>>>>> Generating Azure Node.js clients"
-powershell -File ..\AutoRest\Generators\NodeJS\Azure.NodeJS.Tests\RegenerateExpectedClasses.ps1
