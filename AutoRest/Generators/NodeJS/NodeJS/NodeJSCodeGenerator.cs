6b07880cce81cabeeab914c0f9cd4696173c05b9
stankovski <denis.stankovski@gmail.com>
2015-08-04 16:24:17 -0700

    Updated version of NodeJS client runtime dependency

--- AutoRest/Generators/NodeJS/NodeJS/NodeJSCodeGenerator.cs
+++ AutoRest/Generators/NodeJS/NodeJS/NodeJSCodeGenerator.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Rest.Generator.NodeJS
 {
     public class NodeJSCodeGenerator : CodeGenerator
     {
-        private const string ClientRuntimePackage = "ms-rest version 1.0.0";
+        private const string ClientRuntimePackage = "ms-rest version 1.1.0";
 
         public NodeJsCodeNamer Namer { get; private set; }
 
