2ee045af2b57db1d91f5fba9d2c405cb10019a3e
Jianghao Lu <ljhljh235@users.noreply.github.com>
2015-08-06 18:30:19 -0700

    Bootstrap Java Generator

---
+++ AutoRest/Generators/Java/Java/JavaCodeGenerator.cs
@@ -0,0 +1,114 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Rest.Generator.ClientModel;
+using Microsoft.Rest.Generator.Java.Properties;
+using Microsoft.Rest.Generator.Java.Templates;
+using Microsoft.Rest.Generator.Utilities;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace Microsoft.Rest.Generator.Java
+{
+    public class JavaCodeGenerator : CodeGenerator
+    {
+        private const string ClientRuntimePackage = "ms-rest version 1.1.0";
+
+        public JavaCodeNamer Namer { get; private set; }
+
+        public JavaCodeGenerator(Settings settings) : base(settings)
+        {
+            Namer = new JavaCodeNamer();
+        }
+
+        public override string Name
+        {
+            get { return "Java"; }
+        }
+
+        public override string Description
+        {
+            // TODO resource string.
+            get { return "Java for Http Client Libraries"; }
+        }
+
+        public override string UsageInstructions
+        {
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture,
+                    Resources.UsageInformation, ClientRuntimePackage);
+            }
+        }
+
+        public override string ImplementationFileExtension
+        {
+            get { return ".js"; }
+        }
+
+        /// <summary>
+        /// Normalizes client model by updating names and types to be language specific.
+        /// </summary>
+        /// <param name="serviceClient"></param>
+        public override void NormalizeClientModel(ServiceClient serviceClient)
+        {
+            Namer.NormalizeClientModel(serviceClient);
+            Namer.ResolveNameCollisions(serviceClient, Settings.Namespace,
+                Settings.Namespace + ".Models");
+        }
+
+        /// <summary>
+        /// Generate NodeJS client code for given ServiceClient.
+        /// </summary>
+        /// <param name="serviceClient"></param>
+        /// <returns></returns>
+        public override async Task Generate(ServiceClient serviceClient)
+        {
+            var serviceClientTemplateModel = new ServiceClientTemplateModel(serviceClient);
+            // Service client
+            var serviceClientTemplate = new ServiceClientTemplate
+            {
+                Model = serviceClientTemplateModel,
+            };
+            await Write(serviceClientTemplate, serviceClient.Name.ToCamelCase() + ".js");
+
+            //Models
+            if (serviceClient.ModelTypes.Any())
+            {
+                var modelIndexTemplate = new ModelIndexTemplate
+                {
+                    Model = serviceClientTemplateModel
+                };
+                await Write(modelIndexTemplate, Path.Combine("models", "index.js"));
+                foreach (var modelType in serviceClientTemplateModel.ModelTemplateModels)
+                {
+                    var modelTemplate = new ModelTemplate
+                    {
+                        Model = modelType
+                    };
+                    await Write(modelTemplate, Path.Combine("models", modelType.Name.ToCamelCase() + ".js"));
+                }
+            }
+
+            //MethodGroups
+            if (serviceClientTemplateModel.MethodGroupModels.Any())
+            {
+                var methodGroupIndexTemplate = new MethodGroupIndexTemplate
+                {
+                    Model = serviceClientTemplateModel
+                };
+                await Write(methodGroupIndexTemplate, Path.Combine("operations", "index.js"));
+                foreach (var methodGroupModel in serviceClientTemplateModel.MethodGroupModels)
+                {
+                    var methodGroupTemplate = new MethodGroupTemplate
+                    {
+                        Model = methodGroupModel
+                    };
+                    await Write(methodGroupTemplate, Path.Combine("operations", methodGroupModel.MethodGroupType.ToCamelCase() + ".js"));
+                }
+            }
+        }
+    }
+}
---
+++ AutoRest/Generators/Java/Java/JavaCodeNamer.cs
@@ -0,0 +1,288 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Rest.Generator.ClientModel;
+using Microsoft.Rest.Generator.Java.TemplateModels;
+using Microsoft.Rest.Generator.Utilities;
+using System.Globalization;
+
+namespace Microsoft.Rest.Generator.Java
+{
+    public class JavaCodeNamer : CodeNamer
+    {
+        private readonly HashSet<IType> _normalizedTypes;
+
+        /// <summary>
+        /// Initializes a new instance of CSharpCodeNamingFramework.
+        /// </summary>
+        public JavaCodeNamer()
+        {
+            // List retrieved from 
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords
+            new HashSet<string>
+            {
+                "array",
+                "await",
+                "abstract",
+                "boolean",
+                "buffer",
+                "break",
+                "byte",
+                "case",
+                "catch",
+                "char",
+                "class",
+                "const",
+                "continue",
+                "debugger",
+                "default",
+                "delete",
+                "do",
+                "double",
+                "date",
+                "else",
+                "enum",
+                "error",
+                "export",
+                "extends",
+                "false",
+                "final",
+                "finally",
+                "float",
+                "for",
+                "function",
+                "goto",
+                "if",
+                "implements",
+                "import",
+                "in",
+                "int",
+                "interface",
+                "instanceof",
+                "let",
+                "long",
+                "native",
+                "new",
+                "null",
+                "package",
+                "private",
+                "protected",
+                "public",
+                "return",
+                "short",
+                "static",
+                "super",
+                "switch",
+                "synchronized",
+                "this",
+                "throw",
+                "transient",
+                "true",
+                "try",
+                "typeof",
+                "util",
+                "var",
+                "void",
+                "volatile",
+                "while",
+                "with",
+                "yield"
+            }.ForEach(s => ReservedWords.Add(s));
+
+            _normalizedTypes = new HashSet<IType>();
+        }
+
+        public override string GetFieldName(string name)
+        {
+            return CamelCase(name);
+        }
+
+        public override string GetPropertyName(string name)
+        {
+            return CamelCase(name);
+        }
+
+        public override string GetMethodName(string name)
+        {
+            name = GetEscapedReservedName(name, "Method");
+            return CamelCase(name);
+        }
+
+        public override string GetEnumMemberName(string name)
+        {
+            return CamelCase(name);
+        }
+
+        public override string GetParameterName(string name)
+        {
+            return base.GetParameterName(GetEscapedReservedName(name, "Parameter"));
+        }
+
+        public override string GetVariableName(string name)
+        {
+            return base.GetVariableName(GetEscapedReservedName(name, "Variable"));
+        }
+
+        public override void NormalizeClientModel(ServiceClient client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+
+            base.NormalizeClientModel(client);
+            foreach (var method in client.Methods)
+            {
+                if (method.Group != null)
+                {
+                    method.Group = method.Group.ToCamelCase();
+                }
+                var scope = new ScopeProvider();
+                foreach (var parameter in method.Parameters)
+                {
+                    if (parameter.ClientProperty != null)
+                    {
+                        parameter.Name = string.Format(CultureInfo.InvariantCulture,
+                            "{0}.{1}",
+                            method.Group == null ? "this" : "this.client",
+                            parameter.ClientProperty.Name);
+                    }
+                    else
+                    {
+                        parameter.Name = scope.GetVariableName(parameter.Name);
+                    }
+                }
+            }
+        }
+
+        protected override IType NormalizeType(IType type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+            var enumType = type as EnumType;
+            if (enumType != null && enumType.IsExpandable)
+            {
+                type = PrimaryType.String;
+            }
+
+            // Using Any instead of Contains since object hash is bound to a property which is modified during normalization
+            if (_normalizedTypes.Any(item => type.Equals(item)))
+            {
+                return _normalizedTypes.First(item => type.Equals(item));
+            }
+
+            _normalizedTypes.Add(type);
+            if (type is PrimaryType)
+            {
+                return NormalizePrimaryType(type as PrimaryType);
+            }
+            if (type is SequenceType)
+            {
+                return NormalizeSequenceType(type as SequenceType);
+            }
+            if (type is DictionaryType)
+            {
+                return NormalizeDictionaryType(type as DictionaryType);
+            }
+            if (type is CompositeType)
+            {
+                return NormalizeCompositeType(type as CompositeType);
+            }
+            if (type is EnumType)
+            {
+                return NormalizeEnumType(type as EnumType);
+            }
+
+
+            throw new NotSupportedException(string.Format(CultureInfo.InvariantCulture, 
+                "Type {0} is not supported.", type.GetType()));
+        }
+
+        private static IType NormalizeEnumType(EnumType enumType)
+        {
+            return enumType;
+        }
+
+        private IType NormalizeCompositeType(CompositeType compositeType)
+        {
+            compositeType.Name = GetTypeName(compositeType.Name);
+
+            foreach (var property in compositeType.Properties)
+            {
+                property.Name = GetPropertyName(property.Name);
+                property.Type = NormalizeType(property.Type);
+            }
+
+            return compositeType;
+        }
+
+        private static IType NormalizePrimaryType(PrimaryType primaryType)
+        {
+            if (primaryType == PrimaryType.Boolean)
+            {
+                primaryType.Name = "Boolean";
+            }
+            else if (primaryType == PrimaryType.ByteArray)
+            {
+                primaryType.Name = "Buffer";
+            }
+            else if (primaryType == PrimaryType.Date)
+            {
+                primaryType.Name = "Date";
+            }
+            else if (primaryType == PrimaryType.DateTime)
+            {
+                primaryType.Name = "Date";
+            }
+            else if (primaryType == PrimaryType.Double)
+            {
+                primaryType.Name = "Number";
+            }
+            else if (primaryType == PrimaryType.Int)
+            {
+                primaryType.Name = "Number";
+            }
+            else if (primaryType == PrimaryType.Long)
+            {
+                primaryType.Name = "Number";
+            }
+            else if (primaryType == PrimaryType.Stream)
+            {
+                primaryType.Name = "Object";
+            }
+            else if (primaryType == PrimaryType.String)
+            {
+                primaryType.Name = "String";
+            }
+            else if (primaryType == PrimaryType.TimeSpan)
+            {
+                primaryType.Name = "TimeSpan";
+            }
+            else if (primaryType == PrimaryType.Object)
+            {
+                primaryType.Name = "Object";
+            }
+
+            return primaryType;
+        }
+
+        private IType NormalizeSequenceType(SequenceType sequenceType)
+        {
+            sequenceType.ElementType = NormalizeType(sequenceType.ElementType);
+            sequenceType.NameFormat = "Array";
+            return sequenceType;
+        }
+
+        private IType NormalizeDictionaryType(DictionaryType dictionaryType)
+        {
+            dictionaryType.ValueType = NormalizeType(dictionaryType.ValueType);
+            dictionaryType.NameFormat = "Object";
+            return dictionaryType;
+        }
+    }
+}
