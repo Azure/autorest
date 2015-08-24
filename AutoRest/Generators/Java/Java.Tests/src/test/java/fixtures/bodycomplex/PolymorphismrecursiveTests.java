f6eebce355bde8772a16ba161f598987a2735a1b
Jianghao Lu <ljhljh235@users.noreply.github.com>
2015-08-24 16:21:05 -0700

    Support polymorphism and recursive

--- AutoRest/Generators/Java/Java.Tests/src/test/java/fixtures/bodycomplex/PolymorphismrecursiveTests.java
+++ AutoRest/Generators/Java/Java.Tests/src/test/java/fixtures/bodycomplex/PolymorphismrecursiveTests.java
@@ -22,19 +22,14 @@ public class PolymorphismrecursiveTests {
 
     @Test
     public void getValid() throws Exception {
-        Fish result = client.getPolymorphism().getValid();
-        Assert.assertEquals(Salmon.class, result.getClass());
+        Fish result = client.getPolymorphicrecursive().getValid();
         Salmon salmon = (Salmon)result;
-        Assert.assertEquals("alaska", salmon.getLocation());
-        Assert.assertEquals(1.0, salmon.getLength(), 0f);
-        Assert.assertEquals(Shark.class, salmon.getSiblings().get(0).getClass());
         Shark sib1 = (Shark)(salmon.getSiblings().get(0));
-        Assert.assertEquals(new DateTime(2012, 1, 5, 1, 0, 0, DateTimeZone.UTC), sib1.getBirthday());
-        Assert.assertEquals(Sawshark.class, salmon.getSiblings().get(1).getClass());
-        Sawshark sib2 = (Sawshark)(salmon.getSiblings().get(1));
-        Assert.assertArrayEquals(
-                new byte[]{(byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 254},
-                sib2.getPicture());
+        Salmon sib2 = (Salmon)(sib1.getSiblings().get(0));
+        Shark sib3 = (Shark)(sib2.getSiblings().get(0));
+        Assert.assertEquals(
+                new DateTime(2012, 1, 5, 1, 0, 0, DateTimeZone.UTC),
+                sib3.getBirthday());
     }
 
     @Test
@@ -51,6 +46,7 @@ public class PolymorphismrecursiveTests {
         sib1.setBirthday(new DateTime(2012, 1, 5, 1, 0, 0, DateTimeZone.UTC));
         sib1.setLength(20.0);
         sib1.setSpecies("predator");
+        sib1.setSiblings(new ArrayList<Fish>());
         body.getSiblings().add(sib1);
 
         Sawshark sib2 = new Sawshark();
@@ -59,8 +55,33 @@ public class PolymorphismrecursiveTests {
         sib2.setLength(10.0);
         sib2.setPicture(new byte[] {(byte)255, (byte)255, (byte)255, (byte)255, (byte)254});
         sib2.setSpecies("dangerous");
+        sib2.setSiblings(new ArrayList<Fish>());
         body.getSiblings().add(sib2);
 
-        client.getPolymorphism().putValid(body);
+        Salmon sib11 = new Salmon();
+        sib11.setIswild(true);
+        sib11.setLocation("atlantic");
+        sib11.setSpecies("coho");
+        sib11.setLength(2);
+        sib11.setSiblings(new ArrayList<Fish>());
+        sib1.getSiblings().add(sib11);
+        sib1.getSiblings().add(sib2);
+
+        Shark sib111 = new Shark();
+        sib111.setAge(6);
+        sib111.setBirthday(new DateTime(2012, 1, 5, 1, 0, 0, DateTimeZone.UTC));
+        sib111.setSpecies("predator");
+        sib111.setLength(20);
+        sib11.getSiblings().add(sib111);
+
+        Sawshark sib112 = new Sawshark();
+        sib112.setAge(105);
+        sib112.setBirthday(new DateTime(1900, 1, 5, 1, 0, 0, DateTimeZone.UTC));
+        sib112.setLength(10.0);
+        sib112.setPicture(new byte[] {(byte)255, (byte)255, (byte)255, (byte)255, (byte)254});
+        sib112.setSpecies("dangerous");
+        sib11.getSiblings().add(sib112);
+
+        client.getPolymorphicrecursive().putValid(body);
     }
 }
