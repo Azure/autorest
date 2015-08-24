41e65bfcb6c411113865f9d23772a0d4a051f7a6
David Justice <david@devigned.com>
2015-08-24 12:28:24 -0700

    Base64 require should be base64

--- ClientRuntimes/Ruby/ms-rest/lib/ms_rest.rb
+++ ClientRuntimes/Ruby/ms-rest/lib/ms_rest.rb
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-require 'Base64'
+require 'base64'
 require 'openssl'
 require 'net/http'
 require 'ms_rest/version'
