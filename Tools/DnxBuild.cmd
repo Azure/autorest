ebea6058b0f7d88d6cca10c34c01efbee482c327
stankovski <denis.stankovski@gmail.com>
2015-10-12 15:38:00 -0700

    Fixed compilation under DNX

--- Tools/DnxBuild.cmd
+++ Tools/DnxBuild.cmd
@@ -1,14 +1,14 @@
 pushd %~dp0
 cd ..\ClientRuntimes\CSharp\ClientRuntime\Microsoft.Rest.ClientRuntime
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu restore
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu build --configuration release
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu pack
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu restore
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu build --configuration debug
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu pack
 copy bin\Release\*.nupkg c:\nuget
 
 cd ..\ClientRuntimes\CSharp\ClientRuntime.Azure\Microsoft.Rest.ClientRuntime.Azure
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu restore
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu build --configuration release
-call dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu pack
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu restore
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu build --configuration debug
+call ..\..\..\..\Tools\dnx-coreclr-win-x86.1.0.0-beta7\bin\dnu pack
 copy bin\Release\*.nupkg c:\nuget
 popd
 
