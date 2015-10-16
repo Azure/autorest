68759b525242db89dc7e1b979b219fd093f0701e
annatisch <antisch@microsoft.com>
2015-10-16 09:43:20 -0700

    Removed manager attribute validation

--- ClientRuntimes/Python/sample/pool_manager.py
+++ ClientRuntimes/Python/sample/pool_manager.py
@@ -41,84 +41,8 @@ class PoolManager(object):
     def add(self, pool):
 
         # Validate
-        if pool:
-            if pool.auto_scale_run:
-                if pool.auto_scale_run.error:
-                    if pool.auto_scale_run.error.values:
-
-                        for i in pool.auto_scale_run.error.values:
-                            if not i.name:
-                                raise ValueError('pool.auto_scale_run.error.values.name cannot be None.')
-                            
-                            if not i.value:
-                                raise ValueError('pool.auto_scale_run.error.values.value cannot be None.')
-                        
-                if not pool.auto_scale_run.results:
-                    raise ValueError('pool.auto_scale_run.results cannot be None.')
-                
-                if not pool.auto_scale_run.timestamp:
-                    raise ValueError('pool.auto_scale_run.timestamp cannot be None.')
-                
-            if pool.certificate_references:
-                for i in pool.certificate_references:
-                    if not i.store_location:
-                        raise ValueError('pool.certificate_references.store_location cannot be None.')
-                    
-                    if not i.store_name:
-                        raise ValueError('pool.certificate_references.store_name cannot be None.')
-                    
-                    if not i.thumbprint:
-                        raise ValueError('pool.certificate_references.thumbprint cannot be None.')
-                    
-                    if not i.thumbprint_algorithm:
-                        raise ValueError('pool.certificate_references.thumbprint_algorithm cannot be None.')
-                    
-                    if not i.visibility:
-                        raise ValueError('pool.certificate_references.visibility cannot be None.')
-                
-            if pool.metadata:
-                for i in pool.metadata:
-                    if not i.name:
-                        raise ValueError('pool.metadata.name cannot be None.')
-                
-            if not pool.name:
-                raise ValueError('pool.name cannot be None.')
-            
-            if pool.resize_error:
-                if pool.resize_error.values:
-                    for i in pool.resize_error.values:
-                        if not i.name:
-                            raise ValueError('pool.resize_error.values.name cannot be None.')
-                        
-                        if not i.value:
-                            raise ValueError('pool.resize_error.values.value cannot be None.')
-                    
-            if pool.scheduling_policy:
-                if not pool.scheduling_policy.tvm_fill_type:
-                    raise ValueError('pool.scheduling_policy.tvm_fill_type cannot be None.')
-                
-            if pool.start_task:
-                if not pool.start_task.command_line:
-                    raise ValueError('pool.start_task.command_line cannot be None.')
-                
-                if not pool.start_task.environment_settings:
-                    raise ValueError('pool.start_task.environment_settings cannot be None.')
-                
-                if pool.start_task.environment_settings:
-                    for i in pool.start_task.environment_settings:
-                        if not i.name:
-                            raise ValueError('pool.start_task.environment_settings.name cannot be None.')
-                    
-                if (parameters.pool.start_task.resource_files is None):
-                    raise ValueError('pool.start_task.resource_files cannot be None.')
-                
-                if pool.start_task.resource_files:
-                    for i in pool.start_task.resource_files:
-                        if not i.blob_source:
-                            raise ValueError('pool.start_task.resource_files.blob_source cannot be None.')
-                        
-                        if not i.file_path:
-                            raise ValueError('pool.start_task.resource_files.file_path cannot be None.')
+        if not pool:
+            raise ValueError("pool cannot be None")
         
         content = Serialized(pool)
 
@@ -190,9 +114,6 @@ class PoolManager(object):
         
         if not parameters:
             raise ValueError('parameters cannot be None.')
-        
-        if not parameters.auto_scale_formula:
-            raise ValueError('parameters.auto_scale_formula cannot be None.')
 
         content = Serialized(parameters)
 
@@ -218,9 +139,6 @@ class PoolManager(object):
         
         if (parameters is None):
             raise ValueError('parameters cannot be None.')
-        
-        if (parameters.auto_scale_formula is None):
-            raise ValueError('parameters.auto_scale_formula cannot be None.')
 
         content = Serialized(parameters)
 
@@ -241,7 +159,7 @@ class PoolManager(object):
     def get(self, pool_name=None):
 
         # Validate
-        if (pool_name is None):
+        if not pool_name:
             raise ValueError('pool_name cannot be None.')
 
         try:
@@ -290,52 +208,6 @@ class PoolManager(object):
         
         if not parameters:
             raise ValueError('parameters cannot be None.')
-        
-        if parameters.certificate_references:
-            for i in parameters.certificate_references:
-                if not i.store_location:
-                    raise ValueError('parameters.certificate_references.store_location cannot be None.')
-                
-                if not i.store_name:
-                    raise ValueError('parameters.certificate_references.store_name cannot be None.')
-                
-                if not i.thumbprint:
-                    raise ValueError('parameters.certificate_references.thumbprint cannot be None.')
-                
-                if not i.thumbprint_algorithm:
-                    raise ValueError('parameters.certificate_references.thumbprint_algorithm cannot be None.')
-                
-                if not i.visibility:
-                    raise ValueError('parameters.certificate_references.visibility cannot be None.')
-            
-        if parameters.metadata:
-            for i in parameters.metadata:
-                if not i.name:
-                    raise ValueError('parameters.metadata.name cannot be None.')
-            
-        if parameters.start_task:
-            if not parameters.start_task.command_line:
-                raise ValueError('parameters.start_task.command_line cannot be None.')
-            
-            if parameters.start_task.environment_settings is None:
-                raise ValueError('parameters.start_task.environment_settings cannot be None.')
-            
-            else:
-                for i in parameters.start_task.environment_settings:
-                    if not i.name:
-                        raise ValueError('parameters.start_task.environment_settings.name cannot be None.')
-                    
-
-            if parameters.start_task.resource_files is None:
-                raise ValueError('parameters.start_task.resource_files cannot be None.')
-            
-            else:
-                for i in parameters.start_task.resource_files:
-                    if not i.blob_source:
-                        raise ValueError('parameters.start_task.resource_files.blob_source cannot be None.')
-                    
-                    if not i.file_path:
-                        raise ValueError('parameters.start_task.resource_files.file_path cannot be None.')
 
         content = Serialized(parameters)
 
@@ -406,51 +278,6 @@ class PoolManager(object):
 
         if not properties:
             raise ValueError('properties cannot be None.')
-        
-        if parameters.certificate_references:
-            for i in parameters.certificate_references:
-                if not i.store_location:
-                    raise ValueError('parameters.certificate_references.store_location cannot be None.')
-                
-                if not i.store_name:
-                    raise ValueError('parameters.certificate_references.store_name cannot be None.')
-                
-                if not i.thumbprint:
-                    raise ValueError('parameters.certificate_references.thumbprint cannot be None.')
-                
-                if not i.thumbprint_algorithm:
-                    raise ValueError('parameters.certificate_references.thumbprint_algorithm cannot be None.')
-                
-                if not i.visibility:
-                    raise ValueError('parameters.certificate_references.visibility cannot be None.')
-            
-        if parameters.metadata:
-            for i in parameters.metadata:
-                if not i.name:
-                    raise ValueError('parameters.metadata.name cannot be None.')
-            
-        if parameters.start_task:
-            if not parameters.start_task.command_line:
-                raise ValueError('parameters.start_task.command_line cannot be None.')
-            
-            if parameters.start_task.environment_settings is None:
-                raise ValueError('parameters.start_task.environment_settings cannot be None.')
-            
-            else:
-                for i in parameters.start_task.environment_settings:
-                    if not i.name:
-                        raise ValueError('parameters.start_task.environment_settings.name cannot be None.')
-                
-            if parameters.start_task.resource_files is None:
-                raise ValueError('parameters.start_task.resource_files cannot be None.')
-            
-            else:
-                for i in parameters.start_task.resource_files:
-                    if not i.blob_source:
-                        raise ValueError('parameters.start_task.resource_files.blob_source cannot be None.')
-                    
-                    if not i.file_path:
-                        raise ValueError('parameters.start_task.resource_files.file_path cannot be None.')
                     
         content = Serialized(properties)
 
@@ -476,9 +303,6 @@ class PoolManager(object):
         
         if not parameters:
             raise ValueError('parameters cannot be None.')
-        
-        if not parameters.target_os_version:
-            raise ValueError('parameters.target_os_version cannot be None.')
 
         content = Serialized(parameters)
 
