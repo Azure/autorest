f9164b524a74ff9518e5826b4d04dadd165dd3e1
stankovski <denis.stankovski@gmail.com>
2015-10-21 15:21:03 -0700

    Bumped up the version of Microsoft.Rest.ClientRuntime.Azure due to a breaking change in Resources

--- AutoRest/Generators/CSharp/Azure.CSharp/AzureCSharpCodeGenerator.cs
+++ AutoRest/Generators/CSharp/Azure.CSharp/AzureCSharpCodeGenerator.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Rest.Generator.CSharp.Azure
     {
         private readonly AzureCSharpCodeNamer _namer;
 
-        private const string ClientRuntimePackage = "Microsoft.Rest.ClientRuntime.Azure.1.1.1";
+        private const string ClientRuntimePackage = "Microsoft.Rest.ClientRuntime.Azure.2.0.0";
 
         public AzureCSharpCodeGenerator(Settings settings) : base(settings)
         {
