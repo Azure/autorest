4c9316e2aeb1ff71ca35ff7ab9e93a26a507ffbe
Jianghao Lu <jianghaolu@users.noreply.github.com>
2015-10-29 16:43:01 -0700

    Add in-line comments for CustomHeaderInterceptor

--- ClientRuntimes/Java/azure-client-runtime/src/main/java/com/microsoft/rest/CustomHeaderInterceptor.java
+++ ClientRuntimes/Java/azure-client-runtime/src/main/java/com/microsoft/rest/CustomHeaderInterceptor.java
@@ -19,35 +19,48 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * User agent interceptor for putting a 'User-Agent' header in the request.
+ * An instance of this class enables adding custom headers in client requests
+ * when added to the {@link com.squareup.okhttp.OkHttpClient} interceptors.
  */
 public class CustomHeaderInterceptor implements Interceptor {
     private Map<String, List<String>> headers;
 
     /**
-     * Initialize an instance of {@link UserAgentInterceptor} class with the default
-     * 'User-Agent' header.
+     * Initialize an instance of {@link CustomHeaderInterceptor} class.
      */
     public CustomHeaderInterceptor() {
         headers = new HashMap<>();
     }
 
+    /**
+     * Add a single header key-value pair.
+     *
+     * @param name the name of the header.
+     * @param value the value of the header.
+     * @return the interceptor instance itself.
+     */
     public CustomHeaderInterceptor addHeader(String name, String value) {
         this.headers.put(name, Collections.singletonList(value));
         return this;
     }
 
     /**
-     * Initialize an instance of {@link UserAgentInterceptor} class with the specified
-     * 'User-Agent' header.
+     * Add all headers in a {@link Headers} object.
      *
-     * @param headers the 'User-Agent' header value.
+     * @param headers an OkHttp {@link Headers} object.
+     * @return the interceptor instance itself.
      */
     public CustomHeaderInterceptor addHeaders(Headers headers) {
         this.headers.putAll(headers.toMultimap());
         return this;
     }
 
+    /**
+     * Add all headers in a header map.
+     *
+     * @param headers a map of headers.
+     * @return the interceptor instance itself.
+     */
     public CustomHeaderInterceptor addHeaderMap(Map<String, String> headers) {
         for (Map.Entry<String, String> header : headers.entrySet()) {
             this.headers.put(header.getKey(), Collections.singletonList(header.getValue()));
@@ -55,8 +68,25 @@ public class CustomHeaderInterceptor implements Interceptor {
         return this;
     }
 
+    /**
+     * Add all headers in a header multimap.
+     *
+     * @param headers a multimap of headers.
+     * @return the interceptor instance itself.
+     */
     public CustomHeaderInterceptor addHeaderMultimap(Map<String, List<String>> headers) {
         this.headers.putAll(headers);
+        return this;
+    }
+
+    /**
+     * Remove a header.
+     *
+     * @param name the name of the header to remove.
+     * @return the interceptor instance itself.
+     */
+    public CustomHeaderInterceptor removeHeader(String name) {
+        this.headers.remove(name);
         return this;
     }
 
