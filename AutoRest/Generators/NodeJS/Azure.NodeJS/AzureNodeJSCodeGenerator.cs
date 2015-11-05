eee54a18c87c539e550f107d0da5bad70406abcf
stankovski <denis.stankovski@gmail.com>
2015-11-05 11:28:46 -0800

    Fixed odata generator

--- AutoRest/Generators/NodeJS/Azure.NodeJS/AzureNodeJSCodeGenerator.cs
+++ AutoRest/Generators/NodeJS/Azure.NodeJS/AzureNodeJSCodeGenerator.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Rest.Generator.Azure.NodeJS
 {
     public class AzureNodeJSCodeGenerator : NodeJSCodeGenerator
     {
-        private const string ClientRuntimePackage = "ms-rest-azure version 1.0.0";
+        private const string ClientRuntimePackage = "ms-rest-azure version 1.1.0";
         public const string LongRunningExtension = "x-ms-long-running-operation";
 
         // List of models with paging extensions.
