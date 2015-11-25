46f74cade6075e01c7002ab13b21e7fac9506e4d
annatisch <antisch@microsoft.com>
2015-11-25 10:18:00 -0800

    Started reworking AAD config

--- ClientRuntimes/Python/msrestazure/msrestazure/azure_configuration.py
+++ ClientRuntimes/Python/msrestazure/msrestazure/azure_configuration.py
@@ -41,24 +41,18 @@ class AzureConfiguration(Configuration):
 
         super(AzureConfiguration, self).__init__(base_url, filepath)
 
-        # Authentication
-        self.auth_endpoint = "//login.microsoftonline.com"
-        self.token_uri = "/oauth2/token"
-        self.auth_uri = "/oauth2/authorize"
-        self.tenant = "common"
-        self.resource = 'https://management.core.windows.net/'
-        self.keyring = "AzureAAD"
+
         self.long_running_operation_timeout = 30
 
     def save(self, filepath):
 
-        self._config.add_section("Authentication")
-        self._config.set("Authentication", "auth_endpoint", self.auth_endpoint)
-        self._config.set("Authentication", "token_uri", self.token_uri)
-        self._config.set("Authentication", "auth_uri", self.auth_uri)
-        self._config.set("Authentication", "tenant", self.tenant)
-        self._config.set("Authentication", "resource", self.resource)
-        self._config.set("Authentication", "keyring", self.keyring)
+        #self._config.add_section("Authentication")
+        #self._config.set("Authentication", "auth_endpoint", self.auth_endpoint)
+        #self._config.set("Authentication", "token_uri", self.token_uri)
+        #self._config.set("Authentication", "auth_uri", self.auth_uri)
+        #self._config.set("Authentication", "tenant", self.tenant)
+        #self._config.set("Authentication", "resource", self.resource)
+        #self._config.set("Authentication", "keyring", self.keyring)
 
         self._config.add_section("Azure")
         self._config.set("Azure", "long_running_operation_timeout", self.long_running_operation_timeout)
@@ -69,12 +63,12 @@ class AzureConfiguration(Configuration):
 
         try:
             self._config.read(filepath)
-            self.auth_endpoint = self._config.get("Authentication", "auth_endpoint")
-            self.token_uri = self._config.get("Authentication", "token_uri")
-            self.auth_uri = self._config.get("Authentication", "auth_uri")
-            self.tenant = self._config.set("Authentication", "tenant")
-            self.resource = self._config.set("Authentication", "resource")
-            self.keyring = self._config.set("Authentication", "keyring")
+            #self.auth_endpoint = self._config.get("Authentication", "auth_endpoint")
+            #self.token_uri = self._config.get("Authentication", "token_uri")
+            #self.auth_uri = self._config.get("Authentication", "auth_uri")
+            #self.tenant = self._config.set("Authentication", "tenant")
+            #self.resource = self._config.set("Authentication", "resource")
+            #self.keyring = self._config.set("Authentication", "keyring")
             self.long_running_operation_timeout = self._config.getint("Azure", "long_running_operation_timeout")
 
         except (ValueError, EnvironmentError, NoOptionError):
