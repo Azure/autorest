3e944f1f21f399dc26907da0b81edbe0a92ff977
stankovski <denis.stankovski@gmail.com>
2015-12-29 13:32:59 -0800

    Updated schema and removed one layer from x-ms-request-id and x-ms-client-request-id extensions

--- AutoRest/Generators/Extensions/Azure.Extensions/AzureExtensions.cs
+++ AutoRest/Generators/Extensions/Azure.Extensions/AzureExtensions.cs
@@ -503,10 +503,10 @@ namespace Microsoft.Rest.Generator.Azure
             {
                 if (parameter.Extensions.ContainsKey(ClientRequestIdExtension))
                 {
-                    Newtonsoft.Json.Linq.JContainer extensionObject = parameter.Extensions[ClientRequestIdExtension] as Newtonsoft.Json.Linq.JContainer;
+                    JToken extensionObject = parameter.Extensions[ClientRequestIdExtension] as JToken;
                     if (extensionObject != null)
                     {
-                        bool useParamAsClientRequestId = (bool)extensionObject["value"];
+                        bool useParamAsClientRequestId = (bool)extensionObject;
                         if (useParamAsClientRequestId)
                         {
                             //TODO: Need to do something if they specify two ClientRequestIdExtensions for the same method...?
@@ -530,10 +530,10 @@ namespace Microsoft.Rest.Generator.Azure
             string requestIdName = "x-ms-request-id";
             if (method.Extensions.ContainsKey(RequestIdExtension))
             {
-                Newtonsoft.Json.Linq.JContainer extensionObject = method.Extensions[RequestIdExtension] as Newtonsoft.Json.Linq.JContainer;
+                JToken extensionObject = method.Extensions[RequestIdExtension] as JToken;
                 if (extensionObject != null)
                 {
-                    requestIdName = extensionObject["value"].ToString();
+                    requestIdName = (string)extensionObject;
                 }
             }
             
