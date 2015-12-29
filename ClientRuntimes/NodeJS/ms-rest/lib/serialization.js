5b4e928e0da418efab221720210b48e92f5b416e
Amar Zavery <amzavery@microsoft.com>
2015-12-28 22:00:29 -0800

    serialization in ms-rest

--- ClientRuntimes/NodeJS/ms-rest/lib/serialization.js
+++ ClientRuntimes/NodeJS/ms-rest/lib/serialization.js
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+'use strict';
+
+var util = require('util');
+var moment = require('moment');
 /**
  * Serializes the JSON Object. It serializes Buffer object to a 
  * 'base64' encoded string and a Date Object to a string 
@@ -63,5 +67,284 @@ exports.isValidISODateTime = function (dateString) {
   var re = /^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])?(T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9]))?$/i;
   return re.test(dateString);
 };
+
+//TODO: Check for stream
+exports.serializeInit = function (client, mapper, object) {
+  var payload = {};
+  if (requiresFlattening(mapper, object)) payload.properties = {};
+  for (var key in mapper) {
+    if (key.required) {
+      if (object[key] === null || object[key] === undefined) {
+        throw new Error(util.format('%s cannot be null or undefined.', key));
+      }
+    }
+    
+    if (object[key] !== null && object[key] !== undefined) {
+      if (mapper[key].type.name === 'Number') {
+        if (typeof object[key] !== 'number') {
+          throw new Error(util.format('%s must be of type number.', key));
+        }
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'Boolean') {
+        if (typeof object[key] !== 'boolean') {
+          throw new Error(util.format('%s must be of type boolean.', key));
+        }
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'String') {
+        if (typeof object[key].valueOf() !== 'string') {
+          throw new Error(util.format('%s must be of type string.', key));
+        }
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'Enum') {
+        if (!mapper[key].type.allowedValues.some(function (item) { return item === object[key]; })) {
+          throw new Error(util.format('%s  is not a valid value. The valid values are: %s', 
+            object[key], JSON.stringify(mapper[key].type.allowedValues)));
+        }
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'ByteArray') {
+        if (!Buffer.isBuffer(object[key])) {
+          throw new Error(util.format('%s must be of type Buffer.', key));
+        }
+        object[key] = object[key].toString('base64');
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'Date') {
+        if (!(object[key] instanceof Date || 
+          (typeof object[key].valueOf() === 'string' && !isNaN(Date.parse(object[key]))))) {
+          throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', key));
+        }
+        object[key] = (object[key] instanceof Date) ? object[key].toISOString().substring(0, 10) : object[key];
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'DateTime') {
+        if (!(object[key] instanceof Date || 
+          (typeof object[key].valueOf() === 'string' && !isNaN(Date.parse(object[key]))))) {
+          throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', key));
+        }
+        object[key] = (object[key] instanceof Date) ? object[key].toISOString() : object[key];
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'DateTimeRfc1123') {
+        if (!(object[key] instanceof Date || 
+          (typeof object[key].valueOf() === 'string' && !isNaN(Date.parse(object[key]))))) {
+          throw new Error(util.format('%s must be an instanceof Date or a string in RFC-1123 format.', key));
+        }
+        object[key] = (object[key] instanceof Date) ? object[key].toUTCString() : object[key];
+        assignProperty(key, payload, object);
+      } else if (mapper[key].type.name === 'TimeSpan') {
+        if (!moment.isDuration(object[key])) {
+          throw new Error(util.format('%s must be a TimeSpan.', key));
+        }
+        object[key] = object[key].toISOString();
+        assignProperty(key, payload, object);
+      }
+    }
+  }
+  return payload;
+};
+
+/**
+ * Serialize the given object based on its metadata defined in the mapper
+ *
+ * @param {object} mapper The mapper which defines the metadata of the serializable object
+ *
+ * @param {object|string|Array|number|boolean|Date|stream} object A valid Javascript object to be serialized
+ *
+ * @param {string} objectName Name of the serialized object
+ *
+ * @param {object} [client] The ServiceClient instance which contains the reference to its list of model types
+ *
+ * @returns {object|string|Array|number|boolean|Date|stream} A valid serialized Javascript object
+ */
+exports.serialize = function (mapper, object, objectName, client) {
+  var payload = {};
+  var mapperType = mapper.type.name;
+  if (!objectName) objectName = objectNameFromSerializedName(mapper.serializedName);
+  if (mapperType === 'Sequence') payload = [];
+  if (mapperType.match(/^(Number|String|Boolean)$/ig) !== null) {
+    payload = serializeBasicTypes(mapperType, objectName, object);
+  } else if (mapperType === 'Enum') {
+    payload = serializeEnumType(objectName, mapper.type.allowedValues, object);
+  } else if (mapperType.match(/^(Date|DateTime|TimeSpan|DateTimeRfc1123)$/ig) !== null) {
+    payload = serializeDateTypes(mapperType, object, objectName);
+  } else if (mapperType === 'ByteArray') {
+    payload = serializeBufferType(objectName, object);
+  } else if (mapperType === 'Sequence') {
+    payload = serializeSequenceType(mapper, object, objectName, client);
+  } else if (mapperType === 'Dictionary') {
+    payload = serializeDictionaryType(mapper, object, objectName, client);
+  } else if (mapperType === 'Composite') {
+    payload = serializeCompositeType(mapper, object, objectName, client);
+  }
+  return payload;
+}
+
+function serializeSequenceType(mapper, object, objectName, client) {
+  if (!client) {
+    throw new Error(util.format('Please provide a valid client instance to serialize the ' + 
+        'object: \'%s\' of SequenceType', JSON.stringify(object)));
+  }
+  if (!util.isArray(object)) {
+    throw new Error(util.format('%s must be of type Array', objectName));
+  }
+  if (!mapper.type.element || typeof mapper.type.element !== 'object') {
+    throw new Error(util.format('\'element\' metadata for an Array must be defined in the ' + 
+      'mapper and it must of type \'object\' in %s', objectName));
+  }
+  var tempArray = [];
+  for (var i = 0; i < object.length; i++) {
+    tempArray[i] = exports.serialize(mapper.type.element, object[i], objectName, client);
+  }
+  return tempArray;
+}
+
+function serializeDictionaryType(mapper, object, objectName, client) {
+  if (!client) {
+    throw new Error(util.format('Please provide a valid client instance to serialize the ' + 
+        'object: \'%s\' of DictionaryType', JSON.stringify(object)));
+  }
+  if (typeof object !== 'object') {
+    throw new Error(util.format('%s must be of type object', objectName));
+  }
+  if (!mapper.type.value || typeof mapper.type.value !== 'object') {
+    throw new Error(util.format('\'value\' metadata for a Dictionary must be defined in the ' + 
+      'mapper and it must of type \'object\' in %s', objectName));
+  }
+  var tempDictionary = {};
+  for (var key in object) {
+    tempDictionary[key] = exports.serialize(mapper.type.value, object[key], objectName, client);
+  }
+  return tempDictionary;
+}
+
+function serializeCompositeType(mapper, object, objectName, client) {
+  if (!client) {
+    throw new Error(util.format('Please provide a valid client instance to serialize the ' + 
+        'object: \'%s\' of CompositeType', JSON.stringify(object)));
+  }
+  var payload = {};
+  var modelMapper = {};
+  var mapperType = mapper.type.name;
+  if (mapperType === 'Sequence') payload = [];
+  if (object !== null && object !== undefined) {
+    var modelProps = mapper.type.modelProperties;
+    if (!modelProps) {
+      if (!mapper.type.className) {
+        throw new Error(util.format('Class name for model \'%s\' is not provided in the mapper \'%s\'',
+            objectName, JSON.stringify(mapper)));
+      }
+      //get the mapper if modelProperties of the CompositeType is not present and 
+      //then get the modelProperties from it.
+      modelMapper = new client._models[mapper.type.className]().mapper();
+      if (!modelMapper) {
+        throw new Error(util.format('mapper() cannot be null or undefined for model \'%s\'', 
+              mapper.type.className));
+      }
+      modelProps = modelMapper.type.modelProperties;
+    }
+    for (var key in modelProps) {
+      //make sure required properties of the CompositeType are present
+      if (modelProps[key].required) {
+        if (object[key] === null || object[key] === undefined) {
+          throw new Error(util.format('\'%s\' cannot be null or undefined in \'%s\'.', key, objectName));
+        }
+      }
+      //serialize the property if it is present in the provided object instance
+      if (object[key] !== null && object[key] !== undefined) {
+        var propertyObjectName = objectName + '.' + objectNameFromSerializedName(modelProps[key].serializedName);
+        var propertyMapper = modelProps[key];
+        payload[key] = exports.serialize(propertyMapper, object[key], propertyObjectName, client);
+      }
+    }
+    return payload;
+  }
+  return object;
+}
+
+function serializeBasicTypes(typeName, objectName, value) {
+  if (typeName === 'Number') {
+    if (typeof value !== 'number') {
+      throw new Error(util.format('%s must be of type number.', objectName));
+    }
+  } else if (typeName === 'String') {
+    if (typeof value.valueOf() !== 'string') {
+      throw new Error(util.format('%s must be of type string.', objectName));
+    }
+  } else if (typeName === 'Boolean') {
+    if (typeof value !== 'boolean') {
+      throw new Error(util.format('%s must be of type boolean.', objectName));
+    }
+  }
+  return value;
+}
+
+function serializeEnumType(objectName, allowedValues, value) {
+  if (!allowedValues) {
+    throw new Error(util.format('Please provide a set of allowedValues to validate %s as an Enum Type.', objectName));
+  }
+  if (!allowedValues.some(function (item) { return item === value; })) {
+    throw new Error(util.format('%s is not a valid value for %s. The valid values are: %s', 
+      value, objectName, JSON.stringify(allowedValues)));
+  }
+  return value;
+}
+
+function serializeBufferType(objectName, value) {
+  if (!Buffer.isBuffer(value)) {
+    throw new Error(util.format('%s must be of type Buffer.', objectName));
+  }
+  value = value.toString('base64');
+  return value;
+}
+
+function serializeDateTypes(typeName, value, objectName) {
+  if (typeName === 'Date') {
+    if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+      throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', objectName));
+    }
+    value = (value instanceof Date) ? value.toISOString().substring(0, 10) : new Date(value).toISOString().substring(0, 10);
+  } else if (typeName === 'DateTime') {
+    if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+      throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', objectName));
+    }
+    value = (value instanceof Date) ? value.toISOString() :  new Date(value).toISOString();
+  } else if (typeName === 'DateTimeRfc1123') {
+    if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+      throw new Error(util.format('%s must be an instanceof Date or a string in RFC-1123 format.', objectName));
+    }
+    value = (value instanceof Date) ? value.toUTCString() :  new Date(value).toUTCString();
+  } else if (typeName === 'TimeSpan') {
+    if (!moment.isDuration(value)) {
+      throw new Error(util.format('%s must be a TimeSpan/Duration.', objectName));
+    }
+    value = value.toISOString();
+  }
+  return value;
+}
+
+function assignProperty(key, payload, object) {
+  if (stringContainsProperties(key)) {
+    payload.properties[key] = object[key];
+  } else {
+    payload[key] = object[key];
+  }
+}
+function requiresFlattening(mapper, object) {
+  return Object.keys(mapper).some(function (item) {
+    return ((mapper[key].serializedName.match(/^properties\./ig) !== null) && 
+            (object[key] !== null && object[key] !== undefined));
+  });
+}
+
+function objectNameFromSerializedName(name) {
+  if (stringContainsProperties(name)) {
+    return name.match(/^properties\.(\w+)/ig)[0];
+  }
+  return name;
+}
+
+function stringContainsProperties(prop) {
+  return (prop.match(/^properties\.(\w+)/ig) !== null);
+}
 
 exports = module.exports;
--- ClientRuntimes/NodeJS/ms-rest/test/serializationTests.js
+++ ClientRuntimes/NodeJS/ms-rest/test/serializationTests.js
@@ -3,8 +3,11 @@
 
 var assert = require('assert');
 var should = require('should');
-
+var moment = require('moment');
 var msRest = require('../lib/msRest');
+var testClient = require('./data/TestClient/lib/testClient');
+
+var tokenCredentials = new msRest.TokenCredentials('dummy');
 
 describe('msrest', function () {
   describe('serializeObject', function () {
@@ -108,7 +111,7 @@ describe('msrest', function () {
       done();
     });
   });
-
+  
   describe('deserializeDate', function () {
     it('should correctly deserialize max local positive offset \'9999-12-31T23:59:59+23:59\' value', function (done) {
       var date = msRest.deserializeDate('9999-12-31T23:59:59+23:59');
@@ -121,7 +124,7 @@ describe('msrest', function () {
       date.getUTCMilliseconds().should.equal(0);
       done();
     });
-
+    
     it('should correctly max local negative offset deserialize \'9999-12-31T23:59:59-23:59\' value', function (done) {
       var date = msRest.deserializeDate('9999-12-31T23:59:59-23:59');
       date.getUTCFullYear().should.equal(10000);
@@ -193,7 +196,7 @@ describe('msrest', function () {
       date.getUTCMilliseconds().should.equal(0);
       done();
     });
-
+    
     it('should correctly deserialize min date \'0000-01-01\' value', function (done) {
       var date = msRest.deserializeDate('0000-01-01');
       date.getUTCFullYear().should.equal(0);
@@ -217,7 +220,7 @@ describe('msrest', function () {
       date.getUTCMilliseconds().should.equal(0);
       done();
     });
-
+    
     it('should throw an error for null value', function (done) {
       msRest.deserializeDate.bind(null, null).should.throw();
       done();
@@ -227,76 +230,324 @@ describe('msrest', function () {
       msRest.deserializeDate.bind(null, 'Happy New Year 2016').should.throw();
       done();
     });
-
+    
     it('should throw an error for undefined value', function (done) {
       msRest.deserializeDate.bind(null, undefined).should.throw();
       done();
     });
-
+    
     it('should throw an error for 2010 value', function (done) {
       msRest.deserializeDate.bind(null, 2010).should.throw();
       done();
     });
-
+    
     it('should throw an error for true value', function (done) {
       msRest.deserializeDate.bind(null, true).should.throw();
       done();
     });
-
+    
     it('should throw an error for \'99999-12-31\' value', function (done) {
       msRest.deserializeDate.bind(null, '99999-12-31').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'2010-13-31\' value', function (done) {
       msRest.deserializeDate.bind(null, '2010-13-31').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'2015-22-01\' value', function (done) {
       msRest.deserializeDate.bind(null, '2015-22-01').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'2015-02-32\' value', function (done) {
       msRest.deserializeDate.bind(null, '2015-02-32').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01F01:01:01+00:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01F01:01:01+00:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01t01:01:01/00:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01t01:01:01/00:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01T24:01:01+00:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01T24:01:01+00:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01T23:60:01+00:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01T23:60:01+00:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01T23:01:78+00:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01T23:01:78+00:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01T23:01:54-24:30\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01T23:01:54-24:30').should.throw();
       done();
     });
-
+    
     it('should throw an error for \'1996-01-01T23:01:54-22:66\' value', function (done) {
       msRest.deserializeDate.bind(null, '1996-01-01T23:01:54-22:66').should.throw();
       done();
     });
 
   });
+  
+  describe('serialize', function () {
+    var mapper = {};
+    it('should correctly serialize a string', function (done) {
+      mapper = { type : { name: 'String' } };
+      var serializedObject = msRest.serialize(mapper, 'foo', 'stringBody');
+      serializedObject.should.equal('foo');
+      done();
+    });
+    it('should correctly serialize a number', function (done) {
+      mapper = { type : { name: 'Number' } };
+      var serializedObject = msRest.serialize(mapper, 1.506, 'stringBody');
+      serializedObject.should.equal(1.506);
+      done();
+    });
+    it('should correctly serialize a boolean', function (done) {
+      mapper = { type : { name: 'boolean' } };
+      var serializedObject = msRest.serialize(mapper, false, 'stringBody');
+      serializedObject.should.equal(false);
+      done();
+    });
+    it('should correctly serialize an Enum', function (done) {
+      mapper = { type : { name: 'Enum', allowedValues: [1, 2, 3, 4] } };
+      var serializedObject = msRest.serialize(mapper, 1, 'enumBody');
+      serializedObject.should.equal(1);
+      done();
+    });
+    it('should throw an error if the value is not valid for an Enum', function (done) {
+      mapper = { type : { name: 'Enum', allowedValues: [1, 2, 3, 4] } };
+      try {
+        var serializedObject = msRest.serialize(mapper, 6, 'enumBody');
+      } catch (error) {
+        error.message.should.match(/6 is not a valid value for enumBody\. The valid values are: \[1,2,3,4\]/ig);
+        done();
+      }
+    });
+    it('should throw an error if allowedValues is not specified for an EnumType', function (done) {
+      mapper = { type : { name: 'Enum' } };
+      try {
+        var serializedObject = msRest.serialize(mapper, 6, 'enumBody');
+      } catch (error) {
+        error.message.should.match(/Please provide a set of allowedValues to validate enumBody as an Enum Type\./ig);
+        done();
+      }
+    });
+    it('should correctly serialize a Buffer Object', function (done) {
+      mapper = { type : { name: 'ByteArray' } };
+      var bufferObj = new Buffer('Javascript');
+      var base64str = 'SmF2YXNjcmlwdA==';
+      var serializedObject = msRest.serialize(mapper, bufferObj, 'stringBody');
+      serializedObject.should.equal(base64str);
+      done();
+    });
+    it('should correctly serialize a Date Object', function (done) {
+      var dateObj = new Date('2015-01-01');
+      var dateISO = '2015-01-01';
+      mapper = { type : { name: 'Date' } };
+      msRest.serialize(mapper, dateObj, 'dateObj').should.equal(dateISO);
+      done();
+    });
+    it('should correctly serialize a Date object with max value', function (done) {
+      mapper = { type : { name: 'DateTime' } };
+      var serializedDateString = msRest.serialize(mapper, new Date('9999-12-31T23:59:59-12:00'), 'dateTimeObj');
+      serializedDateString.should.equal('+010000-01-01T11:59:59.000Z');
+      done();
+    });
+    it('should correctly serialize a string in DateTimeRfc1123', function (done) {
+      mapper = { type : { name: 'DateTimeRfc1123' } };
+      var rfc = new Date('Mon, 01 Jan 0001 00:00:00 GMT');
+      var serializedDateString = msRest.serialize(mapper, rfc, 'dateTimeObj');
+      serializedDateString.should.equal('Mon, 01 Jan 2001 00:00:00 GMT');
+      done();
+    });
+    it('should correctly serialize a duration object', function (done) {
+      mapper = { type : { name: 'TimeSpan' } };
+      var duration = moment.duration({ days: 123, hours: 22, minutes: 14, seconds: 12, milliseconds: 11 });
+      var serializedDateString = msRest.serialize(mapper, duration, 'dateTimeObj');
+      serializedDateString.should.equal('P123DT22H14M12.010999999998603S');
+      done();
+    });
+    
+    it('should correctly serialize an array of primitives', function (done) {
+      mapper = { type : { name: 'Sequence', element: { type : { name: 'String' } } } };
+      var array = ['One', 'Two', 'three'];
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedArray = msRest.serialize(mapper, array, 'arrayObj', client);
+      assert.deepEqual(array, serializedArray);
+      done();
+    });
+    
+    it('should correctly serialize an array of array of primitives', function (done) {
+      mapper = {
+        type : {
+          name: 'Sequence', 
+          element: {
+            type : {
+              name: 'Sequence',
+              element: {
+                type: {
+                  name: 'Number'
+                }
+              }
+            }
+          }
+        }
+      };
+      var array = [[1], [2], [1, 2, 3]];
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedArray = msRest.serialize(mapper, array, 'arrayObj', client);
+      assert.deepEqual(array, serializedArray);
+      done();
+    });
+    
+    it('should correctly serialize an array of dictionary of primitives', function (done) {
+      mapper = {
+        type : {
+          name: 'Sequence', 
+          element: {
+            type : {
+              name: 'Dictionary',
+              value: {
+                type: {
+                  name: 'Boolean'
+                }
+              }
+            }
+          }
+        }
+      };
+      var array = [{ 1: true }, { 2: false }, { 1: true, 2: false, 3: true }];
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedArray = msRest.serialize(mapper, array, 'arrayObj', client);
+      assert.deepEqual(array, serializedArray);
+      done();
+    });
+    
+    it('should correctly serialize a dictionary of primitives', function (done) {
+      mapper = { type : { name: 'Dictionary', value: { type : { name: 'String' } } } };
+      var dict = { 1: 'One', 2: 'Two', 3: 'three' };
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedDictionary = msRest.serialize(mapper, dict, 'dictObj', client);
+      assert.deepEqual(dict, serializedDictionary);
+      done();
+    });
+    
+    it('should correctly serialize a dictionary of array of primitives', function (done) {
+      mapper = {
+        type : {
+          name: 'Dictionary', 
+          value: {
+            type : {
+              name: 'Sequence',
+              element: {
+                type: {
+                  name: 'Number'
+                }
+              }
+            }
+          }
+        }
+      };
+      var dict = { 'One': [1], 'Two': [1, 2], 'three': [1, 2, 3] };
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedDictionary = msRest.serialize(mapper, dict, 'dictObj', client);
+      assert.deepEqual(dict, serializedDictionary);
+      done();
+    });
+    
+    it('should correctly serialize a dictionary of dictionary of primitives', function (done) {
+      mapper = {
+        type : {
+          name: 'Dictionary', 
+          value: {
+            type : {
+              name: 'Dictionary',
+              value: {
+                type: {
+                  name: 'Boolean'
+                }
+              }
+            }
+          }
+        }
+      };
+      var dict = { 1: { 'One': true }, 2: { 'Two': false }, 3: { 'three': true } };
+      var client = new msRest.ServiceClient(tokenCredentials);
+      var serializedDictionary = msRest.serialize(mapper, dict, 'dictObj', client);
+      assert.deepEqual(dict, serializedDictionary);
+      done();
+    });
+
+    it('should correctly serialize a composite type', function (done) {
+      var client = new testClient('http://localhost:9090');
+      mapper = new client._models['Product']().mapper();
+      var productObj = {
+        id: 101,
+        name: 'TestProduct',
+        provisioningState: 'Succeeded',
+        tags: {
+          tag1: 'value1',
+          tag2: 'value2'
+        },
+        dispatchTime: new Date('2015-01-01T12:35:36.009Z'),
+        invoiceInfo: {
+          invId: 1002,
+          invDate: '2015-12-25',
+          invProducts: [
+            {
+              'Product1' : {
+                id: 101,
+                name: 'TestProduct'
+              }
+            },
+            {
+              'Product2' : {
+                id: 104,
+                name: 'TestProduct1'
+              }
+            }
+          ]
+        },
+        subProducts: [
+          {
+            subId: 102,
+            subName: 'SubProduct1',
+            makeTime: new Date('2015-12-21T01:01:01'),
+            invoiceInfo: {
+              invId: 1002,
+              invDate: '2015-12-25'
+            }
+          },
+          {
+            subId: 103,
+            subName: 'SubProduct2',
+            makeTime: new Date('2015-12-21T01:01:01'),
+            invoiceInfo: {
+              invId: 1003,
+              invDate: '2015-12-25'
+            }
+          }
+        ]
+      };
+      var serializedProduct = msRest.serialize(mapper, productObj, 'productObject', client);
+      JSON.stringify(productObj).should.equal(JSON.stringify(serializedProduct));
+      done();
+    });
+  });
 });
+
