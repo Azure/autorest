1344bcb5145658ac94d779a8c5a81d5b01913831
matthchr <matthchr@microsoft.com>
2016-01-11 17:55:58 -0800

    Fix issue in linux where node server doesnt exit
      - The issue was that since in Linux we have to start the node server
        using shell=True, that when we called process.kill it just killed the
        shell process and not the node process, which made travis get stuck.
        The fix was to kill the whole process group (on Linux only).

--- gulpfile.js
+++ gulpfile.js
@@ -406,8 +406,8 @@ gulp.task('test:ruby:azure', ['regenerate:expected:rubyazure'], shell.task('ruby
 gulp.task('test:java', shell.task(basePathOrThrow() + '/gradlew :codegen-tests:check', {cwd: './', verbosity: 3}));
 gulp.task('test:java:azure', shell.task(basePathOrThrow() + '/gradlew :azure-codegen-tests:check', {cwd: './', verbosity: 3}));
 
-gulp.task('test:python', shell.task('tox', {cwd: './AutoRest/Generators/Python/Python.Tests', verbosity: 3}));
-gulp.task('test:python:azure', shell.task('tox', {cwd: './AutoRest/Generators/Python/Azure.Python.Tests', verbosity: 3}));
+gulp.task('test:python', shell.task('tox', {cwd: './AutoRest/Generators/Python/Python.Tests/', verbosity: 3}));
+gulp.task('test:python:azure', shell.task('tox', {cwd: './AutoRest/Generators/Python/Azure.Python.Tests/', verbosity: 3}));
 
 var xunitTestsDlls = [
   'AutoRest/AutoRest.Core.Tests/bin/Net45-Debug/AutoRest.Core.Tests.dll',
