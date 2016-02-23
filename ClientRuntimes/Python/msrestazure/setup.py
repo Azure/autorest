4db1e0a83141703f59f1b11a516ceac3aa80a91d
annatisch <antisch@microsoft.com>
2016-02-23 10:16:01 -0800

    bumped msrest version

--- ClientRuntimes/Python/msrestazure/setup.py
+++ ClientRuntimes/Python/msrestazure/setup.py
@@ -49,5 +49,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development'],
     install_requires=[
-        "msrest>=0.0.2"],
+        "msrest>=0.1.0"],
 )
