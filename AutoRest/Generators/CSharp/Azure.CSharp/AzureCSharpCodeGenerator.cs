bbe5f8569a1332b03778f8f3da40047ec88c3ba1
begoldsm <begoldsm@microsoft.com>
2016-03-14 14:43:19 -0700

    Remove accidental empty line.

--- AutoRest/Generators/CSharp/Azure.CSharp/AzureCSharpCodeGenerator.cs
+++ AutoRest/Generators/CSharp/Azure.CSharp/AzureCSharpCodeGenerator.cs
@@ -25,7 +25,6 @@ namespace Microsoft.Rest.Generator.CSharp.Azure
 
         public AzureCSharpCodeGenerator(Settings settings) : base(settings)
         {
-
             _namer = new AzureCSharpCodeNamer(settings);
             IsSingleFileGenerationSupported = true;
             pageClasses = new Dictionary<KeyValuePair<string, string>, string>();
