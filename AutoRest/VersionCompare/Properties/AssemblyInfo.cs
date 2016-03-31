05af7aeefd90e6275bb914ab834cf7433caf53bd
Niklas Gustafsson <niklasg@microsoft.com>
2016-03-31 12:18:30 -0700

    Adding a Swagger version compare tool.

---
+++ AutoRest/VersionCompare/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VersionCompare")]
+[assembly: AssemblyDescription("VersionCompare command-line interface")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft VersionCompare")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00720539-2d80-4ec1-81b7-ea57e4f630e5")]
+
