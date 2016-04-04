b21cf0eb1b43d137ef2e32a7310572e509272499
annatisch <antisch@microsoft.com>
2016-04-04 13:16:09 -0700

    Regenerated with updated version

--- AutoRest/Generators/Python/Python/TemplateModels/ServiceClientTemplateModel.cs
+++ AutoRest/Generators/Python/Python/TemplateModels/ServiceClientTemplateModel.cs
@@ -138,7 +138,7 @@ namespace Microsoft.Rest.Generator.Python
         {
             get
             {
-                return "\"msrest>=0.2.1\"";
+                return "\"msrest>=0.2.0\"";
             }
         }
        
