f4e2139b6982ff8d623eba4ea1ab222f6a09b366
Amar Zavery <amzavery@microsoft.com>
2016-04-04 20:09:36 -0700

    removed import stmts

--- AutoRest/Generators/Java/Java.Tests/src/test/java/fixtures/url/PathsTests.java
+++ AutoRest/Generators/Java/Java.Tests/src/test/java/fixtures/url/PathsTests.java
@@ -4,9 +4,6 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import fixtures.url.models.UriColor;
 
 public class PathsTests {
