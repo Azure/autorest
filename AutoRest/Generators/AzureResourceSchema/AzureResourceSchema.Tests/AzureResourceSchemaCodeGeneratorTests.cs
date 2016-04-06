f7840bfd355ac8ec5c4b4b9bb4062bb561213959
Dan Schulte <daschult@microsoft.com>
2016-04-06 13:08:41 -0700

    Update AzureResourceSchema projects and add initial c# files

---
+++ AutoRest/Generators/AzureResourceSchema/AzureResourceSchema.Tests/AzureResourceSchemaCodeGeneratorTests.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Rest.Generator.ClientModel;
+using System;
+using Xunit;
+
+namespace Microsoft.Rest.Generator.AzureResourceSchema.Tests
+{
+    [Collection("AutoRest Tests")]
+    public class AzureResourceSchemaCodeGeneratorTests
+    {
+        [Fact]
+        public void DescriptionThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+            Assert.Throws<NotImplementedException>(() => { string value = codeGen.Description; });
+        }
+
+        [Fact]
+        public void ImplementationFileExtensionThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+            Assert.Throws<NotImplementedException>(() => { string value = codeGen.ImplementationFileExtension; });
+        }
+
+        [Fact]
+        public void NameThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+            Assert.Throws<NotImplementedException>(() => { string value = codeGen.Name; });
+        }
+
+        [Fact]
+        public void UsageInstructionsThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+            Assert.Throws<NotImplementedException>(() => { string value = codeGen.UsageInstructions; });
+        }
+
+        [Fact]
+        public void GenerateThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+
+            ServiceClient serviceClient = new ServiceClient();
+            Assert.Throws<NotImplementedException>(() => { codeGen.Generate(serviceClient); });
+        }
+
+        [Fact]
+        public void NormalizeClientModelThrowsException()
+        {
+            Settings settings = new Settings();
+            AzureResourceSchemaCodeGenerator codeGen = new AzureResourceSchemaCodeGenerator(settings);
+
+            ServiceClient serviceClient = new ServiceClient();
+            Assert.Throws<NotImplementedException>(() => { codeGen.NormalizeClientModel(serviceClient); });
+        }
+    }
+}
---
+++ AutoRest/Generators/AzureResourceSchema/AzureResourceSchema/AzureResourceSchemaCodeGenerator.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Rest.Generator.ClientModel;
+
+namespace Microsoft.Rest.Generator.AzureResourceSchema
+{
+    public class AzureResourceSchemaCodeGenerator : CodeGenerator
+    {
+        public AzureResourceSchemaCodeGenerator(Settings settings)
+            : base(settings)
+        {
+        }
+
+        public override string Description
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string ImplementationFileExtension
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string UsageInstructions
+        {
+            get
+            {
+                return "MOCK USAGE INSTRUCTIONS";
+            }
+        }
+
+        public override Task Generate(ServiceClient serviceClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void NormalizeClientModel(ServiceClient serviceClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
