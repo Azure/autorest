a5bc4472335fad2710ac0bed31c2d3bfff03726c
Laurent Mazuel <lmazuel@microsoft.com>
2016-04-26 13:37:40 -0700

    msrest/msrestazure 0.3.0

--- AutoRest/Generators/Python/Azure.Python/TemplateModels/AzureServiceClientTemplateModel.cs
+++ AutoRest/Generators/Python/Azure.Python/TemplateModels/AzureServiceClientTemplateModel.cs
@@ -97,7 +97,7 @@ namespace Microsoft.Rest.Generator.Azure.Python
         {
             get
             {
-                return "\"msrest>=0.2.0\", \"msrestazure>=0.2.1\"";
+                return "\"msrest>=0.3.0\", \"msrestazure>=0.3.0\"";
             }
         }
 
