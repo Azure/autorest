6bd13292f7a7724d954d6ea0bf515b04d64336ff
annatisch <antisch@microsoft.com>
2016-05-23 16:15:10 -0700

    Fixed bug in enum namespace docs

--- AutoRest/Generators/Python/Python.Tests/Expected/AcceptanceTests/Url/autoresturltestservice/operations/queries.py
+++ AutoRest/Generators/Python/Python.Tests/Expected/AcceptanceTests/Url/autoresturltestservice/operations/queries.py
@@ -806,7 +806,7 @@ class Queries(object):
         :param enum_query: 'green color' enum value. Possible values include:
          'red color', 'green color', 'blue color'
         :type enum_query: str or :class:`UriColor
-         <autoresturltestservice.models.UriColor>`
+         <fixtures.acceptancetestsurl.models.UriColor>`
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
@@ -849,7 +849,7 @@ class Queries(object):
         :param enum_query: null string value. Possible values include: 'red
          color', 'green color', 'blue color'
         :type enum_query: str or :class:`UriColor
-         <autoresturltestservice.models.UriColor>`
+         <fixtures.acceptancetestsurl.models.UriColor>`
         :param dict custom_headers: headers that will be added to the request
         :param bool raw: returns the direct response alongside the
          deserialized response
--- AutoRest/Generators/Python/Python/TemplateModels/ServiceClientTemplateModel.cs
+++ AutoRest/Generators/Python/Python/TemplateModels/ServiceClientTemplateModel.cs
@@ -257,6 +257,10 @@ namespace Microsoft.Rest.Generator.Python
                 throw new ArgumentNullException("type");
             }
 
+            var modelNamespace = ServiceClient.Name.ToPythonCase().Replace("_", "");
+            if (!ServiceClient.Namespace.IsNullOrEmpty())
+                modelNamespace = ServiceClient.Namespace.ToPythonCase().Replace("_", "");
+
             string result = "object";
             var primaryType = type as PrimaryType;
             var listType = type as SequenceType;
@@ -277,7 +281,7 @@ namespace Microsoft.Rest.Generator.Python
             }
             else if (type is EnumType)
             {
-                result = "str";
+                result = string.Format(CultureInfo.InvariantCulture, "str or :class:`{0} <{1}.models.{0}>`", type.Name, modelNamespace);
             }
             else if (type is DictionaryType)
             {
@@ -285,9 +289,6 @@ namespace Microsoft.Rest.Generator.Python
             }
             else if (type is CompositeType)
             {
-                var modelNamespace = ServiceClient.Name.ToPythonCase().Replace("_", "");
-                if (!ServiceClient.Namespace.IsNullOrEmpty())
-                    modelNamespace = ServiceClient.Namespace.ToPythonCase().Replace("_", "");
                 result = string.Format(CultureInfo.InvariantCulture, ":class:`{0} <{1}.models.{0}>`", type.Name, modelNamespace);
             }
 
