19a6cc5e11182196e1a5a9c1f9c47f37e4c635e2
Dan Schulte <daschult@microsoft.com>
2016-05-25 11:30:50 -0700

    Add ability to seperate acceptance tests for AzureResourceSchema generator by resource provider and api version

--- AutoRest/Generators/AzureResourceSchema/AzureResourceSchema.Tests/AcceptanceTests.cs
+++ AutoRest/Generators/AzureResourceSchema/AzureResourceSchema.Tests/AcceptanceTests.cs
@@ -14,50 +14,56 @@ namespace AutoRest.Generator.AzureResourceSchema.Tests
         [Fact]
         public static void Storage()
         {
-            RunSwaggerTest("storage.json", "Storage");
+            RunSwaggerTest("Storage", "2016-01-01", "storage.json");
         }
 
         [Fact]
         public static void Batch()
         {
-            RunSwaggerTest("BatchManagement.json", "Batch");
+            RunSwaggerTest("Batch", "2015-12-01", "BatchManagement.json");
         }
 
         [Fact]
         public static void Cdn()
         {
-            RunSwaggerTest("cdn.json", "CDN");
+            RunSwaggerTest("CDN", "2016-04-02", "cdn.json");
         }
 
         [Fact]
         public static void Compute()
         {
-            RunSwaggerTest("compute.json", "Compute");
+            RunSwaggerTest("Compute", "2016-03-30", "compute.json");
         }
 
         [Fact]
-        public static void Dns()
+        public static void Dns_2015_05_04_preview()
         {
-            RunSwaggerTest("dns.json", "DNS");
+            RunSwaggerTest("DNS", "2015-05-04-preview", "dns.json");
+        }
+
+        [Fact]
+        public static void Dns_2016_04_01()
+        {
+            RunSwaggerTest("DNS", "2016-04-01", "dns.json");
         }
 
         [Fact]
         public static void Network()
         {
-            RunSwaggerTest("network.json", "Network");
+            RunSwaggerTest("Network", "2016-03-30", "network.json");
         }
 
         [Fact]
         public static void Web()
         {
-            RunSwaggerTest("web.json", "Web");
+            RunSwaggerTest("Web", "2015-08-01", "web.json");
         }
 
-        private static void RunSwaggerTest(string swaggerFileName, string expectedFolderName)
+        private static void RunSwaggerTest(string resourceType, string apiVersion, string swaggerFileName)
         {
             SwaggerSpecHelper.RunTests<AzureResourceSchemaCodeGenerator>(
-                Path.Combine("Swagger", swaggerFileName),
-                Path.Combine("Expected", expectedFolderName));
+                Path.Combine("Swagger", resourceType, apiVersion, swaggerFileName),
+                Path.Combine("Expected", resourceType, apiVersion));
         }
     }
 }
