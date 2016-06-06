d51d8c7c345434ca9980d90f61a7c3396b1b9cfe
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-06-06 15:40:47 -0700

    Address checkstyle errors

--- ClientRuntimes/Java/azure-client-runtime/src/main/java/com/microsoft/azure/TaskItem.java
+++ ClientRuntimes/Java/azure-client-runtime/src/main/java/com/microsoft/azure/TaskItem.java
@@ -1,3 +1,10 @@
+/**
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *
+ */
+
 package com.microsoft.azure;
 
 /**
