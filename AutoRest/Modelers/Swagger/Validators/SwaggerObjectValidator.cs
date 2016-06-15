bca781009d3676a157c82468b5975499f2e0d4e8
Thomas Bombach <bombach.thomas@gmail.com>
2016-06-15 13:54:16 -0700

    Adding IValidator interface and moving first rule into a validator class outside of the modeler

---
+++ AutoRest/Modelers/Swagger/Validators/SwaggerObjectValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Rest.Generator.Logging;
+using Microsoft.Rest.Modeler.Swagger.Model;
+using Microsoft.Rest.Modeler.Swagger.Properties;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Rest.Modeler.Swagger.Validators
+{
+    public class SwaggerObjectValidator : IValidator<SwaggerObject>
+    {
+        public bool IsValid(SwaggerObject entity)
+        {
+            return !ValidationExceptions(entity).Any();
+        }
+
+        public IEnumerable<string> ValidationExceptions(SwaggerObject entity)
+        {
+            if (string.IsNullOrEmpty(entity.Description) && string.IsNullOrEmpty(entity.Reference))
+            {
+                // TODO: need to have a way to include warning, error level
+                yield return Resources.MissingDescription;
+            }
+            yield break;
+        }
+    }
+}
