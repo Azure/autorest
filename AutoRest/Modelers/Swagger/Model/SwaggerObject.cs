7ebebb3fb25de689d7a7d3f47f3eeb0f752a4e76
Thomas Bombach <bombach.thomas@gmail.com>
2016-06-15 13:54:16 -0700

    Adding IValidator interface and moving first rule into a validator class outside of the modeler

--- AutoRest/Modelers/Swagger/Model/SwaggerObject.cs
+++ AutoRest/Modelers/Swagger/Model/SwaggerObject.cs
@@ -155,7 +155,7 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
                     return new PrimaryType(KnownPrimaryType.Stream);
                 default:
                     throw new NotImplementedException(
-                        string.Format(CultureInfo.InvariantCulture, 
+                        string.Format(CultureInfo.InvariantCulture,
                            Resources.InvalidTypeInSwaggerSchema,
                             Type));
             }
@@ -172,10 +172,17 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
 
             base.Validate(context);
 
-            if (string.IsNullOrEmpty(Description) && string.IsNullOrEmpty(Reference))
+            var validator = new Validators.SwaggerObjectValidator();
+            foreach (var message in validator.ValidationExceptions(this))
             {
-                context.LogWarning(Resources.MissingDescription);
+                // TODO: put logging somewhere else & have a way to specify level
+                context.LogWarning(message);
             }
+
+            //if (string.IsNullOrEmpty(Description) && string.IsNullOrEmpty(Reference))
+            //{
+            //    context.LogWarning(Resources.MissingDescription);
+            //}
 
             ValidateConstraints(context);
 
@@ -316,7 +323,7 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
         }
 
         [SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", Justification = "The call to 'new Regex' is made only to look for exceptions.")]
-        private void ValidateConstraints(ValidationContext context) 
+        private void ValidateConstraints(ValidationContext context)
         {
             double numberValue;
             long integerValue = 0;
@@ -348,10 +355,11 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
 
             if (!string.IsNullOrEmpty(Pattern))
             {
-                try {
+                try
+                {
                     new Regex(Pattern);
                 }
-                catch(ArgumentException ae)
+                catch (ArgumentException ae)
                 {
                     context.LogError(string.Format(CultureInfo.InvariantCulture, Resources.InvalidPattern2, Pattern, ae.Message));
                 }
@@ -376,13 +384,13 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
                 context.LogBreakingChange(string.Format(CultureInfo.InvariantCulture, Resources.PropertyValueChanged1, "multipleOf"));
             }
             if ((prior.Maximum == null && Maximum != null) ||
-                (prior.Maximum != null && !prior.Maximum.Equals(Maximum)) || 
+                (prior.Maximum != null && !prior.Maximum.Equals(Maximum)) ||
                 prior.ExclusiveMaximum != ExclusiveMaximum)
             {
                 context.LogBreakingChange(string.Format(CultureInfo.InvariantCulture, Resources.PropertyValueChanged1, "maximum"));
             }
             if ((prior.Minimum == null && Minimum != null) ||
-                (prior.Minimum != null && !prior.Minimum.Equals(Minimum)) || 
+                (prior.Minimum != null && !prior.Minimum.Equals(Minimum)) ||
                 prior.ExclusiveMinimum != ExclusiveMinimum)
             {
                 context.LogBreakingChange(string.Format(CultureInfo.InvariantCulture, Resources.PropertyValueChanged1, "minimum"));
@@ -402,7 +410,7 @@ namespace Microsoft.Rest.Modeler.Swagger.Model
             {
                 context.LogBreakingChange(string.Format(CultureInfo.InvariantCulture, Resources.PropertyValueChanged1, "pattern"));
             }
-            if ((prior.MaxItems  == null && MaxItems != null) ||
+            if ((prior.MaxItems == null && MaxItems != null) ||
                 (prior.MaxItems != null && !prior.MaxItems.Equals(MaxItems)))
             {
                 context.LogBreakingChange(string.Format(CultureInfo.InvariantCulture, Resources.PropertyValueChanged1, "maxItems"));
