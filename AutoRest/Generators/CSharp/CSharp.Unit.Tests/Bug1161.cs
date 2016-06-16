776814367f5878889043d29933a2f1f440bd0ced
Garrett Serack <garretts@microsoft.com>
2016-06-16 10:21:34 -0700

    Adding unit test project for CSharp generator

---
+++ AutoRest/Generators/CSharp/CSharp.Unit.Tests/Bug1161.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.IO;
+using Microsoft.Rest.Generator;
+using Microsoft.Rest.Generator.CSharp;
+using Microsoft.Rest.Generator.Extensibility;
+using Xunit;
+
+namespace AutoRest.Csharp.Unit.Test
+{
+    public class Bug1161 : BugTest
+    {
+        [Fact]
+        public void DoubleQuestionMarksWhenUsingEnumsInAnArray()
+        {
+            using (var fileSystem = "Bug1161.yaml".GenerateCodeInto(CreateMockFilesystem()))
+            {
+                Assert.True(fileSystem.FileExists(@"GeneratedCode\Models\RecurrenceSchedule.cs"));
+
+                var recurrenceSchedule = fileSystem.ReadFileAsText(@"GeneratedCode\Models\RecurrenceSchedule.cs");
+                Assert.NotNull(recurrenceSchedule);
+
+                
+            }
+        }
+    }
+}
---
+++ AutoRest/Generators/CSharp/CSharp.Unit.Tests/TestExtensions.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.IO;
+using Microsoft.Rest.Generator;
+using Microsoft.Rest.Generator.CSharp;
+using Microsoft.Rest.Generator.Extensibility;
+using Microsoft.Rest.Generator.Utilities;
+
+namespace AutoRest.Csharp.Unit.Test
+{
+    internal static class TestExtensions
+    {
+        internal static void Copy(this IFileSystem fileSystem , string sourceFileOnDisk)
+        {
+            Copy( fileSystem, sourceFileOnDisk, Path.GetFileName(sourceFileOnDisk));
+        }
+        internal static void Copy(this IFileSystem fileSystem, string sourceFileOnDisk, string destination)
+        {
+            fileSystem.WriteFile(destination, File.ReadAllText(sourceFileOnDisk));
+        }
+
+        internal static MemoryFileSystem GenerateCodeInto(this string inputFile, MemoryFileSystem fileSystem )
+        {
+            fileSystem.Copy(Path.Combine("Resource", inputFile));
+
+            var settings = new Settings
+            {
+                Modeler = "Swagger",
+                CodeGenerator = "CSharp",
+                FileSystem = fileSystem,
+                OutputDirectory = "GeneratedCode",
+                Input = inputFile,
+            };
+
+            var codeGenerator = new CSharpCodeGenerator(settings);
+            Modeler modeler = ExtensionsLoader.GetModeler(settings);
+            var sc = modeler.Build();
+            codeGenerator.NormalizeClientModel(sc);
+            codeGenerator.Generate(sc).GetAwaiter().GetResult();
+
+            return fileSystem;
+        }
+    }
+}
