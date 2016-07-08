cbe17c8003c2cca713f175ad4e3f9b537347b1a4
Garrett Serack <garretts@microsoft.com>
2016-07-08 14:29:01 -0700

    update license in header of source files to MIT

--- src/generator/AutoRest.NodeJS/TemplateModels/MethodGroupTemplateModel.cs
+++ src/generator/AutoRest.NodeJS/TemplateModels/MethodGroupTemplateModel.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
 using System.Linq;
