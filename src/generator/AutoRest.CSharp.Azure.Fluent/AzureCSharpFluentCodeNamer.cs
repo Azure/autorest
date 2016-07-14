3ffe62cec09ad8f138fc6ea420712c728f1dad39
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-07-13 17:14:43 -0700

    Add fluent csharp code generators

---
+++ src/generator/AutoRest.CSharp.Azure.Fluent/AzureCSharpFluentCodeNamer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using AutoRest.Core;
+using AutoRest.Core.ClientModel;
+using AutoRest.Extensions;
+using AutoRest.Extensions.Azure;
+
+namespace AutoRest.CSharp.Azure.Fluent
+{
+    public class AzureCSharpFluentCodeNamer : AzureCSharpCodeNamer
+    {
+        /// <summary>
+        /// Skips name collision resolution for method groups (operations) as they get
+        /// renamed in template models.
+        /// </summary>
+        /// <param name="serviceClient"></param>
+        /// <param name="exclusionDictionary"></param>
+        protected override void ResolveMethodGroupNameCollision(ServiceClient serviceClient,
+            Dictionary<string, string> exclusionDictionary)
+        {
+            // Do nothing
+        }
+
+        public AzureCSharpFluentCodeNamer(Settings settings)
+            :base(settings)
+        {
+        }
+    }
+}
