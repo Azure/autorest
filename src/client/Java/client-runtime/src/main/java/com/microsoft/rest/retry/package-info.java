372224313ad828a6370724ba959efbebee26482d
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-07-14 15:38:17 -0700

    Reconstruct subtree

--- src/client/Java/client-runtime/src/main/java/com/microsoft/rest/retry/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The package contains classes that define the retry behaviors when an error
- * occurs during a REST call.
- */
-package com.microsoft.rest.retry;
--- src/client/Java/client-runtime/src/main/java/com/microsoft/rest/serializer/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The package contains classes that handle serialization and deserialization
- * for the REST call payloads.
- */
-package com.microsoft.rest.serializer;
