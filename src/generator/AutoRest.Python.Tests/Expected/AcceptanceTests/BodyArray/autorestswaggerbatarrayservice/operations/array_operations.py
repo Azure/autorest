64a8c2a16b992a65d67dff5106183570d0b99b2a
annatisch <antisch@microsoft.com>
2016-07-18 09:42:38 -0700

    Regenerated Tests

--- src/generator/AutoRest.Python.Tests/Expected/AcceptanceTests/BodyArray/autorestswaggerbatarrayservice/operations/array_operations.py
+++ src/generator/AutoRest.Python.Tests/Expected/AcceptanceTests/BodyArray/autorestswaggerbatarrayservice/operations/array_operations.py
@@ -21,6 +21,7 @@ class ArrayOperations(object):
     :param config: Configuration of service client.
     :param serializer: An object model serializer.
     :param deserializer: An object model deserializer.
+    :param deserializer: An objec model deserializer.
     """
 
     def __init__(self, client, config, serializer, deserializer):
