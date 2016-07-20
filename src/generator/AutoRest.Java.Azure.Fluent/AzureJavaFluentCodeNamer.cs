58cc9455e11d72c70b27496e1c5f4cc21e8713f2
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-07-20 10:42:56 -0700

    Change criteria for appending 'Inner' in Java Fluent

--- src/generator/AutoRest.Java.Azure.Fluent/AzureJavaFluentCodeNamer.cs
+++ src/generator/AutoRest.Java.Azure.Fluent/AzureJavaFluentCodeNamer.cs
@@ -21,14 +21,19 @@ namespace AutoRest.Java.Azure.Fluent
 
         public void NormalizeTopLevelTypes(ServiceClient serviceClient)
         {
-            foreach (var param in serviceClient.Methods.SelectMany(m => m.Parameters))
+            //foreach (var param in serviceClient.Methods.SelectMany(m => m.Parameters))
+            //{
+            //    AppendInnerToTopLevelType(param.Type, serviceClient);
+            //}
+            //foreach (var response in serviceClient.Methods.SelectMany(m => m.Responses).Select(r => r.Value))
+            //{
+            //    AppendInnerToTopLevelType(response.Body, serviceClient);
+            //    AppendInnerToTopLevelType(response.Headers, serviceClient);
+            //}
+            foreach (var model in serviceClient.ModelTypes)
             {
-                AppendInnerToTopLevelType(param.Type, serviceClient);
-            }
-            foreach (var response in serviceClient.Methods.SelectMany(m => m.Responses).Select(r => r.Value))
-            {
-                AppendInnerToTopLevelType(response.Body, serviceClient);
-                AppendInnerToTopLevelType(response.Headers, serviceClient);
+                if (model.BaseModelType != null && (model.BaseModelType.Name == "Resource" || model.BaseModelType.Name == "SubResource"))
+                AppendInnerToTopLevelType(model, serviceClient);
             }
         }
 
