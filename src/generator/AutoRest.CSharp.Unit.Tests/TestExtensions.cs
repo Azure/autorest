833aa9b699992a0ca5321c982571eb9232c0363c
John Hart <johnhart@microsoft.com>
2016-07-28 17:46:40 -0700

    Added a new unit test to verify the signature of a composite model builds correctly

--- src/generator/AutoRest.CSharp.Unit.Tests/TestExtensions.cs
+++ src/generator/AutoRest.CSharp.Unit.Tests/TestExtensions.cs
@@ -68,7 +68,8 @@ namespace AutoRest.CSharp.Unit.Tests
             {
                 try
                 {
-                    Directory.Delete(outputFolder, true);
+                    fileSystem.EmptyDirectory(outputFolder);
+                    fileSystem.DeleteDirectory(outputFolder);
                 }
                 catch
                 {
