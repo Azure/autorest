ecbed7fd9b8c6a7f23d936cb338375c8fd75e28f
annatisch <antisch@microsoft.com>
2016-08-04 10:02:38 -0700

    Added default value to doc strings

--- src/generator/AutoRest.Python.Tests/Expected/AcceptanceTests/CustomBaseUri/autorestparameterizedhosttestclient/auto_rest_parameterized_host_test_client.py
+++ src/generator/AutoRest.Python.Tests/Expected/AcceptanceTests/CustomBaseUri/autorestparameterizedhosttestclient/auto_rest_parameterized_host_test_client.py
@@ -22,7 +22,7 @@ class AutoRestParameterizedHostTestClientConfiguration(Configuration):
     attributes.
 
     :param host: A string value that is used as a global part of the
-     parameterized host
+     parameterized host. Default value: "host".
     :type host: str
     :param str filepath: Existing config
     """
@@ -53,7 +53,7 @@ class AutoRestParameterizedHostTestClient(object):
     :vartype paths: .operations.PathsOperations
 
     :param host: A string value that is used as a global part of the
-     parameterized host
+     parameterized host. Default value: "host".
     :type host: str
     :param str filepath: Existing config
     """
