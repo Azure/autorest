bc7dcdda1f1d630ffefac8ac7b96f5aff7e28f29
Laurent Mazuel <lmazuel@microsoft.com>
2016-08-15 12:29:17 -0700

    Fix #1353 + msrest 0.4.2

--- src/client/Python/msrest/msrest/serialization.py
+++ src/client/Python/msrest/msrest/serialization.py
@@ -513,6 +513,8 @@ class Serializer(object):
         :param dict attr: Object to be serialized.
         :rtype: dict or str
         """
+        if attr is None:
+            return None
         obj_type = type(attr)
         if obj_type in self.basic_types:
             return self.serialize_basic(attr, self.basic_types[obj_type])
