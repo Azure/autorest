52ac1c816e6049daf0aa4cff1fae0f5a7f3335f3
Dan Schulte <daschult@microsoft.com>
2016-10-03 11:54:20 -0700

    Add missing [Fact] to test

--- src/generator/AutoRest.AzureResourceSchema.Tests/AzureResourceSchemaAcceptanceTests.cs
+++ src/generator/AutoRest.AzureResourceSchema.Tests/AzureResourceSchemaAcceptanceTests.cs
@@ -130,6 +130,7 @@ namespace AutoRest.AzureResourceSchema.Tests
             RunSwaggerTest("Network", "2016-03-30", "network.json");
         }
         
+        [Fact]
         public static void Network_2016_09_01()
         {
             RunSwaggerTest("Network", "2016-09-01", "network.json");
