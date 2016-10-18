80833ff3f02291a3a5f985c699e130e9807a5496
Deepak Shankargouda <deshank@microsoft.com>
2016-10-18 15:41:17 -0700

    Minor code cleanup

--- src/modeler/AutoRest.Swagger/Validation/OperationParametersValidation.cs
+++ src/modeler/AutoRest.Swagger/Validation/OperationParametersValidation.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using AutoRest.Core.Logging;
 using AutoRest.Core.Properties;
 using AutoRest.Core.Validation;
