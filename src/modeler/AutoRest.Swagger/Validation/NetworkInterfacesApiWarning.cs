6f3f0d5616571d56ed615b1962bb254d161c2213
Deepak Shankargouda <deshank@microsoft.com>
2016-10-24 16:27:56 -0700

    Generic solution for unique resources

--- src/modeler/AutoRest.Swagger/Validation/NetworkInterfacesApiWarning.cs
+++ src/modeler/AutoRest.Swagger/Validation/NetworkInterfacesApiWarning.cs
@@ -8,53 +8,22 @@ using AutoRest.Swagger.Model;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System;
 
 namespace AutoRest.Swagger.Validation
 {
     public class NetworkInterfacesApiWarning : TypedRule<Dictionary<string, Dictionary<string, Operation>>>
     {
-        private static readonly List<string> NetworkApiNamespaces = new List<string>(new string[]{"microsoft.compute", "microsoft.network"});
+        // Ignore everything within /es except when we find microsoft.* and capture that group
+        private readonly Regex apiRegExp = new Regex(@"(?:/[^/]+)+/(?i)microsoft(?-i).([^/]+)(?:/[^/]+)+");
+
         /// <summary>
-        /// This rule passes if the paths contain reference to either Microsoft.Network Apis or 
-        /// Microsoft.Compute Apis but not both
+        /// This rule passes if the paths contain reference to exactly one of the namespace resources
         /// </summary>
         /// <param name="paths"></param>
         /// <returns></returns>
-        public override bool IsValid(Dictionary<string, Dictionary<string, Operation>> paths)
-        {
-            Regex apiRegExp = new Regex(@"/([^/]+)+");
-        
-            string firstMatch = null;
-
-            foreach (var path in paths.Keys)
-            {
-                // for each path, check if it exists in our list of namespaces and 
-                // ensure it's unique for the given json
-                // regex finds all strings of the form /<str> from the path 
-                // firstMatch ensures there is only one kind of namespace referenced
-                for (Match m = apiRegExp.Match(path); m.Success; m = m.NextMatch())
-                {
-                    string val = m.Groups[1].Value.ToLowerInvariant();
-                    if (!NetworkApiNamespaces.Contains(val))
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        if (firstMatch == null)
-                        {
-                            firstMatch = val;
-                        }
-                        else if (firstMatch != val)
-                        {
-                            return false;
-                        }
-                    }
-                }
-
-            }
-            return true;
-        }
+        public override bool IsValid(Dictionary<string, Dictionary<string, Operation>> paths) 
+            => paths.Keys.Select(path => { return apiRegExp.Match(path).Success ? apiRegExp.Match(path).Groups?[0]?.Value : null; }).Except(new List<string> { null }).Distinct().Count() <= 1;
 
         /// <summary>
         /// The template message for this Rule. 
