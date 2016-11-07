754fa6e5601fb8923f3266447632bbed9b019071
Johannes Bader <jobader@microsoft.com>
2016-11-07 13:44:37 -0800

    preview tool

---
+++ src/dev/AutoRest.Preview/AutoRestPreviewForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿using System.IO;
+
+namespace AutoRestPreview
+{
+    partial class AutoRestPreviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoRestPreviewForm));
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.panelDst = new System.Windows.Forms.Panel();
+            this.scintillaDst = new ScintillaNET.Scintilla();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelTargetLang = new System.Windows.Forms.Label();
+            this.comboBoxTargetLang = new System.Windows.Forms.ComboBox();
+            this.panelError = new System.Windows.Forms.Panel();
+            this.labelError = new System.Windows.Forms.Label();
+            this.panelSrc = new System.Windows.Forms.Panel();
+            this.scintillaSrc = new ScintillaNET.Scintilla();
+            this.checkBoxShort = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.panelDst.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelError.SuspendLayout();
+            this.panelSrc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.Controls.Add(this.panelDst, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.panelSrc, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(1011, 738);
+            this.tableLayoutPanel.TabIndex = 3;
+            // 
+            // panelDst
+            // 
+            this.panelDst.Controls.Add(this.scintillaDst);
+            this.panelDst.Controls.Add(this.panel1);
+            this.panelDst.Controls.Add(this.panelError);
+            this.panelDst.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDst.Location = new System.Drawing.Point(508, 7);
+            this.panelDst.Name = "panelDst";
+            this.panelDst.Size = new System.Drawing.Size(496, 724);
+            this.panelDst.TabIndex = 4;
+            // 
+            // scintillaDst
+            // 
+            this.scintillaDst.AdditionalSelectionTyping = true;
+            this.scintillaDst.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scintillaDst.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintillaDst.IndentationGuides = ScintillaNET.IndentView.Real;
+            this.scintillaDst.Location = new System.Drawing.Point(0, 36);
+            this.scintillaDst.MouseSelectionRectangularSwitch = true;
+            this.scintillaDst.MultipleSelection = true;
+            this.scintillaDst.Name = "scintillaDst";
+            this.scintillaDst.ScrollWidth = 160;
+            this.scintillaDst.Size = new System.Drawing.Size(496, 550);
+            this.scintillaDst.TabIndex = 6;
+            this.scintillaDst.WrapIndentMode = ScintillaNET.WrapIndentMode.Indent;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.checkBoxShort);
+            this.panel1.Controls.Add(this.labelTargetLang);
+            this.panel1.Controls.Add(this.comboBoxTargetLang);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(496, 36);
+            this.panel1.TabIndex = 8;
+            // 
+            // labelTargetLang
+            // 
+            this.labelTargetLang.ForeColor = System.Drawing.Color.Silver;
+            this.labelTargetLang.Location = new System.Drawing.Point(3, 2);
+            this.labelTargetLang.Name = "labelTargetLang";
+            this.labelTargetLang.Padding = new System.Windows.Forms.Padding(8);
+            this.labelTargetLang.Size = new System.Drawing.Size(108, 31);
+            this.labelTargetLang.TabIndex = 2;
+            this.labelTargetLang.Text = "Target Language";
+            this.labelTargetLang.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBoxTargetLang
+            // 
+            this.comboBoxTargetLang.FormattingEnabled = true;
+            this.comboBoxTargetLang.Items.AddRange(new object[] {
+            "CSharp",
+            "Azure.CSharp",
+            "Ruby",
+            "Azure.Ruby",
+            "NodeJS",
+            "Azure.NodeJS",
+            "Python",
+            "Azure.Python"});
+            this.comboBoxTargetLang.Location = new System.Drawing.Point(117, 8);
+            this.comboBoxTargetLang.Name = "comboBoxTargetLang";
+            this.comboBoxTargetLang.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxTargetLang.TabIndex = 0;
+            this.comboBoxTargetLang.SelectedIndexChanged += new System.EventHandler(this.comboBoxTargetLang_SelectedIndexChanged);
+            // 
+            // panelError
+            // 
+            this.panelError.BackColor = System.Drawing.Color.Transparent;
+            this.panelError.Controls.Add(this.labelError);
+            this.panelError.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelError.Location = new System.Drawing.Point(0, 586);
+            this.panelError.Name = "panelError";
+            this.panelError.Size = new System.Drawing.Size(496, 138);
+            this.panelError.TabIndex = 7;
+            // 
+            // labelError
+            // 
+            this.labelError.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelError.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelError.Location = new System.Drawing.Point(0, 0);
+            this.labelError.Name = "labelError";
+            this.labelError.Padding = new System.Windows.Forms.Padding(8);
+            this.labelError.Size = new System.Drawing.Size(496, 138);
+            this.labelError.TabIndex = 0;
+            this.labelError.Text = "labelError";
+            // 
+            // panelSrc
+            // 
+            this.panelSrc.Controls.Add(this.scintillaSrc);
+            this.panelSrc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelSrc.Location = new System.Drawing.Point(7, 7);
+            this.panelSrc.Name = "panelSrc";
+            this.panelSrc.Size = new System.Drawing.Size(495, 724);
+            this.panelSrc.TabIndex = 4;
+            // 
+            // scintillaSrc
+            // 
+            this.scintillaSrc.AdditionalSelectionTyping = true;
+            this.scintillaSrc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scintillaSrc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scintillaSrc.IndentationGuides = ScintillaNET.IndentView.Real;
+            this.scintillaSrc.Lexer = ScintillaNET.Lexer.Python;
+            this.scintillaSrc.Location = new System.Drawing.Point(0, 0);
+            this.scintillaSrc.MouseSelectionRectangularSwitch = true;
+            this.scintillaSrc.MultipleSelection = true;
+            this.scintillaSrc.Name = "scintillaSrc";
+            this.scintillaSrc.ScrollWidth = 255;
+            this.scintillaSrc.Size = new System.Drawing.Size(495, 724);
+            this.scintillaSrc.TabIndex = 5;
+            this.scintillaSrc.TabWidth = 2;
+            this.scintillaSrc.Text = File.ReadAllText("default.yaml");
+            this.scintillaSrc.WrapIndentMode = ScintillaNET.WrapIndentMode.Indent;
+            this.scintillaSrc.TextChanged += new System.EventHandler(this.scintillaSrc_TextChanged);
+            // 
+            // checkBoxShort
+            // 
+            this.checkBoxShort.AutoSize = true;
+            this.checkBoxShort.ForeColor = System.Drawing.Color.Silver;
+            this.checkBoxShort.Location = new System.Drawing.Point(290, 10);
+            this.checkBoxShort.Name = "checkBoxShort";
+            this.checkBoxShort.Size = new System.Drawing.Size(82, 17);
+            this.checkBoxShort.TabIndex = 3;
+            this.checkBoxShort.Text = "short output";
+            this.checkBoxShort.UseVisualStyleBackColor = true;
+            this.checkBoxShort.CheckedChanged += new System.EventHandler(this.checkBoxShort_CheckedChanged);
+            // 
+            // AutoRestPreviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 738);
+            this.Controls.Add(this.tableLayoutPanel);
+            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutoRestPreviewForm";
+            this.Text = "AutoRest Preview";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.AutoRestPreviewForm_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.panelDst.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelError.ResumeLayout(false);
+            this.panelSrc.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Panel panelDst;
+        private System.Windows.Forms.Panel panelError;
+        private ScintillaNET.Scintilla scintillaDst;
+        private System.Windows.Forms.Panel panelSrc;
+        private ScintillaNET.Scintilla scintillaSrc;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelTargetLang;
+        private System.Windows.Forms.ComboBox comboBoxTargetLang;
+        private System.Windows.Forms.CheckBox checkBoxShort;
+    }
+}
+
---
+++ src/dev/AutoRest.Preview/AutoRestPreviewForm.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoRest.Core;
+using AutoRest.Core.Extensibility;
+using AutoRest.Core.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CSharp;
+using Microsoft.Rest.CSharp.Compiler.Compilation;
+using ScintillaNET;
+using OutputKind = Microsoft.Rest.CSharp.Compiler.Compilation.OutputKind;
+using static AutoRest.Core.Utilities.DependencyInjection;
+
+namespace AutoRestPreview
+{
+    public partial class AutoRestPreviewForm : Form
+    {
+        private readonly string autoRestJson;
+
+        public AutoRestPreviewForm()
+        {
+            InitializeComponent();
+
+            autoRestJson = File.ReadAllText("AutoRest.json");
+        }
+
+        protected async Task<CompilationResult> Compile(IFileSystem fileSystem)
+        {
+            var compiler = new CSharpCompiler(
+                fileSystem.GetFiles("GeneratedCode", "*.cs", SearchOption.AllDirectories)
+                    .Select(each => new KeyValuePair<string, string>(each, fileSystem.ReadFileAsText(each))).ToArray(),
+                ManagedAssets.FrameworkAssemblies.Concat(
+                    AppDomain.CurrentDomain.GetAssemblies()
+                        .Where(each => !each.IsDynamic && !string.IsNullOrEmpty(each.Location))
+                        .Select(each => each.Location)
+                        .Concat(new[]
+                        {
+                            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                "Microsoft.Rest.ClientRuntime.dll"),
+                            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                "Microsoft.Rest.ClientRuntime.Azure.dll")
+                        })
+                ));
+
+            return await compiler.Compile(OutputKind.DynamicallyLinkedLibrary);
+        }
+
+        private MemoryFileSystem GenerateCodeForTest(string json, string codeGenerator)
+        {
+            using (NewContext)
+            {
+                var fs = new MemoryFileSystem();
+                var settings = new Settings
+                {
+                    Modeler = "Swagger",
+                    CodeGenerator = codeGenerator,
+                    FileSystem = fs,
+                    OutputDirectory = "GeneratedCode",
+                    Namespace = "Test",
+                    Input = "input.json"
+                };
+
+                fs.WriteFile(settings.Input, json);
+                fs.WriteFile("AutoRest.json", autoRestJson);
+
+                return GenerateCodeInto(fs, settings);
+            }
+        }
+
+        private static MemoryFileSystem GenerateCodeInto(MemoryFileSystem fileSystem, Settings settings)
+        {
+            var plugin = ExtensionsLoader.GetPlugin();
+            var modeler = ExtensionsLoader.GetModeler();
+            var codeModel = modeler.Build();
+
+            // After swagger Parser
+            codeModel = AutoRestController.RunExtensions(Trigger.AfterModelCreation, codeModel);
+
+            // After swagger Parser
+            codeModel = AutoRestController.RunExtensions(Trigger.BeforeLoadingLanguageSpecificModel, codeModel);
+
+            using (plugin.Activate())
+            {
+                // load model into language-specific code model
+                codeModel = plugin.Serializer.Load(codeModel);
+
+                // we've loaded the model, run the extensions for after it's loaded
+                codeModel = AutoRestController.RunExtensions(Trigger.AfterLoadingLanguageSpecificModel, codeModel);
+
+                // apply language-specific tranformation (more than just language-specific types)
+                // used to be called "NormalizeClientModel" . 
+                codeModel = plugin.Transformer.TransformCodeModel(codeModel);
+
+                // next set of extensions
+                codeModel = AutoRestController.RunExtensions(Trigger.AfterLanguageSpecificTransform, codeModel);
+
+                // next set of extensions
+                codeModel = AutoRestController.RunExtensions(Trigger.BeforeGeneratingCode, codeModel);
+
+                // Generate code from CodeModel.
+                plugin.CodeGenerator.Generate(codeModel).GetAwaiter().GetResult();
+            }
+
+            return fileSystem;
+        }
+
+        internal static string[] SuppressWarnings = {"CS1701", "CS1591"};
+
+        private async Task<string> Regenerate(string swagger, string language = "CSharp", bool shortVersion = true)
+        {
+            using (var fileSystem = GenerateCodeForTest(swagger, language))
+            {
+                // concat all source
+                var allSources = string.Join("\n\n\n",
+                    fileSystem
+                        .VirtualStore
+                        .Where(file => file.Key.Contains(".") && !file.Key.EndsWith(".json"))
+                        .Select(file => $"//\n// {file.Key}\n//\n\n{file.Value.ToString()}"));
+
+                if (!shortVersion)
+                {
+                    return allSources;
+                }
+
+                switch (language)
+                {
+                    case "CSharp":
+                    case "Azure.CSharp":
+                        // compile
+                        var result = await Compile(fileSystem);
+
+                        // filter the warnings
+                        var warnings = result.Messages.Where(
+                            each => each.Severity == DiagnosticSeverity.Warning
+                                    && !SuppressWarnings.Contains(each.Id)).ToArray();
+
+                        // filter the errors
+                        var errors = result.Messages.Where(each => each.Severity == DiagnosticSeverity.Error).ToArray();
+                        
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception("compilation failed: " + string.Join(", ", errors.Concat(warnings)));
+                        }
+
+                        // try to load the assembly
+                        var asm = Assembly.Load(result.Output.GetBuffer());
+                        if (asm == null)
+                        {
+                            throw new Exception("could not load assembly");
+                        }
+
+                        var types = asm.ExportedTypes;
+                        return string.Join("\n\n", types.Select(CreateTypeSummary));
+                        
+                    default:
+                        return allSources;
+                }
+            }
+        }
+
+        private string GetSimpleName(Type type)
+        {
+            var compiler = new CSharpCodeProvider();
+            var result = compiler.GetTypeOutput(new CodeTypeReference(type));
+            result = Regex.Replace(result, @"[_A-Za-z0-9]+\.", "");
+            result = Regex.Replace(result, @"Nullable<(.+?)>", @"$1?");
+            return result;
+        }
+
+        private string CreateTypeSummary(Type type)
+        {
+            int indentLevel = scintillaDst.TabWidth;
+
+            var sb = new StringBuilder();
+            sb.Append("class ");
+            sb.Append(type.Name);
+            sb.AppendLine();
+            sb.AppendLine("{");
+            foreach (var member in type.GetProperties())
+            {
+                sb.Append(' ', indentLevel);
+                sb.Append(GetSimpleName(member.PropertyType));
+                sb.Append(' ');
+                sb.Append(member.Name);
+                sb.AppendLine(";");
+            }
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        private void SetupCintilla(Scintilla scintilla)
+        {
+            scintilla.StyleResetDefault();
+            scintilla.Styles[Style.Default].Font = "Consolas";
+            scintilla.Styles[Style.Default].Size = 10;
+            scintilla.StyleClearAll();
+
+            // Configure the CPP (C#) lexer styles
+            scintilla.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
+            scintilla.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla.Styles[Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+            scintilla.Styles[Style.Cpp.Number].ForeColor = Color.Olive;
+            scintilla.Styles[Style.Cpp.Word].ForeColor = Color.Blue;
+            scintilla.Styles[Style.Cpp.Word2].ForeColor = Color.Blue;
+            scintilla.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
+            scintilla.Styles[Style.Cpp.Operator].ForeColor = Color.Purple;
+            scintilla.Styles[Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+            scintilla.Lexer = Lexer.Cpp;
+
+            if (true)
+            {
+                BackColor = Color.FromArgb(0, 0, 0);
+                scintilla.Styles[Style.Default].ForeColor = Color.Silver;
+                scintilla.Styles[Style.Default].BackColor = Color.FromArgb(30, 30, 30);
+                scintilla.CaretForeColor = Color.Silver;
+                scintilla.SetSelectionBackColor(true, Color.FromArgb(50, 80, 140));
+
+                scintilla.StyleClearAll();
+                scintilla.Styles[Style.IndentGuide].BackColor = Color.FromArgb(50, 50, 50);
+                scintilla.Styles[Style.IndentGuide].ForeColor = Color.FromArgb(50, 50, 50);
+
+                // Configure the CPP (C#) lexer styles
+                scintilla.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
+                scintilla.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+                scintilla.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+                scintilla.Styles[Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+                scintilla.Styles[Style.Cpp.Number].ForeColor = Color.Olive;
+                scintilla.Styles[Style.Cpp.Word].ForeColor = Color.CornflowerBlue;
+                scintilla.Styles[Style.Cpp.Word2].ForeColor = Color.CornflowerBlue;
+                scintilla.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+                scintilla.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+                scintilla.Styles[Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
+                scintilla.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
+                scintilla.Styles[Style.Cpp.Operator].ForeColor = Color.Purple;
+                scintilla.Styles[Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+                scintilla.Lexer = Lexer.Cpp;
+            }
+
+            //scintilla.Margins[1].Width = 0;
+            scintilla.Margins[1].Type = MarginType.BackColor;
+            //scintilla.Margins[0].Width = 16;
+        }
+
+        private async Task UpdatePreview()
+        {
+            try
+            {
+                string result = await Regenerate(scintillaSrc.Text, comboBoxTargetLang.SelectedItem as string ?? "CSharp", checkBoxShort.Checked);
+                int scrollStart = scintillaDst.FirstVisibleLine;
+                scintillaDst.ReadOnly = false;
+                scintillaDst.Text = result;
+                scintillaDst.ReadOnly = true;
+                scintillaDst.LineScroll(scrollStart, 0);
+
+                panelError.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                labelError.Text = $@"{ex.GetType().Name}: {ex.Message}";
+                panelError.Visible = true;
+            }
+        }
+
+        private void AutoRestPreviewForm_Load(object sender, EventArgs ea)
+        {
+            SetupCintilla(scintillaSrc);
+            SetupCintilla(scintillaDst);
+
+            scintillaSrc.InsertCheck += (o, e) =>
+            {
+                if ((e.Text.EndsWith("\r") || e.Text.EndsWith("\n")))
+                {
+                    var curLine = scintillaSrc.LineFromPosition(e.Position);
+                    var curLineText = scintillaSrc.Lines[curLine].Text;
+
+                    var indent = Regex.Match(curLineText, @"^\s*");
+                    e.Text += indent.Value;
+                }
+            };
+
+            scintillaDst.SetKeywords(0,
+                "abstract as base break case catch checked continue default delegate do else event explicit extern false finally fixed for foreach goto if implicit in interface internal is lock namespace new null object operator out override params partial private protected public readonly ref return sealed sizeof stackalloc switch this throw true try typeof unchecked unsafe using virtual while");
+            scintillaDst.SetKeywords(1,
+                "bool byte char class const decimal double enum float int long sbyte short static string struct uint ulong ushort void");
+            scintillaSrc.SetKeywords(0, "false true");
+
+            comboBoxTargetLang.SelectedItem = comboBoxTargetLang.Items[0];
+        }
+
+        private async void scintillaSrc_TextChanged(object sender, EventArgs e)
+        {
+            await UpdatePreview();
+        }
+
+        private async void comboBoxTargetLang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            await UpdatePreview();
+        }
+
+        private async void checkBoxShort_CheckedChanged(object sender, EventArgs e)
+        {
+            await UpdatePreview();
+        }
+    }
+}
