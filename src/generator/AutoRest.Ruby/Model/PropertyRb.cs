5dc0687b4e0b4c9fcc22ac0e6eff7e62fe18e103
Vishrut Shah <vissha@microsoft.com>
2016-11-14 11:34:17 -0800

    Override only SerializedName for the Property

--- src/generator/AutoRest.Ruby/Model/PropertyRb.cs
+++ src/generator/AutoRest.Ruby/Model/PropertyRb.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using AutoRest.Core;
-using AutoRest.Core.Utilities;
 using AutoRest.Extensions;
 
 namespace AutoRest.Ruby.Model
@@ -14,20 +12,10 @@ namespace AutoRest.Ruby.Model
     {
         protected PropertyRb()
         {
-            // properties with dots by surrounding them with single quotes to represent 
-            // them as a single property and not as one being part of another. For example: 'odata.nextLink' 
-            Name.OnGet += name =>
-            {
-                // completely replacing base behavior.
-
-                // use either the overriden client name or the raw value
-                name = Extensions.GetValue<string>("x-ms-client-name").Else(Name.RawValue);
-
-                return CodeNamer.Instance.GetPropertyName(name);
-
-            };
-
-            SerializedName.OnGet += serializedName => (this.WasFlattened() ? serializedName : serializedName?.Replace(".", "\\\\."))?.Replace("\\\\\\\\","\\\\");
+            // properties with dots in serialized name should escape . with back slash. For example: 'odata.nextLink'
+            // 1. When properties are flattened use serialized name directly
+            // 2. When properties are not flattened and have . in serialized name then escape . with back slash
+            SerializedName.OnGet += serializedName => (this.WasFlattened() ? serializedName : serializedName?.Replace(".", "\\\\."));
         }
     }
 }
