23434a2f2909381d5652045213b1270d033ffa6d
Deepak Shankargouda <deshank@microsoft.com>
2016-11-16 13:17:22 -0800

    Fixing bug 1435 (#1609)
    * Fixing bug 1435
    
    * Added test case
    
    * Minor code cleanup
    
    * undoing unused changes

---
+++ src/generator/AutoRest.CSharp.Unit.Tests/Bug1435.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AutoRest.CSharp.Unit.Tests
+{
+    public class Bug1435 : BugTest
+    {
+        public Bug1435(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        /// <summary>
+        ///     https://github.com/Azure/autorest/issues/1423
+        ///     Verifies autorest succesfully generates code for empty path object
+        /// </summary>
+        [Fact]
+        public async Task EmptyPathObjectCodeGenerationTest()
+        {
+            // simplified test pattern for unit testing aspects of code generation
+            using (var fileSystem = GenerateCodeForTestFromSpec())
+            {
+                // check for the expected class.
+                Assert.True(fileSystem.FileExists(@"GeneratedCode\APIwithreferencedformdata.cs"));
+
+                var result = await Compile(fileSystem);
+
+                // filter the warnings
+                var warnings = result.Messages.Where(
+                    each => each.Severity == DiagnosticSeverity.Warning
+                            && !SuppressWarnings.Contains(each.Id)).ToArray();
+
+                // filter the errors
+                var errors = result.Messages.Where(each => each.Severity == DiagnosticSeverity.Error).ToArray();
+
+                Write(warnings, fileSystem);
+                Write(errors, fileSystem);
+
+                // Don't proceed unless we have zero Warnings.
+                Assert.Empty(warnings);
+
+                // Don't proceed unless we have zero Errors.
+                Assert.Empty(errors);
+
+                // Should also succeed.
+                Assert.True(result.Succeeded);
+
+            }
+        }
+    }
+}
