56a3a6a8c1d9cfa35556458e5e853267806fbe48
Johannes Bader <jobader@microsoft.com>
2016-11-16 18:11:20 -0800

    adding readonly required rule (#1374)

---
+++ src/modeler/AutoRest.Swagger/Validation/NotBothReadonlyRequired.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using AutoRest.Core.Logging;
+using AutoRest.Core.Properties;
+using AutoRest.Core.Validation;
+using AutoRest.Swagger.Model;
+using System.Linq;
+
+namespace AutoRest.Swagger.Validation
+{
+    public class NotBothReadonlyRequired : TypedRule<Schema>
+    {
+        /// <summary>
+        /// An <paramref name="entity" /> fails this rule if it is marked as both readonly and required.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public override bool IsValid(Schema entity) =>
+            !(entity.ReadOnly && entity.IsRequired);
+
+        /// <summary>
+        /// The template message for this Rule.
+        /// </summary>
+        /// <remarks>
+        /// This may contain placeholders '{0}' for parameterized messages.
+        /// </remarks>
+        public override string MessageTemplate => "This entity is marked both as readonly and required, which is illegal.";
+
+        /// <summary>
+        /// The severity of this message (ie, debug/info/warning/error/fatal, etc)
+        /// </summary>
+        public override LogEntrySeverity Severity => LogEntrySeverity.Error;
+    }
+}
