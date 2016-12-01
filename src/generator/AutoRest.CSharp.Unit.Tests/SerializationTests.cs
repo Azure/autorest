4ba39ca24cb9c0c87b614bec4385c0164ccc06a2
Bastien <fake.account@fake.acc>
2016-12-01 20:36:04 +0100

    Change - Simplification of resource unit test file and change language from YAML to JSON.

--- src/generator/AutoRest.CSharp.Unit.Tests/SerializationTests.cs
+++ src/generator/AutoRest.CSharp.Unit.Tests/SerializationTests.cs
@@ -10,7 +10,7 @@ namespace AutoRest.CSharp.Unit.Tests
         [Fact]
         public void ParseSecurityDefinitionType()
         {
-            var swaggerContent = File.ReadAllText(@"Resource\SerializationTests\SerializationTests.yaml");
+            var swaggerContent = File.ReadAllText(@"Resource\SerializationTests\SerializationTests.json");
             var definition = SwaggerParser.Parse(swaggerContent);
             Assert.Equal(SecuritySchemeType.OAuth2, definition.SecurityDefinitions["petstore_auth"].SecuritySchemeType);
             Assert.Equal(SecuritySchemeType.ApiKey, definition.SecurityDefinitions["api_key"].SecuritySchemeType);
