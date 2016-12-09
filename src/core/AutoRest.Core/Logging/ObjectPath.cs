3d28034425bc93c7681c325b7b7e1d728eff27c7
Johannes Bader <jobader@microsoft.com>
2016-12-09 12:15:05 -0800

    explode ObjectPath.cs

--- src/core/AutoRest.Core/Logging/ObjectPath.cs
+++ src/core/AutoRest.Core/Logging/ObjectPath.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using YamlDotNet.RepresentationModel;
 
 namespace AutoRest.Core.Logging
@@ -18,20 +20,11 @@ namespace AutoRest.Core.Logging
             Path = path;
         }
 
-        private ObjectPath Append(ObjectPathPart part)
-        {
-            return new ObjectPath(Path.Concat(new[] { part }));
-        }
+        private ObjectPath Append(ObjectPathPart part) => new ObjectPath(Path.Concat(new[] { part }));
 
-        public ObjectPath AppendIndex(int index)
-        {
-            return Append(new ObjectPathPartIndex(index));
-        }
+        public ObjectPath AppendIndex(int index) => Append(new ObjectPathPartIndex(index));
 
-        public ObjectPath AppendProperty(string property)
-        {
-            return Append(new ObjectPathPartProperty(property));
-        }
+        public ObjectPath AppendProperty(string property) => Append(new ObjectPathPartProperty(property));
 
         public IEnumerable<ObjectPathPart> Path { get; }
         
@@ -45,60 +38,6 @@ namespace AutoRest.Core.Logging
                 result = part.SelectNode(ref node) ?? result;
             }
             return result;
-        }
-    }
-
-    public abstract class ObjectPathPart
-    {
-        public abstract string XPath { get; }
-
-        /// <summary>
-        /// Selects the child node according to this path part.
-        /// Returns null if such node was not found.
-        /// </summary>
-        public abstract YamlNode SelectNode(ref YamlNode node);
-    }
-
-    public class ObjectPathPartIndex : ObjectPathPart
-    {
-        public ObjectPathPartIndex(int index)
-        {
-            Index = index;
-        }
-
-        public int Index { get; }
-
-        public override string XPath => $"[{Index + 1}]";
-
-        public override YamlNode SelectNode(ref YamlNode node)
-        {
-            var snode = node as YamlSequenceNode;
-            node = snode != null && 0 <= Index && Index < snode.Children.Count
-                ? snode.Children[Index]
-                : null;
-            return node;
-        }
-    }
-
-    public class ObjectPathPartProperty : ObjectPathPart
-    {
-        private static string SanitizeXPathProperty(string property) => property.Replace("/", "~1");
-
-        public ObjectPathPartProperty(string property)
-        {
-            Property = property;
-        }
-
-        public string Property { get; }
-
-        public override string XPath => $"/{SanitizeXPathProperty(Property)}";
-
-        public override YamlNode SelectNode(ref YamlNode node)
-        {
-            var child = (node as YamlMappingNode)?.
-                Children?.FirstOrDefault(pair => pair.Key.ToString().Equals(Property, StringComparison.InvariantCultureIgnoreCase));
-            node = child?.Value;
-            return child?.Key;
         }
     }
 }
