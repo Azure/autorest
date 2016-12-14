6b6dab3eec9d82a7579bcea919d1c820d285a40e
Johannes Bader <jobader@microsoft.com>
2016-12-13 21:00:27 -0800

    cleanup

--- src/generator/AutoRest.Java/CodeNamerJv.cs
+++ src/generator/AutoRest.Java/CodeNamerJv.cs
@@ -20,8 +20,6 @@ namespace AutoRest.Java
         public const string ExternalExtension = "x-ms-external";
 
         public static HashSet<string> PrimaryTypes { get; private set; }
-
-        public static HashSet<string> JavaBuiltInTypes { get; private set; }
         
         #region constructor
 
@@ -68,7 +66,6 @@ namespace AutoRest.Java
                 "BigDecimal",
                 "InputStream"
             }.ForEach(s => PrimaryTypes.Add(s));
-            JavaBuiltInTypes = new HashSet<string>();
             new HashSet<string>
             {
                 "int",
@@ -84,17 +81,6 @@ namespace AutoRest.Java
         #endregion
 
         #region naming
-
-        ///// <summary>
-        ///// Resolves name collisions in the client model for method groups (operations).
-        ///// </summary>
-        ///// <param name="serviceClient"></param>
-        ///// <param name="exclusionDictionary"></param>
-        //protected override void ResolveMethodGroupNameCollision(CodeModel serviceClient,
-        //    Dictionary<string, string> exclusionDictionary)
-        //{
-        //    // do nothing
-        //}
 
         public override string GetFieldName(string name)
         {
@@ -178,151 +164,6 @@ namespace AutoRest.Java
         #endregion
 
         #region normalization
-        
-        //public override void NormalizeClientModel(CodeModel client)
-        //{
-        //    if (client == null)
-        //    {
-        //        throw new ArgumentNullException("client");
-        //    }
-
-        //    base.NormalizeClientModel(client);
-        //    foreach (var method in client.Methods)
-        //    {
-        //        if (method.Group != null)
-        //        {
-        //            method.Group = method.Group.ToCamelCase();
-        //        }
-        //        foreach (var parameter in method.Parameters)
-        //        {
-        //            if (parameter.ClientProperty != null)
-        //            {
-        //                parameter.Name = string.Format(CultureInfo.InvariantCulture,
-        //                    "{0}.{1}()",
-        //                    method.Group == null ? "this" : "this.client",
-        //                    parameter.ClientProperty.Name.ToCamelCase());
-        //            }
-
-        //            if (!parameter.IsRequired)
-        //            {
-        //                parameter.Type = WrapPrimitiveType(parameter.Type);
-        //            }
-        //        }
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Normalizes the parameter names of a method
-        ///// </summary>
-        ///// <param name="method"></param>
-        //protected override void NormalizeParameters(Method method)
-        //{
-        //    if (method != null)
-        //    {
-        //        foreach (var parameter in method.Parameters)
-        //        {
-        //            parameter.Name = method.Scope.GetUniqueName(GetParameterName(parameter.GetClientName()));
-        //            parameter.Type = NormalizeTypeReference(parameter.Type);
-        //            QuoteParameter(parameter);
-        //        }
-
-        //        foreach (var parameterTransformation in method.InputParameterTransformation)
-        //        {
-        //            parameterTransformation.OutputParameter.Name = method.Scope.GetUniqueName(GetParameterName(parameterTransformation.OutputParameter.GetClientName()));
-        //            parameterTransformation.OutputParameter.Type = NormalizeTypeReference(parameterTransformation.OutputParameter.Type);
-        //            parameterTransformation.OutputParameter = new ParameterModel(parameterTransformation.OutputParameter, method);
-
-        //            QuoteParameter(parameterTransformation.OutputParameter);
-
-        //            foreach (var parameterMapping in parameterTransformation.ParameterMappings)
-        //            {
-        //                parameterMapping.InputParameter.Name = GetParameterName(parameterMapping.InputParameter.GetClientName());
-        //                if (parameterMapping.InputParameterProperty != null)
-        //                {
-        //                    parameterMapping.InputParameterProperty = GetPropertyName(parameterMapping.InputParameterProperty);
-        //                }
-
-        //                if (parameterMapping.OutputParameterProperty != null)
-        //                {
-        //                    parameterMapping.OutputParameterProperty = GetPropertyName(parameterMapping.OutputParameterProperty);
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Normalizes the client properties names of a client model
-        ///// </summary>
-        ///// <param name="client">A client model</param>
-        //protected override void NormalizeClientProperties(ServiceClient client)
-        //{
-        //    if (client != null)
-        //    {
-        //        foreach (var property in client.Properties)
-        //        {
-        //            property.Name = GetPropertyName(property.GetClientName());
-        //            property.Type = NormalizeTypeReference(property.Type);
-        //            QuoteParameter(property);
-        //        }
-        //    }
-        //}
-        //public override Response NormalizeTypeReference(Response typePair)
-        //{
-        //    return new Response((IModelTypeJv) NormalizeTypeReference(typePair.Body),
-        //                        (IModelTypeJv) NormalizeTypeReference(typePair.Headers));
-        //}
-
-        //public override IType NormalizeTypeDeclaration(IType type)
-        //{
-        //    if (type == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    if (type is IModelTypeJv)
-        //    {
-        //        return type;
-        //    }
-        //    if (_visited.ContainsKey(type))
-        //    {
-        //        return _visited[type];
-        //    }
-
-        //    if (type is PrimaryType)
-        //    {
-        //        _visited[type] = new PrimaryTypeModel(type as PrimaryType);
-        //        return _visited[type];
-        //    }
-        //    if (type is SequenceType)
-        //    {
-        //        SequenceTypeModel model = new SequenceTypeModel(type as SequenceType);
-        //        _visited[type] = model;
-        //        return NormalizeSequenceType(model);
-        //    }
-        //    if (type is DictionaryType)
-        //    {
-        //        DictionaryTypeModel model = new DictionaryTypeModel(type as DictionaryType);
-        //        _visited[type] = model;
-        //        return NormalizeDictionaryType(model);
-        //    }
-        //    if (type is CompositeType)
-        //    {
-        //        CompositeTypeModel model = NewCompositeTypeModel(type as CompositeType);
-        //        _visited[type] = model;
-        //        return NormalizeCompositeType(model);
-        //    }
-        //    if (type is EnumType)
-        //    {
-        //        EnumTypeModel model = NewEnumTypeModel(type as EnumType);
-        //        _visited[type] = model;
-        //        return NormalizeEnumType(model);
-        //    }
-
-
-        //    throw new NotSupportedException(string.Format(CultureInfo.InvariantCulture,
-        //        "Type {0} is not supported.", type.GetType()));
-        //}
 
         public override IModelType NormalizeTypeReference(IModelType type)
         {
@@ -340,102 +181,9 @@ namespace AutoRest.Java
             return NormalizeTypeDeclaration(type);
         }
 
-        //private IType NormalizeEnumType(EnumType enumType)
-        //{
-        //    enumType.Name = GetTypeName(enumType.Name);
-
-        //    for (int i = 0; i < enumType.Values.Count; i++)
-        //    {
-        //        enumType.Values[i].Name = GetEnumMemberName(enumType.Values[i].Name);
-        //    }
-        //    return enumType;
-        //}
-
-        //protected virtual IType NormalizeCompositeType(CompositeType compositeType)
-        //{
-        //    compositeType.Name = GetTypeName(compositeType.Name);
-
-        //    foreach (var property in compositeType.Properties)
-        //    {
-        //        property.Name = GetPropertyName(property.GetClientName());
-        //        property.Type = NormalizeTypeReference(property.Type);
-        //        if (!property.IsRequired)
-        //        {
-        //            property.Type = WrapPrimitiveType(property.Type);
-        //        }
-        //    }
-
-        //    if (compositeType.BaseModelType != null) {
-        //        compositeType.BaseModelType = (CompositeType) NormalizeTypeReference(compositeType.BaseModelType);
-        //    }
-
-        //    return compositeType;
-        //}
-
-        //public static PrimaryTypeModel NormalizePrimaryType(PrimaryType primaryType)
-        //{
-        //    if (primaryType == null)
-        //    {
-        //        throw new ArgumentNullException("primaryType");
-        //    }
-
-        //    return new PrimaryTypeModel(primaryType);
-        //}
-
-        //private IType NormalizeSequenceType(SequenceType sequenceType)
-        //{
-        //    sequenceType.ElementType = WrapPrimitiveType(NormalizeTypeReference(sequenceType.ElementType));
-        //    sequenceType.NameFormat = "List<{0}>";
-        //    return sequenceType;
-        //}
-
-        //private IType NormalizeDictionaryType(DictionaryType dictionaryType)
-        //{
-        //    dictionaryType.ValueType = WrapPrimitiveType(NormalizeTypeReference(dictionaryType.ValueType));
-        //    dictionaryType.NameFormat = "Map<String, {0}>";
-        //    return dictionaryType;
-        //}
-
         #endregion
 
         #region type handling
-
-        //public static IType WrapPrimitiveType(IModelType type)
-        //{
-        //    var primaryType = type as PrimaryType;
-        //    if (primaryType != null)
-        //    {
-        //        if (type.Name == "boolean")
-        //        {
-        //            primaryType.Name = "Boolean";
-        //        }
-        //        else if (type.Name == "double")
-        //        {
-        //            primaryType.Name = "Double";
-        //        }
-        //        else if (type.Name == "int")
-        //        {
-        //            primaryType.Name = "Integer";
-        //        }
-        //        else if (type.Name == "long")
-        //        {
-        //            primaryType.Name = "Long";
-        //        }
-
-        //        return primaryType;
-        //    }
-        //    else if (type == null)
-        //    {
-        //        return new PrimaryType(KnownPrimaryType.None)
-        //        {
-        //            Name = "Void"
-        //        };
-        //    }
-        //    else
-        //    {
-        //        return type;
-        //    }
-        //}
 
         public static string GetJavaException(string exception, CodeModel cm)
         {
