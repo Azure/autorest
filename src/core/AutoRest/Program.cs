d71eb55f13b4acfeb1754b3f50ad95965557bbf4
Johannes Bader <jobader@microsoft.com>
2016-12-16 13:38:32 -0800

    ensure Settings.Namespace != null

--- src/core/AutoRest/Program.cs
+++ src/core/AutoRest/Program.cs
@@ -10,6 +10,7 @@ using AutoRest.Core.Utilities;
 using AutoRest.Properties;
 using AutoRest.Simplify;
 using static AutoRest.Core.Utilities.DependencyInjection;
+using System.IO;
 
 namespace AutoRest
 {
@@ -28,6 +29,23 @@ namespace AutoRest
                     try
                     {
                         settings = Settings.Create(args);
+                        // determine some reasonable default namespace
+                        if (settings.Namespace == null)
+                        {
+                            if (settings.Input != null)
+                            {
+                                settings.Namespace = Path.GetFileNameWithoutExtension(settings.Input);
+                            }
+                            else if (settings.InputFolder != null)
+                            {
+                                settings.Namespace = Path.GetFileNameWithoutExtension(settings.InputFolder.Segments.Last().Trim('/'));
+                            }
+                            else
+                            {
+                                settings.Namespace = "default";
+                            }
+                        }
+
                         // set up logging
                         Logger.Instance.AddListener(new ConsoleLogListener(
                             settings.Debug ? Category.Debug : Category.Warning,
