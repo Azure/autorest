9d504678d29e1af468126292e90c90dd0c797483
Amar Zavery <amzavery@microsoft.com>
2016-12-20 16:09:57 -0800

    Refactored GetParentDir() based on review feedback (#1689)
    * modified the Parse() method to accept filepath of the current swagger doc as a method parameter
    
    * refactored GetParentDir()
    
    * throw if path is null or empty string FileSystem.GetParentDir(path)
    
    * added schema for composite swagger

--- src/core/AutoRest.Core/Utilities/FileSystem.cs
+++ src/core/AutoRest.Core/Utilities/FileSystem.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using AutoRest.Core.Properties;
 using System;
 using System.IO;
 using System.Linq;
@@ -125,6 +126,10 @@ namespace AutoRest.Core.Utilities
 
         public Uri GetParentDir(string path)
         {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new Exception(Resources.PathCannotBeNullOrEmpty);
+            }
             if (IsCompletePath(path))
             {
                 return new Uri(new Uri(path), ".");
