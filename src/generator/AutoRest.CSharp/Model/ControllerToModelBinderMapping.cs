71bd0b304cc2bad4e6e2e812255ecdc2e59c1044
Deepak Shankargouda <deshank@microsoft.com>
2017-01-06 12:02:33 -0800

    Generate Server side code from specs (#1697)
    * Moved templates to separate dirs
    
    * Added CodeGeneratorMode to settings
    
    * minor change
    
    * servers service template added
    
    * server body try1
    
    * generating controller per methodgroup
    
    * service accepts returns models
    
    * minor cleanup
    
    * removing unused files
    
    * Separated common templates
    
    * fixed return types
    
    * Iteration1
    
    * removed unused
    
    * undoing more changes
    
    * Created Rest dir
    
    * stash 1
    
    * stash 2
    
    * All working except compilation errors
    
    * stash 2
    
    * add ignoring
    
    * stash 3
    
    * stash 4
    
    * undo 1
    
    * undo 2
    
    * regen code
    
    * stash 4
    
    * Added unit test for generated server code
    
    * upgraded deps
    
    * Fixed failing tests

---
+++ src/generator/AutoRest.CSharp/Model/ControllerToModelBinderMapping.cs
@@ -0,0 +1,28 @@
+using AutoRest.Core.Model;
+namespace AutoRest.CSharp.Model
+{
+
+    static class ControllerToModelBinderMapping
+    {
+        public static string GetModelBinder(ParameterLocation loc)
+        {
+            switch(loc)
+            {
+                case ParameterLocation.None:
+                    return string.Empty;
+                case ParameterLocation.Query:
+                    return "FromQuery";
+                case ParameterLocation.Header:
+                    return "FromHeader";
+                case ParameterLocation.Path:
+                    return "FromRoute";
+                case ParameterLocation.FormData:
+                    return "FromForm";
+                case ParameterLocation.Body:
+                    return "FromBody";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
