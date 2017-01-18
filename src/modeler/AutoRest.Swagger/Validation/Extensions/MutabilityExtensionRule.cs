6f49c0b1b08a64993d36e6d0e6f48077c15f4490
Vishrut Shah <vissha@microsoft.com>
2017-01-17 19:47:21 -0800

    minor code cleanup in swagger validation rule file (#1724)

--- src/modeler/AutoRest.Swagger/Validation/Extensions/MutabilityExtensionRule.cs
+++ src/modeler/AutoRest.Swagger/Validation/Extensions/MutabilityExtensionRule.cs
@@ -2,26 +2,12 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using AutoRest.Core.Logging;
-using AutoRest.Core.Validation;
-using AutoRest.Swagger.Model;
-using System.Collections.Generic;
 
 namespace AutoRest.Swagger.Validation
 {
     public abstract class MutabilityExtensionRule : ExtensionRule
     {
         protected override string ExtensionName => "x-ms-mutability";
-
-        protected static Schema Get200ResponseSchema(RuleContext context)
-        {
-            OperationResponse response = context?.GetFirstAncestor<Operation>()?.Responses?.GetValueOrNull("200");
-            if (response == null)
-            {
-                return null;
-            }
-            var resolver = new SchemaResolver(context?.GetServiceDefinition());
-            return resolver.Unwrap(response.Schema);
-        }
 
         public abstract override string MessageTemplate { get; }
 
