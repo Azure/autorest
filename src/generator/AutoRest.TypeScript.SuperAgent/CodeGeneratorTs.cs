a5ee1c0bd8da5b6f255847a25166b7ef5b40bb81
Jeno Laszlo <jeno.laszlo@agoda.com>
2017-02-08 14:13:53 +0700

    Added TS project

---
+++ src/generator/AutoRest.TypeScript.SuperAgent/CodeGeneratorTs.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoRest.Core;
+using AutoRest.Core.Model;
+using AutoRest.Core.Utilities;
+using AutoRest.TypeScript.SuperAgent.Model;
+using AutoRest.TypeScript.SuperAgent.Properties;
+
+namespace AutoRest.TypeScript.SuperAgent
+{
+    public class CodeGeneratorTs : CodeGenerator
+    {
+        private const string ClientRuntimePackage = "ms-rest version 1.15.0";
+
+        public override string ImplementationFileExtension => ".ts";
+
+        /// <summary>
+        /// Text to inform the user of required package/module/gem/jar.
+        /// </summary>
+        public override string UsageInstructions => string.Format(CultureInfo.InvariantCulture, Resources.UsageInformation, ClientRuntimePackage);
+
+        /// <summary>
+        /// Generates TypeScript code and outputs it in the file system.
+        /// </summary>
+        /// <param name="codeModel"></param>
+        /// <returns></returns>
+        public override Task Generate(CodeModel cm)
+        {
+            //var disableTypeScriptGeneration = DependencyInjection.Singleton<GeneratorSettingsTs>.Instance.DisableTypeScriptGeneration;
+
+            var codeModel = cm as CodeModelTs;
+            if (codeModel == null)
+            {
+                throw new InvalidCastException("CodeModel is not a TypeScript code model.");
+            }
+
+            throw new NotImplementedException();
+
+            /*
+
+            // Service client
+            var serviceClientTemplate = new ServiceClientTemplate { Model = codeModel };
+            await Write(serviceClientTemplate, codeModel.Name.ToCamelCase() + ".js");
+
+            if (!disableTypeScriptGeneration)
+            {
+                var serviceClientTemplateTS = new ServiceClientTemplateTS { Model = codeModel };
+                await Write(serviceClientTemplateTS, codeModel.Name.ToCamelCase() + ".d.ts");
+            }
+
+            //Models
+            if (codeModel.ModelTypes.Any())
+            {
+                var modelIndexTemplate = new ModelIndexTemplate { Model = codeModel };
+                await Write(modelIndexTemplate, Path.Combine("models", "index.js"));
+
+                if (!disableTypeScriptGeneration)
+                {
+                    var modelIndexTemplateTS = new ModelIndexTemplateTS { Model = codeModel };
+                    await Write(modelIndexTemplateTS, Path.Combine("models", "index.d.ts"));
+                }
+
+                foreach (var modelType in codeModel.ModelTemplateModels)
+                {
+                    var modelTemplate = new ModelTemplate { Model = modelType };
+                    await Write(modelTemplate, Path.Combine("models", modelType.NameAsFileName.ToCamelCase() + ".js"));
+                }
+            }
+
+            //MethodGroups
+            if (codeModel.MethodGroupModels.Any())
+            {
+                var methodGroupIndexTemplate = new MethodGroupIndexTemplate { Model = codeModel };
+                await Write(methodGroupIndexTemplate, Path.Combine("operations", "index.js"));
+
+                if (!disableTypeScriptGeneration)
+                {
+                    var methodGroupIndexTemplateTS = new MethodGroupIndexTemplateTS { Model = codeModel };
+                    await Write(methodGroupIndexTemplateTS, Path.Combine("operations", "index.d.ts"));
+                }
+
+                foreach (var methodGroupModel in codeModel.MethodGroupModels)
+                {
+                    var methodGroupTemplate = new MethodGroupTemplate { Model = methodGroupModel };
+                    await
+                        Write(methodGroupTemplate,
+                            Path.Combine("operations", methodGroupModel.TypeName.ToCamelCase() + ".ts"));
+                }
+            }*/
+        }
+    }
+}
---
+++ src/generator/AutoRest.TypeScript.SuperAgent/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoRest.TypeScript.SuperAgent")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f19209c-3a73-4864-8da3-3bc82bd4a061")]
