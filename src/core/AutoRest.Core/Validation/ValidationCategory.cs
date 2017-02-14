5f364adcfc041338eceebc5b4360b5d98233d498
Johannes Bader <jobader@microsoft.com>
2017-02-14 09:27:29 -0800

    unused imports

--- src/core/AutoRest.Core/Validation/ValidationCategory.cs
+++ src/core/AutoRest.Core/Validation/ValidationCategory.cs
@@ -2,9 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AutoRest.Core.Validation
 {
