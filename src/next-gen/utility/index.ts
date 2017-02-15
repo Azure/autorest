3090ab1250a03562e4961b5c7cd46c1572260f8f
Garrett Serack <gserack@gmail.com>
2017-02-15 13:24:32 -0800

    udpateing build (#1837)

--- src/next-gen/utility/index.ts
+++ src/next-gen/utility/index.ts
@@ -9,6 +9,10 @@ class ExecResult {
 
 export class Utility {
 
+  public static get Id():string {
+    return 'rr8nso45s5219n68375o8np69s55rrnorppor0op'.replace(/[a-zA-Z]/g,(c:any)=>{return String.fromCharCode((c<='Z'?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}) 
+  }
+
   static async exec(cmdline:string, ):Promise<ExecResult> {
     return new Promise<ExecResult>( (resolve, reject) => {
       shelljs.exec(cmdline,{async: true, silent: true}, (code,stdout,stderr)=> {
