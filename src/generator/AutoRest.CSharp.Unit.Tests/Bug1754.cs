b495d214b240e65e988245c54b28346b7257842b
Deepak Shankargouda <deshank@microsoft.com>
2017-02-14 17:31:12 -0800

    fixing tests

--- src/generator/AutoRest.CSharp.Unit.Tests/Bug1754.cs
+++ src/generator/AutoRest.CSharp.Unit.Tests/Bug1754.cs
@@ -58,7 +58,7 @@ namespace AutoRest.CSharp.Unit.Tests.Resource
 
 
                 // try to load the assembly
-                var asm = Assembly.Load(result.Output.GetBuffer());
+                var asm = LoadAssembly(result.Output);
                 Assert.NotNull(asm);
 
                 // verify that parameter is of correct type
