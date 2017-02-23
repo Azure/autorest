ee81e26d6416c40fba703341615ca63c50bd7486
Deepak Shankargouda <deshank@microsoft.com>
2017-02-23 12:20:43 -0800

    Final comment

--- src/modeler/AutoRest.Swagger/Validation/CollectionObjectPropertiesNamingValidation.cs
+++ src/modeler/AutoRest.Swagger/Validation/CollectionObjectPropertiesNamingValidation.cs
@@ -67,6 +67,6 @@ namespace AutoRest.Swagger.Validation
         /// <summary>
         /// The severity of this message (ie, debug/info/warning/error/fatal, etc)
         /// </summary>
-        public override Category Severity => Category.Fatal;
+        public override Category Severity => Category.Error;
     }
 }
