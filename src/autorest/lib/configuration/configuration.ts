d6e612b37239c519e65da78462c52808c26d0c8d
Johannes Bader <olydis@users.noreply.github.com>
2017-02-28 22:29:37 -0800

    TS: loading Swagger files and support for blaming (#1887)
    * load Swagger
    
    * blaming

--- src/autorest/lib/configuration/configuration.ts
+++ src/autorest/lib/configuration/configuration.ts
@@ -3,6 +3,41 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export interface AutoRestConfiguration {
+import * as URI from "urijs";
+import { resolveUri } from "../io/input";
 
+export interface AutoRestConfiguration {
+  "input-file": string[] | string;
+  "base-folder"?: string;
+}
+
+export class AutoRestConfigurationManager {
+  public constructor(
+    private config: AutoRestConfiguration,
+    private configurationFileUri: string) {
+  }
+
+  private get baseFolderUri(): string {
+    const configFileFolderUri = new URI(".").absoluteTo(this.configurationFileUri).toString();
+    const baseFolder = this.config["base-folder"] || "";
+    const baseFolderUri = resolveUri(configFileFolderUri, baseFolder);
+    return baseFolderUri.replace(/\/$/g, "") + "/";
+  }
+
+  private resolveUri(path: string): string {
+    return resolveUri(this.baseFolderUri, path);
+  }
+
+  private inputFiles(): string[] {
+    const raw = this.config["input-file"];
+    return typeof raw === "string"
+      ? [raw]
+      : raw;
+  }
+
+  public get inputFileUris(): Iterable<string> {
+    return this.inputFiles().map(inputFile => this.resolveUri(inputFile));
+  }
+
+  // TODO: stuff like generator specific settings (= YAML merging root with generator's section)
 }
--- src/autorest/lib/source-map/sourceMap.ts
+++ src/autorest/lib/source-map/sourceMap.ts
@@ -4,13 +4,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as jsonpath from "jsonpath";
-import * as yamlAst from "../parsing/yamlAst";
 import * as sourceMap from "source-map";
+import * as yamlAst from "../parsing/yamlAst";
+import { DataHandleRead } from "../data-store/dataStore";
 
 /**
  * Also allow for object paths that will gladly be resolved by us.
  */
 export type Position = sourceMap.Position | { path: jsonpath.PathComponent[] };
+
+export function parseJsonPath(jsonPath: string): Position {
+  return { path: jsonpath.parse(jsonPath).map(part => part.expression.value) };
+}
 
 export interface Mapping {
   generated: Position;
@@ -21,34 +26,28 @@ export interface Mapping {
 
 export type Mappings = Iterable<Mapping>;
 
-export function compile(mappings: Mappings, target: sourceMap.SourceMapGenerator, yamlFiles: { [fileName: string]: string } = {}): void {
+export async function compilePosition(position: Position, yamlFile: DataHandleRead): Promise<sourceMap.Position> {
+  const path = (position as any).path;
+  if (path) {
+    return yamlAst.resolvePath(yamlFile, path);
+  }
+  return position as sourceMap.Position;
+}
+
+export async function compile(mappings: Mappings, target: sourceMap.SourceMapGenerator, yamlFiles: DataHandleRead[] = []): Promise<void> {
+  // build lookup
+  const yamlFileLookup: { [key: string]: DataHandleRead } = {};
+  for (const yamlFile of yamlFiles) {
+    yamlFileLookup[yamlFile.key] = yamlFile;
+  }
+
   const generatedFile = target.toJSON().file;
-
-  const getAST = (() => {
-    const yamlASTs: { [fileName: string]: yamlAst.YAMLNode } = {};
-    return (fileName: string) => {
-      if (!yamlASTs[fileName]) {
-        const yaml = yamlFiles[fileName];
-        if (yaml === undefined) {
-          throw new Error(`File '${fileName}' was not provided.`);
-        }
-        yamlASTs[fileName] = yamlAst.parse(yaml) as yamlAst.YAMLNode;
-      }
-      return yamlASTs[fileName];
-    }
-  })();
-
-  const compilePosition = (position: Position, fileName: string) => {
-    if ((position as any).path) {
-      return yamlAst.resolvePath(yamlFiles[fileName], getAST(fileName), (position as any).path);
-    }
-    return position as sourceMap.Position;
-  };
+  const compilePos = (position: Position, key: string) => compilePosition(position, yamlFileLookup[key]);
 
   for (const mapping of mappings) {
     const compiledMapping: sourceMap.Mapping = {
-      generated: compilePosition(mapping.generated, generatedFile),
-      original: compilePosition(mapping.original, mapping.source),
+      generated: await compilePos(mapping.generated, generatedFile),
+      original: await compilePos(mapping.original, mapping.source),
       name: mapping.name,
       source: mapping.source
     };
