8004ddb710f8a3e456cada7c33b9fa280c5d7dd6
Sarangan Rajamanickam <sarajama@microsoft.com>
2017-03-14 16:09:45 -0700

    Fix typo

--- src/core/AutoRest.Core/Properties/Resources.Designer.cs
+++ src/core/AutoRest.Core/Properties/Resources.Designer.cs
@@ -760,7 +760,7 @@ namespace AutoRest.Core.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Paths in x-ms-paths must overload a normal path in the paths section, i.e. a path in the x-ms-path must either be same as a path in the paths section or a path in the paths sections followed by additional parameters..
+        ///   Looks up a localized string similar to Paths in x-ms-paths must overload a normal path in the paths section, i.e. a path in the x-ms-paths must either be same as a path in the paths section or a path in the paths sections followed by additional parameters..
         /// </summary>
         public static string XMSPathBaseNotInPaths {
             get {
