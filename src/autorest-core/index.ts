075035ee07e44f933f610c3f98585a4c8b7cf681
Johannes Bader <jobader@microsoft.com>
2017-03-17 10:06:47 -0700

    cleanup

--- src/autorest-core/index.ts
+++ src/autorest-core/index.ts
@@ -12,40 +12,6 @@ import { RunPipeline, DataPromise } from "./lib/pipeline/pipeline";
 import { MultiPromiseUtility } from "./lib/approved-imports/multi-promise";
 import { CancellationToken } from "./lib/approved-imports/cancallation";
 
-/* @internal */
-export async function run(
-  configurationUri: string,
-  cancellationToken: CancellationToken = CancellationToken.None)
-  : Promise<void> {
-
-  const dataStore: DataStoreView = new DataStore(cancellationToken);
-  const outputData = await RunPipeline(configurationUri, dataStore);
-}
-
-/* @internal */
-export async function runWithKnownSetOfFiles(
-  configuration: AutoRestConfiguration,
-  inputFiles: { [fileName: string]: string },
-  cancellationToken: CancellationToken = CancellationToken.None)
-  : Promise<void> {
-
-  const dataStore = new DataStore(cancellationToken);
-  const configFileUri = CreateFileUri("config.yaml");
-
-  // input
-  const inputView = dataStore.CreateScope(KnownScopes.Input).AsFileScope();
-  const hwConfig = await inputView.Write(configFileUri);
-  await hwConfig.WriteData(Stringify(configuration));
-  for (const fileName in inputFiles) {
-    if (typeof fileName === "string") {
-      const hwFile = await inputView.Write(CreateFileUri(fileName));
-      await hwFile.WriteData(inputFiles[fileName]);
-    }
-  }
-
-  const outputData = await RunPipeline(configFileUri, dataStore);
-}
-
 export interface IFileSystem {
 
 }
