fcbe85fbedb4c5690f11b307cca17dc0e1eab112
Garrett Serack <garretts@microsoft.com>
2017-03-17 14:05:00 -0700

    merged from master

--- src/autorest-core/lib/approved-imports/linq.ts
+++ src/autorest-core/lib/approved-imports/linq.ts
@@ -3,8 +3,4 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-<<<<<<< HEAD
-export { Enumerable as IEnumerable, From } from "linq-es2015";
-=======
-export { From, Enumerable } from "linq-es2015";
->>>>>>> 80192f1137dabe125d8b8c36c0d00dfe89d25900
+export { Enumerable, Enumerable as IEnumerable, From } from "linq-es2015";
