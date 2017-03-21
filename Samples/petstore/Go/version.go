6a595ff8680d36c6945093d494954f35ba207a94
Martin Strobel <marstr@users.noreply.github.com>
2017-03-21 09:34:22 -0700

    Version template fix (#1975)
    * [Go] Adding optional prefix "v" to Generator recognized semvers.
    
    * [Go] Calculating Version & UserAgent at Gen time.
    
    The previous update to Version and UserAgent created some benign race-conditions. Despite the fact that there was no behavioral difference, it still triggered Go errors when "-race" was used. To fix this, we're offloading the calculation of Version and UserAgent information into the Generator. Note, this PR doesn't attempt to fix the broader issue of the fact that UserAgents are always tied to Azure-SDK-for-Go and ARM.
    
    * Updating Go Petstore Sample

--- Samples/petstore/Go/version.go
+++ Samples/petstore/Go/version.go
@@ -1,42 +1,13 @@
 package petstore
 
 
-import (
-    "bytes"
-    "fmt"
-    "strings"
-)
-
-const (
-    major = "0"
-    minor = "0"
-    patch = "0"
-    tag   = ""
-    userAgentFormat = "Azure-SDK-For-Go/%s arm-%s/%s"
-)
-// cached results of UserAgent and Version to prevent repeated operations.
-var (
-    userAgent string
-    version string
-)
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-    if userAgent == "" {
-        userAgent = fmt.Sprintf(userAgentFormat, Version(), "petstore", "1.0.0")
-    }
-    return userAgent
+    return "Azure-SDK-For-Go/0.0.0 arm-petstore/1.0.0"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-    if version == "" {
-        versionBuilder := bytes.NewBufferString(fmt.Sprintf("%s.%s.%s", major, minor, patch))
-        if tag != "" {
-            versionBuilder.WriteRune('-')
-            versionBuilder.WriteString(strings.TrimPrefix(tag, "-"))
-        }
-        version = string(versionBuilder.Bytes())
-    }
-    return version
+  return "0.0.0"
 }
