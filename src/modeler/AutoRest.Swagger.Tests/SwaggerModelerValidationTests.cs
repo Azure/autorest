2dca86be80c33acff9d87e6e1a885d5fe2da514e
Deepak Shankargouda <deshank@microsoft.com>
2017-03-27 17:39:08 -0700

    Minor test fix

--- src/modeler/AutoRest.Swagger.Tests/SwaggerModelerValidationTests.cs
+++ src/modeler/AutoRest.Swagger.Tests/SwaggerModelerValidationTests.cs
@@ -563,25 +563,6 @@ namespace AutoRest.Swagger.Tests
             messages.AssertOnlyValidationMessage(typeof(ArmResourcePropertiesBag), 0);
         }
 
-
-        /// <summary>
-        /// Verifies resource models are correctly identified
-        /// </summary>
-        [Fact]
-        public void ValidResourceModels()
-        {
-            var filePath = Path.Combine(Core.Utilities.Extensions.CodeBaseDirectory, "Resource", "Swagger", "Validation", "positive", "valid-resource-model-definitions.json");
-            var fileText = System.IO.File.ReadAllText(filePath);
-            var servDef = SwaggerParser.Parse(filePath, fileText);
-            Uri uriPath = null;
-            Uri.TryCreate(filePath, UriKind.RelativeOrAbsolute, out uriPath);
-            var context = new RuleContext(servDef, uriPath);
-            Assert.Equal(4, context.ResourceModels.Count());
-            Assert.Equal(1, context.TrackedResourceModels.Count());
-            Assert.Equal(3, context.ProxyResourceModels.Count());
-        }
-        
-
         /// <summary>
         /// Verifies resource models are correctly identified
         /// </summary>
