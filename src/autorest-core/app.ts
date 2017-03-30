efee1e491b51d65508bedc880c3768250eaf3060
Garrett Serack <garretts@microsoft.com>
2017-03-29 20:20:45 -0700

    fixed bad shbang

--- src/autorest-core/app.ts
+++ src/autorest-core/app.ts
@@ -1,4 +1,4 @@
-#!/usr/bin / env node
+#!/usr/bin/env node
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
