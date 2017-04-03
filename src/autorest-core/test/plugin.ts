bf96b9e8509f235fe7e9096add88fe4c89395efc
Johannes Bader <jobader@microsoft.com>
2017-04-03 09:57:33 -0700

    fixed test case

--- src/autorest-core/test/plugin.ts
+++ src/autorest-core/test/plugin.ts
@@ -96,7 +96,7 @@ import { LoadLiterateSwagger } from "../lib/pipeline/swagger-loader";
       assert.ok(message.Details.jsonref);
       assert.ok(message.Details["json-path"]);
       assert.ok(message.Details.validationCategory);
-      assert.strictEqual(message.Plugin, "AzureValidator");
+      assert.strictEqual(message.Plugin, "azure-validator");
     }
   }
 
