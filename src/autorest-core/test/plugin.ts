d4aa7ae1926716fdbd08baba9d54223e22f1c5d1
Johannes Bader <jobader@microsoft.com>
2017-04-13 09:14:27 -0700

    sway fix

--- src/autorest-core/test/plugin.ts
+++ src/autorest-core/test/plugin.ts
@@ -55,7 +55,7 @@ import { LoadLiterateSwagger } from "../lib/pipeline/swagger-loader";
       }
     });
     assert.strictEqual(await autoRest.Process().finish, true);
-    const expectedNumErrors = 10;
+    const expectedNumErrors = 7;
     if (errorMessages.length !== expectedNumErrors) {
       console.log(JSON.stringify(errorMessages, null, 2));
     }
