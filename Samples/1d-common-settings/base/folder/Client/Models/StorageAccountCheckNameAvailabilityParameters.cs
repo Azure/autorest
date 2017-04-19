a4eff510b4a4816c1c08ea6d8e56c190ee3de821
Johannes Bader <jobader@microsoft.com>
2017-04-18 19:29:36 -0700

    new sample

---
+++ Samples/1d-common-settings/base/folder/Client/Models/StorageAccountCheckNameAvailabilityParameters.cs
@@ -0,0 +1,63 @@
+// This is my custom license header. I am a nice person so please don't steal
+// my code.
+//
+// Cheers.
+
+namespace AwesomeNamespace.Models
+{
+    using Microsoft.Rest;
+    using Newtonsoft.Json;
+    using System.Linq;
+
+    public partial class StorageAccountCheckNameAvailabilityParameters
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// StorageAccountCheckNameAvailabilityParameters class.
+        /// </summary>
+        public StorageAccountCheckNameAvailabilityParameters()
+        {
+          CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// StorageAccountCheckNameAvailabilityParameters class.
+        /// </summary>
+        public StorageAccountCheckNameAvailabilityParameters(string name, string type = default(string))
+        {
+            Name = name;
+            Type = type;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults
+        /// </summary>
+        partial void CustomInit();
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Validate the object.
+        /// </summary>
+        /// <exception cref="ValidationException">
+        /// Thrown if validation fails
+        /// </exception>
+        public virtual void Validate()
+        {
+            if (Name == null)
+            {
+                throw new ValidationException(ValidationRules.CannotBeNull, "Name");
+            }
+        }
+    }
+}
