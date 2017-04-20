92076c873d0b5f15f73ddba04deeb8fbe259046f
Deepak Shankargouda <deshank@microsoft.com>
2017-04-20 14:06:21 -0700

    minor changes

--- src/modeler/AutoRest.Swagger/Validation/ResourceModelValidation.cs
+++ src/modeler/AutoRest.Swagger/Validation/ResourceModelValidation.cs
@@ -23,7 +23,7 @@ namespace AutoRest.Swagger.Validation
         /// <summary>
         /// Id of the Rule.
         /// </summary>
-        public override string Id => "M2020";
+        public override string Id => "M3001";
 
         /// <summary>
         /// Violation category of the Rule.
