6774366adeb0285643dac75434c8d8b13c1687c0
Johannes Bader <jobader@microsoft.com>
2017-04-21 14:35:52 -0700

    easier transform

--- src/autorest-core/lib/pipeline/manipulation.ts
+++ src/autorest-core/lib/pipeline/manipulation.ts
@@ -38,7 +38,7 @@ export class Manipulator {
           // transform
           for (const t of trans.transform) {
             const target = await scope.Write(`transform_${nextId()}.yaml`);
-            data = (await ManipulateObject(data, target, w, obj => safeEval(t, { $: obj, $doc: data.ReadObject() }))).result;
+            data = (await ManipulateObject(data, target, w, obj => safeEval(`(() => { { ${t} }; return $; })()`, { $: obj, $doc: data.ReadObject() }))).result;
           }
           // set
           for (const s of trans.set) {
