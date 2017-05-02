5794ee4bc81b961d6915f58eaabca51abfb34b25
Deepak Shankargouda <deshank@microsoft.com>
2017-05-02 09:51:45 -0700

    addressing PR

--- src/autorest-core/test/plugin.ts
+++ src/autorest-core/test/plugin.ts
@@ -2,10 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-// polyfills for language support
+// polyfills for language support 
 require("../lib/polyfill.min.js");
 
-import { suite, test, timeout, skip } from "mocha-typescript";
+import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 import * as assert from "assert";
 import { PumpMessagesToConsole } from './test-utility';
 
