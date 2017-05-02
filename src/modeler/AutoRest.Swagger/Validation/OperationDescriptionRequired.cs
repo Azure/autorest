13c96b7b36534084eb604f2e8144ea1056060a9d
Deepak Shankargouda <deshank@microsoft.com>
2017-05-02 16:03:59 -0700

    working stash1

--- src/modeler/AutoRest.Swagger/Validation/OperationDescriptionRequired.cs
+++ src/modeler/AutoRest.Swagger/Validation/OperationDescriptionRequired.cs
@@ -1,24 +1,27 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using AutoRest.Core.Logging;
 using AutoRest.Swagger.Model;
 using AutoRest.Swagger.Validation.Core;
+using System.Collections.Generic;
 
 namespace AutoRest.Swagger.Validation
 {
-    public class OperationDescriptionRequired : DescriptionRequired<string>
+    public class OperationDescriptionRequired : DescriptionRequired<Operation>
     {
         /// <summary>
         /// This rule fails if the description is null and the reference is null (since the reference could have a description)
         /// </summary>
         /// <param name="entity">Entity being validated</param>
         /// <param name="context">Rule context</param>
-        /// <param name="formatParameters">formatted parameters</param>
-        /// <returns><c>true</c> if entity contains description, <c>false</c> otherwise</returns>
-        public override bool IsValid(string entity, RuleContext context, out object[] formatParameters)
+        public override IEnumerable<ValidationMessage> GetValidationMessages(Operation entity, RuleContext context)
         {
-            formatParameters = new string[] { string.Format("Operation '{0}'", ((Operation)context.Parent.Value).OperationId) };
-            return !string.IsNullOrWhiteSpace(entity);
+            if (string.IsNullOrWhiteSpace(entity.Description))
+            {
+                yield return new ValidationMessage(new FileObjectPath(context.File, context.Path.AppendProperty("operationId")), this, entity.OperationId);
+            }
+            
         }
     }
 }
