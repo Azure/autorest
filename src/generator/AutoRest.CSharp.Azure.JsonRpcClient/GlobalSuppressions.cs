4d152eb3452aebcce74c9c2655a1ca910e84eb98
Joel Hendrix <jhendrix@microsoft.com>
2017-05-05 14:10:24 -0700

    Add the .NET test generator. (#2248)
    * Add the .NET test generator.
    
    This is the .NET test generator.  It will create a mock .NET SDK that
    translates API calls into JSON-RPC to be passed to test SDK servers (e.g.
    an SDK created by the Go.TestGen generator).
    
    * branched out pipeline graph for JSON-RPC client

---
+++ src/generator/AutoRest.CSharp.Azure.JsonRpcClient/GlobalSuppressions.cs
@@ -0,0 +1,35 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Code Analysis results, point to "Suppress Message", and click 
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", 
+    Scope = "namespace", Target = "AutoRest.CSharp.Azure.Templates", Justification="Parallel with other generators.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", 
+    Scope = "namespace", Target = "AutoRest.CSharp", Justification="Parallel with other generators.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", 
+    "CA1303:Do not pass literals as localized parameters", 
+    MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", 
+    Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#BuildUrl(System.String)", 
+    Justification="Literal string is generated code.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", 
+    "CA1303:Do not pass literals as localized parameters", 
+    MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", 
+    Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#AddQueryParametersToUri(System.String,AutoRest.Core.Utilities.IndentedStringBuilder)", 
+    Justification="Literal string is generated code.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", 
+    "CA1303:Do not pass literals as localized parameters", 
+    MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", 
+    Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#ReplaceSubscriptionIdInUri(System.String,AutoRest.Core.Utilities.IndentedStringBuilder)", 
+    Justification="Literal string is generated code.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#SetDefaultHeaders",
+    Justification = "Required for Azure customization.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#InitializeResponseBody",
+    Justification = "Required for Azure customization.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "AutoRest.Core.Utilities.IndentedStringBuilder.AppendLine(System.String)", Scope = "member", Target = "AutoRest.CSharp.Azure.AzureMethodTemplateModel.#InitializeException")]
+
---
+++ src/generator/AutoRest.CSharp.JsonRpcClient/PluginCs.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// 
+
+using AutoRest.Core.Extensibility;
+using AutoRest.Core.Model;
+using AutoRest.Core.Utilities;
+using AutoRest.CSharp.Model;
+using static AutoRest.Core.Utilities.DependencyInjection;
+
+namespace AutoRest.CSharp.JsonRpcClient
+{
+    public sealed class PluginCs :Plugin<GeneratorSettingsCs, TransformerCs, CodeGeneratorCsJsonRpc, CodeNamerCs, CodeModelCs>
+    {
+        public PluginCs()
+        {
+            Context = new Context
+            {
+                // inherit base settings
+                Context,
+
+                // set code model implementations our own implementations 
+                new Factory<CodeModel, CodeModelCs>(),
+                new Factory<Method, MethodCs>(),
+                new Factory<CompositeType, CompositeTypeCs>(),
+                new Factory<Property, PropertyCs>(),
+                new Factory<Parameter, ParameterCs>(),
+                new Factory<DictionaryType, DictionaryTypeCs>(),
+                new Factory<SequenceType, SequenceTypeCs>(),
+                new Factory<MethodGroup, MethodGroupCs>(),
+                new Factory<EnumType, EnumTypeCs>(),
+                new Factory<PrimaryType, PrimaryTypeCs>()
+            };
+        }
+    }
+}
