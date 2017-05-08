4c2dd98c69e054a3d1a9b8f16e4715b3563c4997
Garrett Serack <garretts@microsoft.com>
2017-05-08 15:09:49 -0700

    WIP

--- src/autorest-core/lib/source-map/merging.ts
+++ src/autorest-core/lib/source-map/merging.ts
@@ -65,8 +65,91 @@ function Merge(a: any, b: any, path: JsonPath = []): any {
   throw new Error(`'${stringify(path)}' has incomaptible values (${yaml.Stringify(a)}, ${yaml.Stringify(b)}).`);
 }
 
+export function ShallowCopy(input: any, ...filter: Array<string>): any {
+  if (!input) {
+    return input;
+  }
+  const keys = input.Keys ? input.Keys : Object.getOwnPropertyNames(input);
+
+  const result: any = {};
+  for (const key of keys) {
+    if (filter.indexOf(key) == -1) {
+      const value = input[key];
+      if (value !== undefined) {
+        result[key] = value;
+      }
+    }
+  }
+  return result;
+}
+
+
+
+// Note: I am not convinced this works precisely as it should
+// but it works well enough for my needs right now
+// I will revisit it later.
+const macroRegEx = /\$\(([a-zA-Z0-9_-]*)\)/ig
+export function resolveRValue(value: any, propertyName: string, higherPriority: any, lowerPriority: any, jsAware: number = 0): any {
+  if (value) {
+    // resolves the actual macro value.
+    const resolve = (macroExpression: string, macroKey: string) => {
+      // if the original set has it, use that.
+      if (higherPriority && higherPriority[macroKey]) {
+        return resolveRValue(higherPriority[macroKey], macroKey, lowerPriority, null, jsAware - 1);
+      }
+
+      if (lowerPriority) {
+        // check to see if the value is in the overrides set before the key itself.
+        const keys = Object.getOwnPropertyNames(lowerPriority);
+        const macroKeyLocation = keys.indexOf(macroKey);
+        if (macroKeyLocation > -1) {
+          if (macroKeyLocation < keys.indexOf(propertyName)) {
+            // the macroKey is in the overrides, and it precedes the propertyName itself
+            return resolveRValue(lowerPriority[macroKey], macroKey, higherPriority, lowerPriority, jsAware - 1);
+          }
+        }
+      }
+
+      // can't find the macro. maybe later.
+      return macroExpression;
+    };
+
+    // resolve the macro value for strings
+    if (typeof value === "string") {
+      const match = macroRegEx.exec(value.trim());
+      if (match) {
+        if (match[0] === match.input) {
+          // the target value should be the result without string twiddling
+          if (jsAware > 0) {
+            return `'${resolve(match[0], match[1])}'`;
+          }
+          return resolve(match[0], match[1]);
+        }
+        // it looks like we should do a string replace.
+        return value.replace(macroRegEx, resolve)
+      }
+    }
+
+    // resolve macro values for array values
+    if (value instanceof Array) {
+      const result = [];
+      for (const each of value) {
+        // since we're not naming the parameter,
+        // if there isn't a higher priority,
+        // we can fall back to a wide-lookup in lowerPriority.
+        result.push(resolveRValue(each, "", higherPriority || lowerPriority, null));
+      }
+      return result;
+    }
+  }
+  if (jsAware > 0) {
+    return `'${value}'`;
+  }
+  return value;
+}
+
 export function MergeOverwriteOrAppend(a: any, b: any, concatListPathFilter: (path: JsonPath) => boolean = _ => false, path: JsonPath = []): any {
-  if (a === null || b == null) {
+  if (a === null || b === null) {
     return null; // TODO: overthink, we could use this to force mute something even if it's "concat" mode...
   }
 
@@ -91,17 +174,17 @@ export function MergeOverwriteOrAppend(a: any, b: any, concatListPathFilter: (pa
 
     // forward if only present in one of the nodes
     if (a[key] === undefined) {
-      result[key] = b[key];
+      result[key] = resolveRValue(b[key], key, a, b);
       continue;
     }
     if (b[key] === undefined) {
-      result[key] = a[key];
+      result[key] = resolveRValue(a[key], key, null, a);
       continue;
     }
 
     // try merge objects otherwise
-    const aMember = a[key];
-    const bMember = b[key];
+    const aMember = resolveRValue(a[key], key, b, a);
+    const bMember = resolveRValue(b[key], key, a, b);
     result[key] = MergeOverwriteOrAppend(aMember, bMember, concatListPathFilter, subpath);
   }
   return result;
