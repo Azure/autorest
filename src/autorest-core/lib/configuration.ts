8311e8cc658225daefb215e143f9f643299b6edb
Johannes Bader <jobader@microsoft.com>
2017-05-10 09:04:15 -0700

    after-merge fix

--- src/autorest-core/lib/configuration.ts
+++ src/autorest-core/lib/configuration.ts
@@ -1,4 +1,3 @@
-import { BlameTree } from './source-map/blaming';
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
