2c21d16b6728889040e03469e906bec4b538a48e
Sarangan Rajamanickam <sarajama@microsoft.com>
2017-05-22 13:12:50 -0700

    Change Id duplicate 2014 to 2015

--- src/modeler/AutoRest.Swagger/Validation/ParameterNotDefinedInGlobalParamters.cs
+++ src/modeler/AutoRest.Swagger/Validation/ParameterNotDefinedInGlobalParamters.cs
@@ -19,7 +19,7 @@ namespace AutoRest.Swagger.Validation
         /// <summary>
         /// Id of the Rule.
         /// </summary>
-        public override string Id => "R2014";
+        public override string Id => "R2015";
 
         /// <summary>
         /// Violation category of the Rule.
