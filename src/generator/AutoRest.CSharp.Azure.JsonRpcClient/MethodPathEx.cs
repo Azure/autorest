8e1e9dacd8dd81065dcab16c054e3fcb963dfcd0
Sergey Shandar <sergey@live.com.au>
2017-05-26 10:42:10 -0700

    paramex

---
+++ src/generator/AutoRest.CSharp.Azure.JsonRpcClient/MethodPathEx.cs
@@ -0,0 +1,42 @@
+﻿using AutoRest.Core.Model;
+using System;
+using System.Collections.Generic;
+
+namespace AutoRest.CSharp.Azure.JsonRpcClient
+{
+    public static class MethodPathEx
+    {
+        public static IEnumerable<Tuple<string, bool>> GetPathParts(this Method method)
+        {
+            var url = method.Url.Value;
+            while (url != string.Empty)
+            {
+                var index = url.IndexOf('{');
+
+                if (index < 0)
+                {
+                    yield return Tuple.Create(url, false);
+                    break;
+                }
+
+                if (index > 0)
+                {
+                    yield return Tuple.Create(url.Substring(0, index), false);
+                }
+
+                url = url.Substring(index + 1);
+
+                index = url.IndexOf('}');
+
+                if (index <= 0)
+                {
+                    throw new Exception("invalid url: " + method.Url.Value);
+                }
+
+                yield return Tuple.Create(url.Substring(0, index), true);
+
+                url = url.Substring(index + 1);
+            }
+        }
+    }
+}
---
+++ src/generator/AutoRest.CSharp.Azure.JsonRpcClient/ParamEx.cs
@@ -0,0 +1,10 @@
+﻿using AutoRest.Core.Model;
+
+namespace AutoRest.CSharp.Azure.JsonRpcClient
+{
+    public static class ParamEx
+    {
+        public static string GetInfoName(this Parameter p)
+            => $"{p.Method.Name}_{p.SerializedName.Value.Replace('-', '_')}_ParamInfo";
+    }
+}
