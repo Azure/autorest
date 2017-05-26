64f2bacf96a3f735dd2408b344936fd009aa8929
Jeno Laszlo <jeno.laszlo@agoda.com>
2017-05-26 16:17:21 +0700

    added new generator

---
+++ src/generator/AutoRest.CSharp.LoadBalanced/LoadBalancedCodeGeneratorCs.cs
@@ -0,0 +1,182 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoRest.Core;
+using AutoRest.Core.Logging;
+using AutoRest.Core.Model;
+using AutoRest.Core.Utilities;
+using AutoRest.CSharp.LoadBalanced.Model;
+using AutoRest.CSharp.LoadBalanced.Templates;
+using AutoRest.CSharp.LoadBalanced.Templates.Rest.Client;
+using AutoRest.CSharp.LoadBalanced.Templates.Rest.Common;
+using AutoRest.CSharp.LoadBalanced.Templates.Rest.Server;
+using AutoRest.Extensions;
+using static AutoRest.Core.Utilities.DependencyInjection;
+
+namespace AutoRest.CSharp.LoadBalanced
+{
+    public class LoadBalancedCodeGeneratorCs : CodeGenerator
+    {
+        private const string ClientRuntimePackage = "Microsoft.Rest.ClientRuntime.2.2.0";
+
+        public override bool IsSingleFileGenerationSupported => true;
+
+
+        public override string UsageInstructions => string.Format(CultureInfo.InvariantCulture,
+            Properties.Resources.UsageInformation, ClientRuntimePackage);
+
+        public override string ImplementationFileExtension => ".cs";
+
+        private async Task GenerateServerSideCode(CodeModelCs codeModel)
+        {
+            var project = new ProjectModel
+                          {
+                              RootNameSpace = codeModel.Namespace
+                          };
+
+            foreach (string methodGrp in codeModel.MethodGroupNames)
+            {
+                using (NewContext)
+                {
+                    codeModel.Name = methodGrp;
+                    // Service server
+                    var serviceControllerTemplate = new ServiceControllerTemplate { Model = codeModel };
+                    var filePath = $"{codeModel.Name}{ImplementationFileExtension}";
+                    project.FilePaths.Add(filePath);
+                    await Write(serviceControllerTemplate, filePath);
+                }
+            }
+            // Models
+            foreach (var compositeType in codeModel.ModelTypes.Union(codeModel.HeaderTypes))
+            {
+                var model = (CompositeTypeCs) compositeType;
+                var modelTemplate = new ModelTemplate { Model = model };
+                var fileName = $"{model.Name}{ImplementationFileExtension}";
+                var filePath = Path.Combine(Settings.Instance.ModelsName, fileName);
+                project.FilePaths.Add(filePath);
+                await Write(modelTemplate, Path.Combine(Settings.Instance.ModelsName, filePath));
+            }
+
+            var projectTemplate = new CsProjTemplate() { Model = project };
+            var projFilePath = $"{project.RootNameSpace}.csproj";
+
+            await Write(projectTemplate, projFilePath);
+        }
+
+        private async Task GenerateClientSideCode(CodeModelCs codeModel)
+        {
+            // Service client
+            var serviceClientTemplate = new ServiceClientTemplate { Model = codeModel };
+            await Write(serviceClientTemplate, $"{codeModel.Name}{ImplementationFileExtension}");
+
+            // Service client interface
+            var serviceClientInterfaceTemplate = new ServiceClientInterfaceTemplate { Model = codeModel };
+            await Write(serviceClientInterfaceTemplate, $"I{codeModel.Name}{ImplementationFileExtension}");
+
+            // operations
+            foreach (MethodGroupCs methodGroup in codeModel.Operations)
+            {
+                if (!methodGroup.Name.IsNullOrEmpty())
+                {
+                    // Operation
+                    var operationsTemplate = new MethodGroupTemplate { Model = methodGroup };
+                    await Write(operationsTemplate, $"{operationsTemplate.Model.TypeName}{ImplementationFileExtension}");
+
+                    // Operation interface
+                    var operationsInterfaceTemplate = new MethodGroupInterfaceTemplate { Model = methodGroup };
+                    await Write(operationsInterfaceTemplate, $"I{operationsInterfaceTemplate.Model.TypeName}{ImplementationFileExtension}");
+                }
+
+                var operationExtensionsTemplate = new ExtensionsTemplate { Model = methodGroup };
+                await Write(operationExtensionsTemplate, $"{methodGroup.ExtensionTypeName}Extensions{ImplementationFileExtension}");
+            }
+
+            // Models
+            foreach (CompositeTypeCs model in codeModel.ModelTypes.Union(codeModel.HeaderTypes))
+            {
+                if (model.Extensions.ContainsKey(SwaggerExtensions.ExternalExtension) &&
+                    (bool)model.Extensions[SwaggerExtensions.ExternalExtension])
+                {
+                    continue;
+                }
+
+                var modelTemplate = new ModelTemplate{ Model = model };
+                await Write(modelTemplate, Path.Combine(Settings.Instance.ModelsName, $"{model.Name}{ImplementationFileExtension}"));
+            }
+
+            // Enums
+            foreach (EnumTypeCs enumType in codeModel.EnumTypes)
+            {
+                var enumTemplate = new EnumTemplate { Model = enumType };
+                await Write(enumTemplate, Path.Combine(Settings.Instance.ModelsName, $"{enumTemplate.Model.Name}{ImplementationFileExtension}"));
+            }
+
+            // Exceptions
+            foreach (CompositeTypeCs exceptionType in codeModel.ErrorTypes)
+            {
+                var exceptionTemplate = new ExceptionTemplate { Model = exceptionType, };
+                await Write(exceptionTemplate, Path.Combine(Settings.Instance.ModelsName, $"{exceptionTemplate.Model.ExceptionTypeDefinitionName}{ImplementationFileExtension}"));
+            }
+            
+            // Xml Serialization
+            if (codeModel.ShouldGenerateXmlSerialization)
+            {
+                var xmlSerializationTemplate = new XmlSerializationTemplate();
+                await Write(xmlSerializationTemplate, Path.Combine(Settings.Instance.ModelsName, $"{XmlSerialization.XmlDeserializationClass}{ImplementationFileExtension}"));
+            }
+        }
+
+        private async Task GenerateRestCode(CodeModelCs codeModel)
+        {
+            if (Settings.Instance.CodeGenerationMode.IsNullOrEmpty() || Settings.Instance.CodeGenerationMode.EqualsIgnoreCase("rest-client"))
+            {
+                Logger.Instance.Log(Category.Info, "Defaulting to generate client side Code");
+                await GenerateClientSideCode(codeModel);
+            } 
+            else if (Settings.Instance.CodeGenerationMode.EqualsIgnoreCase("rest"))
+            {
+                Logger.Instance.Log(Category.Info, "Generating client side Code");
+                await GenerateClientSideCode(codeModel);
+                Logger.Instance.Log(Category.Info, "Generating server side Code");
+                await GenerateServerSideCode(codeModel);
+            }
+            else if (Settings.Instance.CodeGenerationMode.EqualsIgnoreCase("rest-server"))
+            {
+                Logger.Instance.Log(Category.Info, "Generating server side Code");
+                await GenerateServerSideCode(codeModel);
+            }
+
+        }
+
+        /// <summary>
+        /// Generates C# code for service client.
+        /// </summary>
+        /// <param name="cm"></param>
+        /// <returns></returns>
+        public override async Task Generate(CodeModel cm)
+        {
+            // get c# specific codeModel
+            var codeModel = cm as CodeModelCs;
+            if (codeModel == null)
+            {
+                throw new InvalidCastException("CodeModel is not a c# CodeModel");
+            }
+            if (Settings.Instance.CodeGenerationMode.IsNullOrEmpty() || Settings.Instance.CodeGenerationMode.ToLower().StartsWith("rest"))
+            {
+                Logger.Instance.Log(Category.Info, "Generating Rest Code");
+                await GenerateRestCode(codeModel);
+            }
+            else
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.InvariantCulture,
+                        string.Format(AutoRest.Core.Properties.Resources.ParameterValueIsNotValid, Settings.Instance.CodeGenerationMode, "server/client"), "CodeGenerator"));
+            }
+        }
+    }
+}
---
+++ src/generator/AutoRest.CSharp.LoadBalanced/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoRest.CSharp.LoadBalanced")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c024f83f-73e6-40f4-8150-128c70226301")]
