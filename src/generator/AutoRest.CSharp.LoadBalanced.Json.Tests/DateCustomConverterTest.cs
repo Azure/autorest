1a76c0f89ad223aee0c4f44c445f10f2cd8eacd7
Jeno Laszlo <jeno.laszlo@agoda.com>
2017-06-23 10:56:03 +0700

    added first typeconverter test

---
+++ src/generator/AutoRest.CSharp.LoadBalanced.Json.Tests/DateCustomConverterTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace AutoRest.CSharp.LoadBalanced.Json.Tests
+{
+    [TestFixture]
+    public class DateCustomConverterTest
+    {
+        [Test, TestCaseSource(nameof(GetSerializationTestCases))]
+        public void DateSerializationTest(TestObject testObject, string expectedJson)
+        {
+            Assert.AreEqual(JsonConvert.SerializeObject(testObject), expectedJson);
+        }
+
+        [Test, TestCaseSource(nameof(GetSerializationTestCases))]
+        public void DateDeserializationTest(TestObject testObject, string jsonText)
+        {
+            var deserializedObject = JsonConvert.DeserializeObject<TestObject>(jsonText);
+            Assert.AreEqual(testObject.MyDate, deserializedObject.MyDate);
+        }
+
+
+        public static IEnumerable<object[]> GetSerializationTestCases()
+        {
+            yield return new object[]
+                         {
+                             new TestObject { MyDate = new DateTime(2030, 1, 23, 2, 3, 12) },
+                             "{\"my\":\"1/23/2030 2:03:12 AM\",\"my2\":null}"
+                         };
+
+            yield return new object[]
+                         {
+                             new TestObject { MyDate2 = new DateTime(2030, 1, 23, 2, 3, 12) },
+                             "{\"my\":\"1/1/0001 12:00:00 AM\",\"my2\":\"1/23/2030 2:03:12 AM\"}"
+                         };
+        }
+
+        public class TestObject
+        {
+            [JsonProperty("my"), JsonConverter(typeof(DateTimeToStringConverter))]
+            public DateTime MyDate { get; set; }
+
+            [JsonProperty("my2"), JsonConverter(typeof(DateTimeToStringConverter))]
+            public DateTime? MyDate2 { get; set; }
+        }
+    }
+}
--- src/generator/AutoRest.CSharp.LoadBalanced.Json/DateTimeToStringConverter.cs
+++ src/generator/AutoRest.CSharp.LoadBalanced.Json/DateTimeToStringConverter.cs
@@ -6,30 +6,33 @@ namespace AutoRest.CSharp.LoadBalanced.Json
 {
     public class DateTimeToStringConverter : JsonConverter
     {
-        /// <summary>Writes the JSON representation of the object.</summary>
-        /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="serializer">The calling serializer.</param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var date = (DateTime)value;
+            // TODO: custom date format stuff goes here 
             JToken.FromObject(date.ToString()).WriteTo(writer);
         }
 
-        /// <summary>Reads the JSON representation of the object.</summary>
-        /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <param name="existingValue">The existing value of object being read.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        /// <returns>The object value.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            var rawObject = reader.Value?.ToString();
+            if (string.IsNullOrWhiteSpace(rawObject))
+            {
+                return existingValue;
+            }
+
+            DateTime dateValue;
+            if (DateTime.TryParse(rawObject, out dateValue))
+            {
+                return dateValue;
+            }
+
+            return existingValue;
         }
 
         public override bool CanConvert(Type objectType)
         {
-            return objectType == typeof(DateTime);
+            return objectType == typeof(DateTime) || objectType == typeof(DateTime?);
         }
     }
 }
