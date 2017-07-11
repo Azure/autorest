4249061d7cc46ffe906af94d468808ebe6406292
Sergey Shandar <sergey@live.com.au>
2017-07-10 17:43:50 -0700

    PHP template

---
+++ src/generator/AutoRest.Php/CodeGeneratorPhp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AutoRest.Core;
+
+namespace AutoRest.Php
+{
+    public sealed class CodeGeneratorPhp : CodeGenerator
+    {
+        public override string ImplementationFileExtension => throw new NotImplementedException();
+
+        public override string UsageInstructions => throw new NotImplementedException();
+    }
+}
