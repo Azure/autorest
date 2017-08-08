a900c9c612f6893fba00eafb63aa93c2583641fb
Johannes Bader <jobader@microsoft.com>
2017-08-08 09:53:37 -0700

    EnumType.SerializedName

--- src/modeler/AutoRest.Swagger/ObjectBuilder.cs
+++ src/modeler/AutoRest.Swagger/ObjectBuilder.cs
@@ -90,7 +90,6 @@ namespace AutoRest.Swagger
                             }
                         }
                     }
-                    enumType.SerializedName = enumType.Name;
                     if (string.IsNullOrEmpty(enumType.Name))
                     {
                         throw new InvalidOperationException(
@@ -125,7 +124,6 @@ namespace AutoRest.Swagger
                 {
                     enumType.ModelAsString = true;
                     enumType.SetName( string.Empty);
-                    enumType.SerializedName = string.Empty;
                 }
                 enumType.XmlProperties = (SwaggerObject as Schema)?.Xml;
                 return enumType;
