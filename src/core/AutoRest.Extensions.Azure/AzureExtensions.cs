ca23399ddd00103bb56cf29f271dcfe72b9d2c9a
Johannes Bader <jobader@microsoft.com>
2017-08-14 17:38:21 -0700

    proc. parameterized host

--- src/core/AutoRest.Extensions.Azure/AzureExtensions.cs
+++ src/core/AutoRest.Extensions.Azure/AzureExtensions.cs
@@ -68,7 +68,6 @@ namespace AutoRest.Extensions.Azure
                 {
                     throw new ArgumentNullException("codeModel");
                 }
-               
 
                 // This extension from general extensions must be run prior to Azure specific extensions.
                 ProcessParameterizedHost(codeModel);
