a406084a6cf23c6dd58ae776888481f96ba085f1
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2017-08-31 13:35:18 +0700

    Include new templates within the generator

--- src/generator/AutoRest.CSharp.LoadBalanced/LoadBalancedCodeGeneratorCs.cs
+++ src/generator/AutoRest.CSharp.LoadBalanced/LoadBalancedCodeGeneratorCs.cs
@@ -26,7 +26,6 @@ namespace AutoRest.CSharp.LoadBalanced
 
         public override bool IsSingleFileGenerationSupported => true;
 
-
         public override string UsageInstructions => string.Format(CultureInfo.InvariantCulture,
             "The {0} nuget package is required to compile the generated code.", ClientRuntimePackage);
 
@@ -49,6 +48,16 @@ namespace AutoRest.CSharp.LoadBalanced
             var metricsFilePath = "Metrics.cs";
             project.FilePaths.Add(metricsFilePath);
             await Write(metricsTemplate, metricsFilePath);
+			
+            var brokenRuleTemplate = new BrokenRuleTemplate();
+            var brokenRuleFilePath = "BrokenRule.cs";
+            project.FilePaths.Add(brokenRuleFilePath);
+            await Write(brokenRuleTemplate, brokenRuleFilePath);
+			
+            var responseTemplate = new ResponseTemplate();
+            var responseFilePath = "Response.cs";
+            project.FilePaths.Add(responseFilePath);
+            await Write(responseTemplate, responseFilePath);
 
             usings.AddRange(new[]
                             {
