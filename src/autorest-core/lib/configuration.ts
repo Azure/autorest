16885d9befd535db49631fcb2ad4fc0e64cc340f
Johannes Bader <jobader@microsoft.com>
2017-09-07 17:41:22 -0700

    support --use=package@version

--- src/autorest-core/lib/configuration.ts
+++ src/autorest-core/lib/configuration.ts
@@ -517,9 +517,16 @@ export class Configuration {
       const extMgr = await this.extensionManager;
       for (const useEntry of use) {
         if (typeof useEntry === "string") {
-          // TODO: consider resolving foo@1.33.7 too, but careful: @ is valid in paths :-/
-          const pkg = await extMgr.findPackage("foo", useEntry);
-          configs["use-extension"][pkg.name] = useEntry;
+          // attempt <package>@<version> interpretation
+          const separatorIndex = useEntry.lastIndexOf('@');
+          const versionPart = useEntry.slice(separatorIndex + 1);
+          if (separatorIndex !== -1 && require("semver-regex")().test(versionPart)) {
+            const pkg = await extMgr.findPackage(useEntry.slice(0, separatorIndex), versionPart);
+            configs["use-extension"][pkg.name] = versionPart;
+          } else {
+            const pkg = await extMgr.findPackage("foo", useEntry);
+            configs["use-extension"][pkg.name] = useEntry;
+          }
         }
       }
       delete configs.use;
