806356ed09dd099322eb0e52a544dcf2e27f3c05
Johannes Bader <jobader@microsoft.com>
2017-09-20 10:10:06 -0700

    regen

--- Samples/test/reflect-api-versions/SdkClient/SdkInfo_Compute.cs
+++ Samples/test/reflect-api-versions/SdkClient/SdkInfo_Compute.cs
@@ -1,29 +1,36 @@
 
-private static partial class SdkInfo
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SdkInfo
 {
-    public static IEnumerable<Tuple<string, string, string>> SdkInfo_Compute
+    internal static partial class SdkInfo
     {
-        get
-        {
-            return new Tuple<string, string, string>[]
-            {
-                new Tuple<string, string, string>("Compute", "AvailabilitySets", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "Disks", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "Images", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "ResourceSkus", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "Snapshots", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "Usage", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineImages", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineRunCommands", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetExtensions", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMs", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSets", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineSizes", "2017-03-30"),
-                new Tuple<string, string, string>("Compute", "VirtualMachines", "2017-03-30"),
-                new Tuple<string, string, string>("ContainerService", "ContainerServices", "2017-01-31"),
-            }
-        }
-    }
+      public static IEnumerable<Tuple<string, string, string>> SdkInfo_Compute
+      {
+          get
+          {
+              return new Tuple<string, string, string>[]
+              {
+                  new Tuple<string, string, string>("Compute", "AvailabilitySets", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "Disks", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "Images", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "ResourceSkus", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "Snapshots", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "Usage", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineImages", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineRunCommands", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetExtensions", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMs", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineScaleSets", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachineSizes", "2017-03-30"),
+                  new Tuple<string, string, string>("Compute", "VirtualMachines", "2017-03-30"),
+                  new Tuple<string, string, string>("ContainerService", "ContainerServices", "2017-01-31"),
+              }.AsEnumerable();
+          }
+      }
+  }
 }
