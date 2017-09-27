306edd04a9526c8aa4604c3a7cfcc490baf2438d
Garrett Serack <garretts@microsoft.com>
2017-09-27 13:31:54 -0700

    fix both

--- src/autorest-core/static_modules/create-static-fs.js
+++ src/autorest-core/static_modules/create-static-fs.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // patch const ENABLE_WORKERS = process.platform === 'darwin'
 // in .\node_modules\npm\lib\install\action\extract.js
 
-let txt = fs.readFileSync('./node_modules/npm/lib/install/action/extract.js',"utf8").replace("const ENABLE_WORKERS = process.platform === 'darwin'", "const ENABLE_WORKERS = false");
+let txt = fs.readFileSync('./node_modules/npm/lib/install/action/extract.js',"utf8").replace("const ENABLE_WORKERS = process.platform === 'darwin'", "const ENABLE_WORKERS = false;");
 fs.writeFileSync('./node_modules/npm/lib/install/action/extract.js', txt );
 
 function getFileNames(path, result) {
