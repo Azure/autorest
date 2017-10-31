ba1e635bbbce6464484cd40acacf9929ab86a620
Garrett Serack <garretts@microsoft.com>
2017-10-31 14:29:09 -0700

    fixing checkout/install

---
+++ src/autorest-core/post-install.js
@@ -0,0 +1,8 @@
+try {
+    // if this installed for development
+    // static-link should be installed. 
+    // if we require it, it should build the static fs.
+    require("./node_modules/static-link/dist/static-link")
+} catch(E) {
+
+}
