12ac4616a9d51f05dfab95a4f6664c0a1a4438e7
Garrett Serack <garretts@microsoft.com>
2018-11-12 09:47:35 -0800

    cleanup more

--- src/autorest/.scripts/preinstall-check.js
+++ src/autorest/.scripts/preinstall-check.js
@@ -1,9 +1,9 @@
-var version=process.versions.node.split(`.`);
-if(version[0]<8||(version[0]==8&&version[1]<11)) {
+const version = process.versions.node.split(`.`);
+if(version[0]<10||(version[0]==10&&version[1]<13)) {
   console.error( `\n\n
 *******************************************************************************
   The version of Node.js ${process.versions.node} is not sufficent.
-  You need to install Node.js v8.11.0 or greater.
+  You need to install Node.js v10.13.0 or greater.
 
   The prefered version is the current LTS (Long Term Support) version.
   see: https://nodejs.org/en/download/
