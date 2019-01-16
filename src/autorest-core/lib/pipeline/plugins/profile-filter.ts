d0fb583ecf43d028738b533e852d550e04b14cdf
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-01-15 18:37:06 -0800

    fixed profile-filter.

--- src/autorest-core/lib/pipeline/plugins/profile-filter.ts
+++ src/autorest-core/lib/pipeline/plugins/profile-filter.ts
@@ -1,14 +1,41 @@
 import { AnyObject, DataHandle, DataSink, DataSource, Node, Processor, ProxyObject, QuickDataSource, visit } from '@microsoft.azure/datastore';
+import { clone, Dictionary, values } from '@microsoft.azure/linq';
 import * as oai from '@microsoft.azure/openapi';
+import * as compareVersions from 'compare-versions';
 import { ConfigurationView } from '../../configuration';
 import { PipelinePlugin } from '../common';
-
 export interface ApiData {
   apiVersion: string;
   matches: Array<string>;
 }
 
 type componentType = 'schemas' | 'responses' | 'parameters' | 'examples' | 'requestBodies' | 'headers' | 'securitySchemes' | 'links' | 'callbacks';
+
+function getMaxApiVersion(apiVersions: Array<string>): string {
+  let result = '0';
+  for (const version of apiVersions) {
+    if (version && compareVersions(getSemverEquivalent(version), getSemverEquivalent(result)) >= 0) {
+      result = version;
+    }
+  }
+
+  return result;
+}
+
+// azure rest specs currently use versioning of the form yyyy-mm-dd
+// to take into consideration this we convert to an equivalent of
+// semver for comparisons.
+function getSemverEquivalent(version: string) {
+  let result = '';
+  for (const i of version.split('-')) {
+    if (!result) {
+      result = i;
+      continue;
+    }
+    result = Number.isNaN(Number.parseInt(i)) ? `${result}-${i}` : `${result}.${i}`;
+  }
+  return result;
+}
 
 export class ProfileFilter extends Processor<any, oai.Model> {
   filterTargets: Array<{ apiVersion: string; pathRegex: RegExp }> = [];
@@ -27,10 +54,28 @@ export class ProfileFilter extends Processor<any, oai.Model> {
     callbacks: new Set<string>()
   };
 
-  private components: any;
+  private componentsToKeep = {
+    schemas: new Set<string>(),
+    responses: new Set<string>(),
+    parameters: new Set<string>(),
+    examples: new Set<string>(),
+    requestBodies: new Set<string>(),
+    headers: new Set<string>(),
+    securitySchemes: new Set<string>(),
+    links: new Set<string>(),
+    callbacks: new Set<string>()
+  };
 
-  constructor(input: DataHandle, private profiles: any, private profilesToUse: Array<string>, private apiVersions: Array<string>) {
+  private components: any;
+  private profilesApiVersions: Array<string> = [];
+  private apiVersions: Array<string> = [];
+  private maxApiVersion: string = '';
+
+  constructor(input: DataHandle, private profiles: any, private profilesToUse: Array<string>, apiVersions: Array<string>) {
     super(input);
+
+    // get the max version to fix the document version 
+    this.apiVersions = apiVersions;
   }
 
   async init() {
@@ -55,26 +100,34 @@ export class ProfileFilter extends Processor<any, oai.Model> {
       }
 
       for (const target of targets) {
+        this.maxApiVersion = getMaxApiVersion([target.apiVersion, this.maxApiVersion]);
+        this.profilesApiVersions.push(target.apiVersion);
         const apiVersion = target.apiVersion;
         const pathRegex = this.getPathRegex(target.matches);
         this.filterTargets.push({ apiVersion, pathRegex });
       }
+
     } else if (this.apiVersions.length > 0) {
-      //
+      this.maxApiVersion = getMaxApiVersion([this.maxApiVersion, getMaxApiVersion(this.apiVersions)]);
     }
+
+    const paths = this.newObject(this.generated, 'paths', '/paths');
+    this.visitPaths(paths, visit(currentDoc['paths']));
   }
 
   public async process(targetParent: ProxyObject<oai.Model>, originalNodes: Iterable<Node>) {
     for (const { value, key, pointer, children } of originalNodes) {
       switch (key) {
+        case 'info':
+          const info = <oai.Info>targetParent.info || this.newObject(targetParent, 'info', pointer);
+          this.visitInfo(info, children);
         case 'components':
           const components = <oai.Components>targetParent.components || this.newObject(targetParent, 'components', pointer);
           this.visitComponents(components, children);
           break;
 
         case 'paths':
-          const paths = <oai.PathItem>targetParent.paths || this.newObject(targetParent, 'paths', pointer);
-          this.visitPaths(paths, children);
+          // already handled at init
           break;
 
         default:
@@ -84,6 +137,52 @@ export class ProfileFilter extends Processor<any, oai.Model> {
     }
   }
 
+  visitInfo(targetParent: AnyObject, nodes: Iterable<Node>) {
+    for (const { value, key, pointer, children } of nodes) {
+      switch (key) {
+        case 'version':
+          this.clone(targetParent, key, pointer, this.maxApiVersion);
+          break;
+        default:
+          this.clone(targetParent, key, pointer, value);
+          break;
+      }
+    }
+  }
+
+  visitPath(targetParent: AnyObject, nodes: Iterable<Node>) {
+    for (const { value, key, pointer } of nodes) {
+      switch (key) {
+        case 'x-ms-metadata':
+        default:
+          this.clone(targetParent, key, pointer, value);
+          break;
+      }
+
+    }
+  }
+
+  visitPathMetadata(targetParent: AnyObject, nodes: Iterable<Node>) {
+    for (const { value, key, pointer } of nodes) {
+      switch (key) {
+        case 'apiVersions':
+          // the api versions need to be cleaned
+          this.newArray(targetParent, key, pointer);
+          for (const version of value) {
+            if (this.apiVersions.includes(version) || this.profilesApiVersions.includes(version)) {
+              targetParent[key].__push__(version);
+            }
+          }
+
+        default:
+          this.clone(targetParent, key, pointer, value);
+          break;
+      }
+
+    }
+  }
+
+  // finish() modify info version to just have the highest api version from the profile config or apiversion config
   visitPaths(targetParent: AnyObject, nodes: Iterable<Node>) {
     // filter paths
     for (const { value, key, pointer } of nodes) {
@@ -94,6 +193,8 @@ export class ProfileFilter extends Processor<any, oai.Model> {
         // Profile Mode
         for (const each of this.filterTargets) {
           if (path.match(each.pathRegex) && apiVersions.includes(each.apiVersion)) {
+
+            // modify metadata so just the api versions used are included
             this.clone(targetParent, key, pointer, value);
           }
         }
@@ -122,20 +223,7 @@ export class ProfileFilter extends Processor<any, oai.Model> {
         const type: componentType = refParts.pop();
         if (!this.visitedComponents[type].has(componentUid)) {
           this.visitedComponents[type].add(componentUid);
-          if (type === 'schemas') {
-            if (this.generated.components === undefined) {
-              this.newObject(this.generated, 'components', `/components`);
-            }
-
-            if (this.generated.components && this.generated.components.schemas === undefined) {
-              this.newObject(this.generated.components, 'schemas', `/components/schemas`);
-            }
-
-            if (this.generated.components && this.generated.components.schemas) {
-              this.clone(this.generated.components.schemas, componentUid, `/components/schemas/${componentUid}`, this.components[type][componentUid]);
-            }
-          }
-
+          this.componentsToKeep[type].add(componentUid);
           this.crawlObject(this.components[type][componentUid]);
         }
       } else if (value && typeof (value) === 'object') {
@@ -148,13 +236,31 @@ export class ProfileFilter extends Processor<any, oai.Model> {
     for (const { value, key, pointer, children } of originalNodes) {
       switch (key) {
         case 'schemas':
+        case 'responses':
+        case 'parameters':
+        case 'examples':
+        case 'requestBodies':
+        case 'headers':
+        case 'links':
+          // everything else, just copy recursively.
+          if (targetParent[key] === undefined) {
+            this.newObject(targetParent, key, pointer);
+          }
 
+          this.visitComponent(key, targetParent[key], children);
           break;
-
-        // everything else, just copy recursively.
+        case 'securitySchemes':
         default:
           this.clone(targetParent, key, pointer, value);
           break;
+      }
+    }
+  }
+
+  visitComponent<T>(type: string, container: ProxyObject<Dictionary<T>>, nodes: Iterable<Node>) {
+    for (const { key, value, pointer } of nodes) {
+      if (this.componentsToKeep[type].has(key)) {
+        this.clone(container, key, pointer, value);
       }
     }
   }
@@ -181,11 +287,15 @@ async function filter(config: ConfigurationView, input: DataSource, sink: DataSi
 
   for (const each of inputs) {
     const profileData = config.GetEntry('profiles');
-    const apiVersions = config.GetEntry('api-version') || [];
-    const profilesToUse = config.GetEntry('use-profile') || [];
+
+    const configApiVersion = config.GetEntry('api-version');
+    const apiVersions: Array<string> = configApiVersion ? (typeof (configApiVersion) === 'string') ? [configApiVersion] : configApiVersion : [];
+
+    const configUseProfile = config.GetEntry('use-profile');
+    const profilesToUse: Array<string> = configUseProfile ? (typeof (configUseProfile) === 'string') ? [configUseProfile] : configUseProfile : [];
     if (profilesToUse.length > 0 || apiVersions.length > 0) {
       const processor = new ProfileFilter(each, profileData, profilesToUse, apiVersions);
-      result.push(await sink.WriteObject(each.Description, await processor.getOutput(), each.identity, each.artifactType, await processor.getSourceMappings()));
+      result.push(await sink.WriteObject('profile-filtered-oai-doc...', await processor.getOutput(), each.identity, 'profile-filtered-oai-doc...', await processor.getSourceMappings()));
     } else {
       result.push(each);
     }
