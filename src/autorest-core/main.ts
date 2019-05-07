83f3214f0fe6202ca10e7da6bb3993e20cf9eba6
Garrett Serack <garretts@microsoft.com>
2019-05-07 15:52:23 -0700

    Update main.ts

--- src/autorest-core/main.ts
+++ src/autorest-core/main.ts
@@ -13,6 +13,3 @@ export { Message, Channel } from "./lib/message"
 export { Artifact } from "./lib/artifact"
 export { AutoRest, ConfigurationView, IdentifyDocument, IsConfigurationExtension, IsConfigurationDocument, IsOpenApiExtension, LiterateToJson, IsOpenApiDocument } from "./lib/autorest-core"
 export { DocumentFormat, DocumentExtension, DocumentPatterns, DocumentType } from "./lib/document-type"
-
-
-
