bcb9ade8349026d4dfaefed3597b93a61ff2fff2
Garrett Serack <garretts@microsoft.com>
2019-09-04 15:16:19 -0700

    editing build sequence

--- core/.scripts/post-install.js
+++ core/.scripts/post-install.js
@@ -7,7 +7,7 @@ try {
     // if this installed for development
     // static-link should be installed. 
     // if we require it, it should build the static fs.
-    require("./node_modules/static-link/dist/static-link")
+    require("../node_modules/static-link/dist/static-link")
 } catch(E) {
-
+ console.log(E)
 }
