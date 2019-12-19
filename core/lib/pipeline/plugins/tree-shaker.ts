c65b976a91753ef0c22c4192baaf6cba458a6f55
Garrett Serack <garretts@microsoft.com>
2019-12-19 10:41:34 -0800

    propogate client name on properties when shaking out an enum

--- core/lib/pipeline/plugins/tree-shaker.ts
+++ core/lib/pipeline/plugins/tree-shaker.ts
@@ -577,6 +577,7 @@ export class OAI3Shaker extends Transformer<AnyObject, AnyObject> {
         $ref: `#${baseReferencePath}/${id}`,
         description: value.description,  // we violate spec to allow a unique description at the $ref spot, (ie: there are two fields that are of type 'color' -- one is 'borderColor' and one is 'fillColor' -- may be differen descriptions.)
         'x-ms-client-flatten': value['x-ms-client-flatten'], // we violate spec to allow flexibility in terms of flattening
+        'x-ms-client-name': value['x-ms-client-name'], // we violate spec to allow flexibility in terms of naming too. *sigh*
         readOnly: value.readOnly
       }, pointer
     };
