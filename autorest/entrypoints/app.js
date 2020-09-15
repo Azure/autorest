67a7f761c52333259ba2cd50f8d59b767c2b7c4a
David Wilson <daviwil@microsoft.com>
2020-09-15 14:49:14 -0700

    Add --max-memory-size parameter for increasing Node.js heap size

--- autorest/entrypoints/app.js
+++ autorest/entrypoints/app.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
-// load modules from static linker filesystem.
+
+global.isDebuggerEnabled =
+  !!require('inspector').url()
+    || global.v8debug
+    || /--debug|--inspect/.test(process.execArgv.join(' '));
+
+const maxMemorySizeArg = process.argv.join(' ').match(/--max-memory-size=(\w+)/);
+const maxMemorySize = maxMemorySizeArg && parseInt(maxMemorySizeArg[1]);
+if (isNaN(maxMemorySize)) {
+  console.error(`\nWarning: --max-memory-size parameter '${maxMemorySizeArg[1]}' is not an integer, ignoring it.\n`);
+}
+
 // if the process was started with a low heap size (and we're not debugging!) then respawn with a bigger heap size.
-if ((require('v8').getHeapStatistics()).heap_size_limit < 8000000000 && !(require('inspector').url() || global.v8debug || /--debug|--inspect/.test(process.execArgv.join(' ')))) {
-  process.env['NODE_OPTIONS'] = `${process.env['NODE_OPTIONS'] || ''} --max-old-space-size=8192 --no-warnings`;
+if (maxMemorySize && !isDebuggerEnabled && (require('v8').getHeapStatistics()).heap_size_limit < (maxMemorySize * 1024000)) {
+  process.env['NODE_OPTIONS'] = `${process.env['NODE_OPTIONS'] || ''} --max-old-space-size=${maxMemorySize} --no-warnings`;
   const argv = process.argv.indexOf('--break') === -1 ? process.argv.slice(1) : ['--inspect-brk', ...process.argv.slice(1).filter(each => each !== '--break')];
   require('child_process').spawn(process.execPath, argv, { argv0: 'node', stdio: 'inherit' }).on('close', code => { process.exit(code); });
 } else {
-  global.isDebuggerEnabled = !!require('inspector').url();
-
+  // load modules from static linker filesystem.
   if (isDebuggerEnabled) {
     try {
       // try to let source maps resolve
