c09d987540a6547cbbc518c8d13abe51d73d2b4e
Timothee Guerin <tiguerin@microsoft.com>
2020-12-10 08:37:27 -0800

    Initial changes

--- core/lib/openapi/conversion.ts
+++ core/lib/openapi/conversion.ts
@@ -3,13 +3,17 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { DataHandle, DataSink } from "@azure-tools/datastore";
+import { DataHandle, DataSink, IFileSystem } from "@azure-tools/datastore";
 import { Oai2ToOai3 } from "@azure-tools/oai2-to-oai3";
 import { clone } from "@azure-tools/linq";
 
-export async function convertOAI2toOAI3(input: DataHandle, sink: DataSink): Promise<DataHandle> {
-  const converter = new Oai2ToOai3(input.originalFullPath, await input.ReadObject());
-  converter.convert();
+export async function convertOAI2toOAI3(
+  input: DataHandle,
+  sink: DataSink,
+  fileSystem?: IFileSystem,
+): Promise<DataHandle> {
+  const converter = new Oai2ToOai3(input.originalFullPath, await input.ReadObject(), fileSystem);
+  await converter.convert();
   const generated = clone(converter.generated);
   return sink.WriteObject("OpenAPI", generated, input.identity);
 }
--- core/lib/pipeline/plugins/conversion.ts
+++ core/lib/pipeline/plugins/conversion.ts
@@ -1,10 +1,11 @@
+import { IFileSystem } from "@azure-tools/datastore";
 import { convertOAI2toOAI3 } from "../../openapi/conversion";
 import { createPerFilePlugin, PipelinePlugin } from "../common";
 
 /* @internal */
-export function createSwaggerToOpenApi3Plugin(): PipelinePlugin {
+export function createSwaggerToOpenApi3Plugin(fileSystem?: IFileSystem): PipelinePlugin {
   return createPerFilePlugin(async () => async (fileIn, sink) => {
-    const fileOut = await convertOAI2toOAI3(fileIn, sink);
+    const fileOut = await convertOAI2toOAI3(fileIn, sink, fileSystem);
     return sink.Forward(fileIn.Description, fileOut);
   });
 }
