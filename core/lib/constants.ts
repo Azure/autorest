c87f9f8d6f6ede60b87282181b5abfc16b8239b1
Timothee Guerin <tiguerin@microsoft.com>
2020-12-15 19:26:53 +0100

    Remove unused imports

--- core/lib/constants.ts
+++ core/lib/constants.ts
@@ -1,6 +1,3 @@
-import { paths } from "jsonpath";
-import { resolve } from "vscode-languageserver/lib/files";
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
