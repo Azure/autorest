e6372bb276ffb1b871ebc032bb2b79d581d39a35
Timothee Guerin <tiguerin@microsoft.com>
2020-12-17 10:01:35 +0100

    Add tests

--- packages/apps/autorest/src/args.ts
+++ packages/apps/autorest/src/args.ts
@@ -17,28 +17,24 @@ export interface AutorestArgs {
 }
 
 /**
+ * Regex to parse flags in format:
+ * --foo:bar
+ * --foo=bar
+ * --foo
+ */
+const FLAG_REGEX = /^--([^=:]+)([=:](.+))?$/;
+
+/**
  * Parse a list of command line arguments.
  * @param argv List of cli args(process.argv)
  */
 export const parseArgs = (argv: string[]): AutorestArgs => {
-  const cwd = process.cwd();
-  
   const result: any = {};
   for (const arg of argv) {
-    const match = /^--([^=:]+)([=:](.+))?$/g.exec(arg);
+    const match = FLAG_REGEX.exec(arg);
     if (match) {
       const key = match[1];
-      let rawValue = match[3] || "true";
-      if (rawValue.startsWith(".")) {
-        // starts with a . or .. -> this is a relative path to current directory
-        rawValue = join(cwd, rawValue);
-      }
-
-      // untildify!
-      if (/^~[/|\\]/g.exec(rawValue)) {
-        rawValue = join(homedir(), rawValue.substring(2));
-      }
-
+      const rawValue = resolvePathArg(match[3] || "true");
       let value;
       try {
         value = JSON.parse(rawValue);
@@ -53,5 +49,38 @@ export const parseArgs = (argv: string[]): AutorestArgs => {
     }
   }
   return result;
-}
+};
 
+const cwd = process.cwd();
+
+/**
+ * Check if the argument raw value is a relative path or using ~ for user home dir
+ * and then convert it to an aboluste one.
+ * @param rawValue Raw argument value.
+ * @returns string value
+ */
+const resolvePathArg = (rawValue: string): string => {
+  if (rawValue.startsWith(".")) {
+    // starts with a . or .. -> this is a relative path to current directory
+    rawValue = join(cwd, rawValue);
+  }
+
+  // untildify!
+  if (/^~[/|\\]/g.exec(rawValue)) {
+    rawValue = join(homedir(), rawValue.substring(2));
+  }
+  return rawValue;
+};
+
+const parseValue = (rawValue: string) => {
+  try {
+    const value = JSON.parse(rawValue);
+    // restrict allowed types (because with great type selection comes great responsibility)
+    if (typeof value !== "string" && typeof value !== "boolean") {
+      return rawValue;
+    }
+    return value;
+  } catch (e) {
+    return rawValue;
+  }
+};
