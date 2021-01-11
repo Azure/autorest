c3de382979c891874a2cd764f6f7b2b36cc516f7
Timothee Guerin <tiguerin@microsoft.com>
2021-01-11 11:11:45 -0800

    Fix prettier

--- packages/tools/md-mock-api/src/cli/cli.ts
+++ packages/tools/md-mock-api/src/cli/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('source-map-support').install();
+require("source-map-support").install();
 
 import { hideBin } from "yargs/helpers";
 import { ApiMockApp, ApiMockAppConfig } from "../app";
