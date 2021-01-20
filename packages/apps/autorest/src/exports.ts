08c9276f90396ef721843c3c0ea9ea673cf38769
jianye xi <jianyxi@microsoft.com>
2021-01-20 10:10:31 +0800

    use inline eslint block

--- packages/apps/autorest/src/exports.ts
+++ packages/apps/autorest/src/exports.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../definitions/core.d.ts" />
 /// <reference path="../definitions/vscode.d.ts" />
 
