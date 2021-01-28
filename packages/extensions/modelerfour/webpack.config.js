51532d6c75459740b3a5131edaef82ffd4c0b4de
Timothee Guerin <tiguerin@microsoft.com>
2021-01-28 08:34:01 -0800

    Webpack is working

---
+++ packages/extensions/modelerfour/webpack.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+const path = require('path');
+
+/**
+ * @type {import("webpack").Configuration}
+ */
+module.exports = {
+  mode: "production",
+  target: "node",
+  devtool: "source-map",
+  entry: {
+    main: "./src/main.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist", "src"),
+    filename: "[name].js",
+  },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  plugins: [
+  ],
+};
