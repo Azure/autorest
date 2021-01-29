6d94c167924486abfa1a429b5c3df99c867e55ac
Timothee Guerin <tiguerin@microsoft.com>
2021-01-29 15:46:06 -0800

    Move @azure-tools/autorest-extension-base from Azure/perks and rename to @autorest/extension-base (#3822)

---
+++ packages/libs/extension-base/exports.ts
@@ -0,0 +1,3 @@
+export { AutoRestExtension, IAutoRestPluginInitiator as Host } from './extension-base';
+export { ArtifactMessage, Message, Artifact, Channel, Mapping, RawSourceMap, SourceLocation, JsonPath, Position } from './types';
+export * from './convenience';
