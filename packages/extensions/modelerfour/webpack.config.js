e6fb147ddc8b4e51908bd0981ae5135876ce9432
Timothee Guerin <tiguerin@microsoft.com>
2021-01-28 17:31:31 -0800

    Feature: Migrate modelerfour static-link -> webpack (#3815)

---
+++ packages/extensions/modelerfour/webpack.config.js
@@ -0,0 +1,31 @@
+// @ts-check
+const path = require("path");
+
+/**
+ * @type {import("webpack").Configuration}
+ */
+module.exports = {
+  mode: "production",
+  target: "node",
+  devtool: "source-map",
+  entry: {
+    main: "./src/main.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist", "src"),
+    filename: "[name].js",
+    devtoolModuleFilenameTemplate: "../../[resource-path]",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ],
+  },
+  plugins: [],
+  optimization: {
+    minimize: false,
+  },
+};
