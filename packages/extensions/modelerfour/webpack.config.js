ee21e5cf84ac0d96588c91396d90a630d0abafd0
Timothee Guerin <tiguerin@microsoft.com>
2021-01-28 16:52:51 -0800

    Remove usles comments

--- packages/extensions/modelerfour/webpack.config.js
+++ packages/extensions/modelerfour/webpack.config.js
@@ -17,12 +17,10 @@ module.exports = {
     devtoolModuleFilenameTemplate: "../../[resource-path]",
   },
   resolve: {
-    // Add ".ts" and ".tsx" as resolvable extensions.
     extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" },
     ],
   },
