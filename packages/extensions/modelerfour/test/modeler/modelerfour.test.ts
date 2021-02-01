e765f8b131bce26ea53ba2cd994e1097bfeaa9e7
Timothee Guerin <tiguerin@microsoft.com>
2021-02-01 14:20:23 -0800

    Handle circular dependency in additionalProperties (#3819)

--- packages/extensions/modelerfour/test/modeler/modelerfour.test.ts
+++ packages/extensions/modelerfour/test/modeler/modelerfour.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
