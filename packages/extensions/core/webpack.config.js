9add033f9f4c67e0368d640378e2d6dbbe2d7906
Timothee Guerin <tiguerin@microsoft.com>
2021-02-08 13:57:32 -0800

    Wip

--- packages/extensions/core/webpack.config.js
+++ packages/extensions/core/webpack.config.js
@@ -4,26 +4,40 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const baseWebpackConfig = require("../../../common/config/webpack.base.config");
 
-/**
- * @type {import("webpack").Configuration}
- */
-module.exports = {
+const coreBaseConfig = {
   ...baseWebpackConfig,
-  entry: {
-    "app": "./src/app.ts",
-    "exports": "./src/exports.ts",
-    "language-service": "./src/language-service/language-service.ts",
-  },
   output: {
     ...baseWebpackConfig.output,
     path: path.resolve(__dirname, "dist"),
-    library: "AutoRestCore",
-    libraryTarget: "commonjs2",
   },
   externals: [
     nodeExternals({
       allowlist: [/^(?:(?!jsonpath|@azure-tools\/extension).)*$/],
     }),
   ],
-  plugins: [],
 };
+
+/**
+ * @type {import("webpack").Configuration}
+ */
+module.exports = [
+  {
+    ...coreBaseConfig,
+    entry: {
+      "app": "./src/app.ts",
+      "language-service": "./src/language-service/language-service.ts",
+    },
+  },
+  {
+    ...coreBaseConfig,
+    entry: {
+      exports: "./src/exports.ts",
+    },
+    output: {
+      ...coreBaseConfig.output,
+      path: path.resolve(__dirname, "dist"),
+      library: "AutoRestCore",
+      libraryTarget: "commonjs2",
+    },
+  },
+];
