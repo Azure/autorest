7d21446c5f41001d4b99fc4d949b4f92d73c8817
Timothee Guerin <tiguerin@microsoft.com>
2021-02-17 13:46:53 -0800

    Wip

---
+++ packages/extensions/core/test/plugins/components-cleaner/components-cleaner.test.ts
@@ -0,0 +1,57 @@
+import { ComponentsCleaner } from "../../../src/lib/pipeline/plugins/components-cleaner";
+import fs from "fs";
+import { CancellationTokenSource, DataStore, MemoryFileSystem } from "@azure-tools/datastore";
+import assert from "assert";
+
+const readData = async (file: string) => {
+  const map = new Map<string, string>();
+
+  const inputUri = `mem://${file}`;
+  const inputText = await fs.promises.readFile(`${__dirname}/inputs/${file}`);
+  map.set(inputUri, inputText.toString());
+
+  const mfs = new MemoryFileSystem(map);
+  const cts: CancellationTokenSource = {
+    cancel() {
+      /* unused */
+    },
+    dispose() {
+      /* unused */
+    },
+    token: { isCancellationRequested: false, onCancellationRequested: <any>null },
+  };
+  const ds = new DataStore(cts.token);
+  const scope = ds.GetReadThroughScope(mfs);
+  return await scope.Read(inputUri);
+};
+
+const runComponentCleaner = async (scenarioName: string) => {
+  const input = await readData(`${scenarioName}.json`);
+  assert(input);
+  const cleaner = new ComponentsCleaner(input);
+  return {
+    input: await input?.ReadObject(),
+    output: await cleaner.getOutput(),
+  };
+};
+
+const expectScenarioToMatchSnapshot = async (scenarioName: string) => {
+  const { output } = await runComponentCleaner(scenarioName);
+  expect(JSON.stringify(output, null, 2)).toMatchRawFileSnapshot(`${__dirname}/expected/${scenarioName}.json`);
+};
+
+describe("ComponentCleaner", () => {
+  it("doesn't remove anything when there isn't any secondary file components", async () => {
+    const { input, output } = await runComponentCleaner("no-secondary-file-components");
+    expect(output).toEqual(input);
+  });
+
+  it("doesn't remove anything when all secondary components are referenced from primary file.", async () => {
+    const { input, output } = await runComponentCleaner("all-secondary-components-referenced");
+    expect(output).toEqual(input);
+  });
+
+  it("secondary-file components not referenced by something in a primary-file.", async () => {
+    await expectScenarioToMatchSnapshot("some-unused-secondary-components");
+  });
+});
