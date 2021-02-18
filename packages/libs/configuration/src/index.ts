b3d457dfe985584c759946bf8c35e6005f18f51d
Timothee Guerin <tiguerin@microsoft.com>
2021-02-18 08:55:16 -0800

    Configuration manager

--- packages/libs/configuration/src/index.ts
+++ packages/libs/configuration/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./autorest-configuration";
 export * from "./autorest-raw-configuration";
 export * from "./configuration-merging";
+export * from "./configuration-manager";
 export * from "./directive";
 export * from "./utils";
