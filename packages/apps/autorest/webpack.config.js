c210dc9a32a8875b2ac59db76bcbfdd08d82f597
Timothee Guerin <tiguerin@microsoft.com>
2021-03-08 15:46:32 -0800

    fix crash

--- packages/apps/autorest/webpack.config.js
+++ packages/apps/autorest/webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "commonjs2",
   },
+  resolve: {
+    ...baseWebpackConfig.resolve,
+    alias: {
+      jsonpath: path.resolve(__dirname, "node_modules", "jsonpath", "jsonpath.min.js"),
+    },
+  },
   plugins: [
     // We need to copy the yarn cli.js so @azure-tools/extensions can call the file as it is.(Not bundled in the webpack bundle.)
     new CopyPlugin({
