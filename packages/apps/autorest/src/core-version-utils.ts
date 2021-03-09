e38dba3e70d00ee1e2c36bd96136df613f55f331
Timothee Guerin <tiguerin@microsoft.com>
2021-03-08 16:17:45 -0800

    Fix loading

--- packages/apps/autorest/src/core-version-utils.ts
+++ packages/apps/autorest/src/core-version-utils.ts
@@ -4,6 +4,7 @@ import { AutorestLogger } from "../../../libs/configuration/node_modules/@autore
 import { CreateFileOrFolderUri, CreateFolderUri, ResolveUri } from "@azure-tools/uri";
 import { AppRoot } from "./constants";
 import { extensionManager } from "./autorest-as-a-service";
+import chalk from "chalk";
 
 const inWebpack = typeof __webpack_require__ === "function";
 const nodeRequire = inWebpack ? __non_webpack_require__ : require;
@@ -20,14 +21,15 @@ export const getRequestedCoreVersion = (args: AutorestArgs): string => {
   return args.version || (args.latest && "latest") || (args.preview && "preview") || "latest-installed";
 };
 
+const cwd = process.cwd();
+
 /**
  * Tries to load the version of autorest core from a config file.
  * @param args CLI Version.
  * @param selectedVersion Path to or loaded version of @autorest/core.
  */
 export const findCoreVersionUsingConfiguration = async (args: AutorestArgs): Promise<string | undefined> => {
-  const configFileOrFolder = args.configFileOrFolder;
-
+  const configFileOrFolder = ResolveUri(CreateFolderUri(cwd), args.configFileOrFolder || ".");
   /* eslint-disable no-console */
   const logger: AutorestLogger = {
     fatal: (x) => console.error(x),
@@ -42,5 +44,11 @@ export const findCoreVersionUsingConfiguration = async (args: AutorestArgs): Pro
     extensionManager: await extensionManager,
   });
   const { config } = await loader.load([args], false);
+  if (config.version) {
+    // eslint-disable-next-line no-console
+    console.log(
+      chalk.yellow(`NOTE: AutoRest core version selected from configuration: ${chalk.yellow.bold(config.version)}.`),
+    );
+  }
   return config.version;
 };
