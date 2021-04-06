a7be1521bb888c8b20dc0cd1a04cbbfa7325456a
Timothee Guerin <tiguerin@microsoft.com>
2021-04-06 15:47:16 -0700

    WIp

--- packages/extensions/adl/src/adl-compiler-plugin.ts
+++ packages/extensions/adl/src/adl-compiler-plugin.ts
@@ -1,14 +1,13 @@
 // TODO-TIM fix this in extension-base
-import { Host, startSession } from "@autorest/extension-base";
+import { Host } from "@autorest/extension-base";
+import { fileURLToPath } from "url";
 import { compileAdl } from "./adl-compiler.js";
 
 export async function setupAdlCompilerPlugin(host: Host) {
-  const session = await startSession(host);
-  const inputFiles = await session.getValue<string[]>("inputFileUris");
+  const inputFiles = await host.GetValue("inputFileUris");
   console.error("Input files", inputFiles);
   const entrypoint = inputFiles[0];
-  const result = await compileAdl(entrypoint);
-  session;
+  const result = await compileAdl(fileURLToPath(entrypoint));
   console.error("Result", result);
   host.WriteFile("swagger-document.json", "{}", undefined, "swagger-document");
 }
--- packages/extensions/adl/src/adl-compiler.ts
+++ packages/extensions/adl/src/adl-compiler.ts
@@ -1,14 +1,14 @@
 import { CompilerHost, createProgram } from "@azure-tools/adl";
 import { readdir, readFile, realpath, stat } from "fs/promises";
 import { join, resolve } from "path";
-import { pathToFileURL } from "url";
+import { fileURLToPath, pathToFileURL } from "url";
 
 export function createAdlHost(): CompilerHost {
   return {
     readFile: (path: string) => readFile(path, "utf-8"),
     readDir: (path: string) => readdir(path, { withFileTypes: true }),
     getCwd: () => process.cwd(),
-    getExecutionRoot: () => resolve(process.cwd()),
+    getExecutionRoot: () => resolve(fileURLToPath(import.meta.url), "../../node_modules/@azure-tools/adl"),
     getJsImport: (path: string) => import(pathToFileURL(path).href),
     getLibDirs() {
       const rootDir = this.getExecutionRoot();
