d485ad927090d3a971b43d13e01bbaca8e8422ed
Timothee Guerin <tiguerin@microsoft.com>
2021-04-20 16:04:37 -0700

    fix

--- regression-tests/smoke-test.ps1
+++ regression-tests/smoke-test.ps1
@@ -1,6 +1,6 @@
 param (
-   [string] $coreVersion = "../packages/extension/core",
-   [string] $m4Version = "../packages/extension/modelerfour"
+   [string] $coreVersion = "",
+   [string] $m4Version = ""
 )
 
 $ErrorActionPreference = "Stop"
@@ -19,7 +19,16 @@ function Exec
     }
 }
 
+if($coreVersion -eq "") {
+    $coreVersion = Resolve-Path "$PSScriptRoot/../packages/extensions/core"
+}
 
+if($m4Version -eq "") {
+    $m4Version = Resolve-Path "$PSScriptRoot/../packages/extensions/modelerfour"
+}
+
+Write-Host "Using Core $coreVersion"
+Write-Host "Using M4 $m4Version"
 foreach ($input in Get-Content (Join-Path $PSScriptRoot "smoke-tests.yaml"))
 {
     if ($input -match "^(?<readme>[^#].*?specification/(?<name>[\w-]+(/[\w-]+)+)/readme.md)(:(?<args>.*))?")
