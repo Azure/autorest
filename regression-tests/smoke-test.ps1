448442de75df3793cedf9623d4dea29fd7f9c74f
Timothee Guerin <tiguerin@microsoft.com>
2021-04-20 11:06:28 -0700

    Smoke tests

---
+++ regression-tests/smoke-test.ps1
@@ -0,0 +1,31 @@
+param (
+   [string] $coreVersion,
+   [string] $m4Version,
+)
+
+$ErrorActionPreference = "Stop"
+
+foreach ($input in Get-Content (Join-Path $PSScriptRoot "smoke-tests.yaml"))
+{
+    if ($input -match "^(?<readme>[^#].*?specification/(?<name>[\w-]+(/[\w-]+)+)/readme.md)(:(?<args>.*))?")
+    {
+        $readme = $Matches["readme"]
+
+        Write-Host "Testing spec: $readme"
+        Exec { autorest --version=$coreVersion --use:$m4Version }
+    }
+}
+
+
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ("Error executing command {0}" -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
