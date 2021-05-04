ebb01615eed56db76a382784a98b4edcd1dd467e
Timothee Guerin <tiguerin@microsoft.com>
2021-05-04 11:15:09 -0700

    /

--- packages/tools/fixer/src/app/swagger-fixer/swagger-fixer.ts
+++ packages/tools/fixer/src/app/swagger-fixer/swagger-fixer.ts
@@ -17,14 +17,16 @@ export function fixSwaggerMissingType(filename: string, spec: any): FixResult {
 
   forEachDefinitions(newSpec, (definition, path) => {
     if (!("type" in definition) && autorestAssumeSchemaIsObject(definition)) {
-      definition.type = "object";
+      // Set type:object as the "first" property.
       fixes.push({
         filename,
         code: FixCode.MissingTypeObject,
         message: `Schema is defining properties but is missing type: object.`,
         path,
       });
+      return { type: "object", ...definition };
     }
+    return definition;
   });
 
   return { spec: newSpec, fixes };
@@ -34,44 +36,49 @@ function autorestAssumeSchemaIsObject(definition: any) {
   return definition.properties || definition.additionalProperties || definition.allOf;
 }
 
-function forEachDefinitions(spec: any, handler: (definition: any, path: string[]) => void) {
+function forEachDefinitions(spec: any, handler: (definition: any, path: string[]) => any) {
   if (!spec.definitions) {
     return;
   }
 
   for (const [name, definition] of Object.entries<any>(spec.definitions)) {
-    forEachNestedDefinitions(definition, ["definitions", name], handler);
+    spec.definitions[name] = forEachNestedDefinitions(definition, ["definitions", name], handler);
   }
 }
 
-function forEachNestedDefinitions(definition: any, path: string[], handler: (definition: any, path: string[]) => void) {
+function forEachNestedDefinitions(
+  definition: any,
+  path: string[],
+  handler: (definition: any, path: string[]) => any,
+): any {
   if (typeof definition !== "object") {
     return;
   }
 
-  handler(definition, path);
+  definition = handler(definition, path);
 
   if (definition.properties) {
     for (const [name, nestedDefinition] of Object.entries<any>(definition.properties)) {
-      forEachNestedDefinitions(nestedDefinition, [...path, "properties", name], handler);
+      definition.properties[name] = forEachNestedDefinitions(nestedDefinition, [...path, "properties", name], handler);
     }
   }
 
   if (definition.allOf) {
     for (const [name, nestedDefinition] of Object.entries<any>(definition.allOf)) {
-      forEachNestedDefinitions(nestedDefinition, [...path, "allOf", name], handler);
+      definition.allOf[name] = forEachNestedDefinitions(nestedDefinition, [...path, "allOf", name], handler);
     }
   }
 
   if (definition.oneOf) {
     for (const [name, nestedDefinition] of Object.entries<any>(definition.oneOf)) {
-      forEachNestedDefinitions(nestedDefinition, [...path, "oneOf", name], handler);
+      definition.oneOf[name] = forEachNestedDefinitions(nestedDefinition, [...path, "oneOf", name], handler);
     }
   }
 
   if (definition.anyOf) {
     for (const [name, nestedDefinition] of Object.entries<any>(definition.anyOf)) {
-      forEachNestedDefinitions(nestedDefinition, [...path, "anyOf", name], handler);
+      definition.anyOf[name] = forEachNestedDefinitions(nestedDefinition, [...path, "anyOf", name], handler);
     }
   }
+  return definition;
 }
