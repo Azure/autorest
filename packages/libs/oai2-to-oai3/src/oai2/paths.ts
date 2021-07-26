08b1953292e8a8a63680d09e7ce51bc928913252
Timothee Guerin <tiguerin@microsoft.com>
2021-07-26 10:03:45 -0700

    Feature: New tree source map builder (#4229)

--- packages/libs/oai2-to-oai3/src/oai2/paths.ts
+++ packages/libs/oai2-to-oai3/src/oai2/paths.ts
@@ -1,4 +1,4 @@
-import { OpenAPI2Definition, OpenAPI2Reference } from "./definition";
+import { OpenAPI2Definition, OpenAPI2Reference, Refable } from "./definition";
 import { OpenAPI2HeaderDefinition } from "./header";
 import { OpenAPI2Parameter } from "./parameter";
 
@@ -7,7 +7,7 @@ import { OpenAPI2Parameter } from "./parameter";
  */
 export type HttpMethodCustom = "x-trace";
 
-export type HttpMethod = "get" | "post" | "patch" | "put" | "delete" | "options" | "head" | "trace";
+export type HttpMethod = "get" | "post" | "patch" | "put" | "delete" | "options" | "head" | "trace" | "x-trace";
 
 export type OpenAPI2Path = {
   [method in HttpMethod]: OpenAPI2Path;
@@ -19,7 +19,7 @@ export interface OpenAPI2Operation {
   operationId: string;
   description: string;
   responses: OpenAPI2OperationResponses;
-  parameters?: OpenAPI2Parameter[];
+  parameters?: Refable<OpenAPI2Parameter>[];
   produces?: string[];
   consumes?: string[];
 }
