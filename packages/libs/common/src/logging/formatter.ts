c84e1fd89872a25c8bbd7a80a56373d97a1c22c9
Timothee Guerin <tiguerin@microsoft.com>
2021-08-13 21:59:19 -0700

    Test colors

--- packages/libs/common/src/logging/formatter.ts
+++ packages/libs/common/src/logging/formatter.ts
@@ -2,6 +2,8 @@ import { serializeJsonPointer } from "@azure-tools/json";
 import { EnhancedPosition } from "@azure-tools/datastore";
 import { EnhancedLogInfo, EnhancedSourceLocation } from "./types";
 import { color } from "../utils";
+import { LogLevel } from ".";
+import chalk, { level } from "chalk";
 
 export interface LogFormatter {
   log(log: EnhancedLogInfo): string;
@@ -21,20 +23,61 @@ export function createLogFormatter(format: "json" | "regular" | undefined, optio
   return format === "json" ? new JsonLogFormatter(options) : new PrettyLogFormatter(options);
 }
 
+const LEVEL_STR: Record<LogLevel, string> = {
+  debug: "debug  ",
+  verbose: "verbose",
+  information: "info   ",
+  warning: "warn   ",
+  error: "error  ",
+  fatal: "fatal  ",
+};
+
+const LEVEL_COLORED_STR: Record<LogLevel, string> = {
+  debug: chalk.blue(LEVEL_STR.debug),
+  verbose: chalk.gray(LEVEL_STR.verbose),
+  information: chalk.green(LEVEL_STR.information),
+  warning: chalk.yellow.bold(LEVEL_STR.warning),
+  error: chalk.red.bold(LEVEL_STR.error),
+  fatal: chalk.redBright.bold(LEVEL_STR.fatal),
+};
+
 export class PrettyLogFormatter implements LogFormatter {
   private options: { color: boolean; timestamp: boolean };
 
-  public constructor(options: FormatterOptions) {
+  public constructor(options: FormatterOptions = {}) {
     this.options = { ...defaultOptions, ...options };
   }
 
   public log(log: EnhancedLogInfo): string {
-    const addTimestamp = this.options.timestamp && (log.level === "debug" || log.level === "verbose");
-    const t = addTimestamp ? ` [${getUpTime()} s]` : "";
-    let text = `${log.level.toUpperCase()}${log.code ? ` (${log.code})` : ""}${t}: ${log.message}`;
+    const useColor = this.options.color;
+    const t = this.formatTimestamp(log.level);
+    const level = useColor ? LEVEL_COLORED_STR[log.level] : LEVEL_STR[log.level];
+    const message = useColor ? color(log.message) : log.message;
+
+    let text = `${level} |${this.formatCode(log.code)}${t} ${message}`;
     for (const source of log.source ?? []) {
       text += this.formatSource(source);
     }
+    return text;
+  }
+
+  private formatCode(code: string | undefined): string {
+    if (!code) {
+      return "";
+    }
+
+    return ` ${this.color(code, chalk.green)} |`;
+  }
+
+  private formatTimestamp(level: LogLevel): string {
+    if (!(this.options.timestamp && (level === "debug" || level === "verbose"))) {
+      return "";
+    }
+    const colored = this.color(`[${getUpTime()} s]`, chalk.yellow);
+    return ` ${colored}`;
+  }
+
+  private color(text: string, color: (text: string) => string) {
     return this.options.color ? color(text) : text;
   }
 
@@ -43,7 +86,7 @@ export class PrettyLogFormatter implements LogFormatter {
       return "";
     }
     try {
-      return `\n    - ${source.document}${this.formatPosition(source.position)}`;
+      return `\n    - ${this.color(source.document, chalk.cyan)}${this.formatPosition(source.position)}`;
     } catch (e) {
       // no friendly name, so nothing more specific to show
       return "";
@@ -53,9 +96,9 @@ export class PrettyLogFormatter implements LogFormatter {
   private formatPosition(position: EnhancedPosition) {
     let text = "";
     if (position.line !== undefined) {
-      text += `:${position.line}`;
+      text += `:${this.color(position.line.toString(), chalk.cyan.bold)}`;
       if (position.column !== undefined) {
-        text += `:${position.column}`;
+        text += `:${this.color(position.column.toString(), chalk.cyan.bold)}`;
       }
     }
 
@@ -80,6 +123,6 @@ export class JsonLogFormatter implements LogFormatter {
 /**
  * @returns uptime of process in seconds
  */
-function getUpTime(): number {
-  return Math.floor(process.uptime() * 100) / 100;
+function getUpTime(): string {
+  return process.uptime().toFixed(2);
 }
