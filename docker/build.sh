665084071e7712206e817eef3b4dea0a3a9981c9
Timothee Guerin <tiguerin@microsoft.com>
2021-09-10 13:43:16 -0700

    All

--- docker/build.sh
+++ docker/build.sh
@@ -5,3 +5,5 @@ docker build -t azuresdk/autorest ./base/ubuntu
 docker build -t azuresdk/autorest-dotnet ./dotnet/ubuntu
 docker build -t azuresdk/autorest-python ./python/ubuntu
 docker build -t azuresdk/autorest-java ./java/ubuntu
+
+docker build -t azuresdk/autorest-all ./all/ubuntu
