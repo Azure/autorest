7749a7612f3b289ab585bdbdee1e46b00a0c03b0
Timothee Guerin <tiguerin@microsoft.com>
2022-01-05 15:30:40 -0800

    Fix

--- packages/libs/deduplication/src/main.ts
+++ packages/libs/deduplication/src/main.ts
@@ -390,7 +390,7 @@ export class Deduplicator {
   private async crawlObject(obj: any) {
     for (const { key, value } of visit(obj)) {
       // We don't want to navigate the examples.
-      if (key.startsWith("x-")) {
+      if (key === "x-ms-examples") {
         continue;
       }
 
@@ -408,7 +408,7 @@ export class Deduplicator {
   private updateRefs(obj: any): void {
     for (const { key, value } of visit(obj)) {
       // We don't want to navigate the examples.
-      if (key.startsWith("x-")) {
+      if (key === "x-ms-examples") {
         continue;
       }
 
