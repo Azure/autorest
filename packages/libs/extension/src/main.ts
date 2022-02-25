44f051a8e05467b28cb122c12aa01deb0b0a31a5
Timothee Guerin <tiguerin@microsoft.com>
2022-02-25 08:33:15 -0800

    Revert

--- packages/libs/extension/src/main.ts
+++ packages/libs/extension/src/main.ts
@@ -135,8 +135,6 @@ export async function fetchPackageMetadata(spec: string): Promise<pacote.Manifes
     });
   } catch (error) {
     logger.error(`Error resolving package ${spec}`, error);
-    // eslint-disable-next-line no-console
-    console.error(`Error resolving package ${spec}`, error);
     throw new UnresolvedPackageException(spec);
   }
 }
