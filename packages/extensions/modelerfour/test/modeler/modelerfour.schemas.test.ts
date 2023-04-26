fbb311baa71e3ab8262da2fcfdcacdbf048e67dd
Mike Kistler <mikekistler@microsoft.com>
2023-04-26 11:01:31 -0500

    Add one more test for type autocorrect

--- packages/extensions/modelerfour/test/modeler/modelerfour.schemas.test.ts
+++ packages/extensions/modelerfour/test/modeler/modelerfour.schemas.test.ts
@@ -555,6 +555,49 @@ describe("Modelerfour.Schemas", () => {
       expect(widget?.type).toBe("object");
     });
 
+    it("Auto-corrects schema with no type and allOf with no type to type: object", async () => {
+      const spec = createTestSpec();
+      addSchema(
+        spec,
+        "Widget",
+        {
+          description: "A widget",
+          allOf: [{ $ref: "#/components/schemas/BaseWidget" }, { $ref: "#/components/schemas/Metadata" }],
+        },
+        { name: "Widget" },
+      );
+
+      addSchema(
+        spec,
+        "BaseWidget",
+        {
+          properties: {
+            id: { type: "string" },
+            color: { type: "string" },
+          },
+        },
+        { name: "BaseWidget" },
+      );
+
+      addSchema(
+        spec,
+        "Metadata",
+        {
+          properties: {
+            createdAt: { type: "string", format: "date-time" },
+            updatedAt: { type: "string", format: "date-time" },
+          },
+        },
+        { name: "Metadata" },
+      );
+
+      const codeModel = await runModeler(spec);
+
+      const widget = findByName("Widget", codeModel.schemas.objects);
+      expect(widget).toBeDefined();
+      expect(widget?.type).toBe("object");
+    });
+
     it("Does not auto-correct schema with no type and oneOf with elements that are not type: object", async () => {
       const spec = createTestSpec();
       addSchema(
