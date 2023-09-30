33271d13a25e22405459a42fb0dfe49968f0e06b
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2023-09-29 17:17:56 -0700

    Do not generate core ARM types

--- packages/extensions/openapi-to-cadl/src/model.ts
+++ packages/extensions/openapi-to-cadl/src/model.ts
@@ -1,15 +1,28 @@
 import { CodeModel, isObjectSchema, Schema } from "@autorest/codemodel";
-import { transformTspArmResource } from "./transforms/transform-arm-resources";
-import { ArmResourceSchema, isResourceSchema } from "./utils/resource-discovery";
 import { getDataTypes } from "./data-types";
 import { CadlDataType, CadlProgram } from "./interfaces";
+import { transformTspArmResource } from "./transforms/transform-arm-resources";
 import { transformEnum } from "./transforms/transform-choices";
 import { getCadlType, transformObject } from "./transforms/transform-object";
 import { transformOperationGroup } from "./transforms/transform-operations";
 import { transformServiceInformation } from "./transforms/transform-service-information";
+import { ArmResourceSchema, isResourceSchema } from "./utils/resource-discovery";
 import { isChoiceSchema } from "./utils/schemas";
 
 const models: Map<CodeModel, CadlProgram> = new Map();
+
+const _ArmCoreTypes = [
+  "Resource",
+  "ProxyResource",
+  "TrackedResource",
+  "ErrorAdditionalInfo",
+  "ErrorDetail",
+  "ErrorResponse",
+  "Operation",
+  "OperationDisplay",
+  "Origin",
+  "SystemData",
+];
 
 export function getModel(codeModel: CodeModel): CadlProgram {
   let model = models.get(codeModel);
@@ -45,7 +58,9 @@ function transformModel(codeModel: CodeModel): CadlProgram {
   );
 
   const cadlObjects =
-    codeModel.schemas.objects?.filter((o) => !isResourceSchema(o)).map((o) => transformObject(o, codeModel)) ?? [];
+    codeModel.schemas.objects
+      ?.filter((o) => !isResourceSchema(o) && !_ArmCoreTypes.includes(o.language.default.name))
+      .map((o) => transformObject(o, codeModel)) ?? [];
   const armResources =
     codeModel.schemas.objects
       ?.filter((o) => isResourceSchema(o))
