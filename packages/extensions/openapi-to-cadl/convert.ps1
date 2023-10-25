3926a2240b4ed3492487c6b11e1fcfce07dbcf8e
tadelesh <tadelesh.shi@live.cn>
2023-10-25 14:31:43 +0800

    update script to prevent private npm issue

--- packages/extensions/openapi-to-cadl/convert.ps1
+++ packages/extensions/openapi-to-cadl/convert.ps1
@@ -2,23 +2,19 @@ param(
     [Parameter(Mandatory)]
     [string]$swaggerConfigFile,
     [string]$outputFolder,
-    [string]$registry = "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-js-test-autorest/npm/registry/",
-    [string]$csharpCodegen = "@autorest/csharp@3.0.0-alpha.20231017.8",
+    [string]$csharpCodegen = "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-js-test-autorest/npm/registry/@autorest/csharp/-/csharp-3.0.0-alpha.20231017.8.tgz",
     [string]$converterCodegen = ""
 )
 
 function GenerateMetadata ()
 {
     Write-Host "##Generating metadata with csharp codegen in $outputFolder with $csharpCodegen"
-    if (-not [string]::IsNullOrEmpty($registry))
-    {
-        Set-Item -Path Env:autorest_registry -Value "$registry"
-    }
     $cmd = "autorest --csharp --max-memory-size=8192 --use=$csharpCodegen --output-folder=$outputFolder --mgmt-debug.only-generate-metadata --azure-arm --skip-csproj $swaggerConfigFile"
     Write-Host "$cmd"
     Invoke-Expression  $cmd
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
-    if (Test-Path (Join-Path $outputFolder resources.json)) {
+    if (Test-Path (Join-Path $outputFolder resources.json))
+    {
         Remove-Item (Join-Path $outputFolder resources.json)
     }
     Rename-Item (Join-Path $outputFolder metadata.json) (Join-Path $outputFolder resources.json)
@@ -27,7 +23,7 @@ function GenerateMetadata ()
 function DoConvert ()
 {
     Write-Host "##Converting from swagger to tsp with in $outputFolder with $converterCodegen"
-    $cmd = "autorest --openapi-to-cadl --use=$converterCodegen --output-folder=$outputFolder --src-path=tsp-output $swaggerConfigFile"
+    $cmd = "autorest --openapi-to-cadl --isArm --use=$converterCodegen --output-folder=$outputFolder --src-path=tsp-output $swaggerConfigFile"
     Write-Host "$cmd"
     Invoke-Expression  $cmd
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
@@ -39,7 +35,7 @@ if ([string]::IsNullOrEmpty($outputFolder))
 }
 else
 {
-    $outputFolder = Resolve-Path $outputFolder
+    $outputFolder = (Resolve-Path $outputFolder) -replace "(\\|\/)$"
 }
 Write-Host "##output folder is $outputFolder"
 
