fb13f77bcd6c7b8e32689dfb6f056bd16f0e2e3c
Timothee Guerin <tiguerin@microsoft.com>
2025-05-21 12:12:14 -0700

    ..

--- packages/libs/datastore/src/json-path/json-path.ts
+++ packages/libs/datastore/src/json-path/json-path.ts
@@ -2,7 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { createSandbox } from "@azure-tools/codegen";
 import { JSONPath } from "jsonpath-plus";
 
 export type JsonPath = JsonPathComponent[];
